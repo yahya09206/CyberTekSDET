aa5f38fc2e5cd3df88fbbe4742338005098dd182
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-25 07:46:01 -0700

    section on concatenation

---
+++ Day05_ArithmeticOperators/Address.java
@@ -0,0 +1,15 @@
+public class Address {
+
+	public static void main(String[] args){
+		String name = "Conor",
+				buildingNumber = "123",
+				streetName = "Legacy Circle",
+				city = "Herndon",
+				state = "VA",
+				zipCode = "20171";
+
+		//Print Address
+		System.out.println(name + "\n" + buildingNumber +" " + streetName
+                +"\n"+ city+" " + state+" "+zipCode );
+	}
+}
