80962100c2c788d03616baf51cc678dd7718e7e4
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-25 09:10:35 -0700

    first test is passing

--- Day05_ArithmeticOperators/OddOrEven.java
+++ Day05_ArithmeticOperators/OddOrEven.java
@@ -1,5 +1,8 @@
 public class OddOrEven {
 	public static void main(String[] args){
-		
+		//check remainder
+		int number = 100;
+		boolean isEven = number  % 2 == 0; //if the remainder of number / 2 is equal to 0
+		System.out.println(isEven);
 	}
 }
