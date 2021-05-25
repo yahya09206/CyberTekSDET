70345e8a9c32c47642d7aa6f94572b6f2e6c10a0
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-25 08:03:16 -0700

    calculating present day age

--- Day05_ArithmeticOperators/Birthday.java
+++ Day05_ArithmeticOperators/Birthday.java
@@ -1,8 +1,13 @@
-public class Birthday.java {
+public class Birthday {
 	public static void main(String[] args){
 		String name = "John",
-				birthMonth = "May",
+				birthMonth = "May";
 		int birthDay = 15;
 		int birthYear = 1993;
+
+		System.out.println(name + " was born on " + birthMonth + "/" + birthDay + "/" + birthYear);
+
+		int age = 2021 - birthYear;
+		System.out.println(name + " is " + age);
 	}
 }
