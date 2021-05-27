b72967f4cae4d730d25c207c15231a8d16494263
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-27 07:40:18 -0700

    operator precedence

---
+++ Day06_UranyOperators/OperatorPrecedence.java
@@ -0,0 +1,9 @@
+public class OperatorPrecedence {
+	public static void main(String[] args){
+		System.out.println("Addition: " + 2 + 3);
+		System.out.println("Addition: " + (2 + 3));
+
+		System.out.println("-----------------------");
+		
+	}
+}
