6bd7cbb87b6ea27a535e95156dcf589dde43e6c4
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-28 08:07:24 -0700

    more practice with shorthand operators

--- Day07_UnaryAndShortHandOperators/ShortHandOperators.java
+++ Day07_UnaryAndShortHandOperators/ShortHandOperators.java
@@ -2,7 +2,26 @@ public class ShortHandOperators {
 	public static void main(String[] args){
 		String name1 = "Muhtar";
 		String name2 = "Mehmet";
+		String name3 = name2;
 
-		name2 = name1;
+		name2 = name1;	
+		name1 = name2;
+
+		System.out.println("name1 = " + name1);
+		System.out.println("name2 = " + name2);
+
+		System.out.println("------------------------");
+		int a = 200;
+		System.out.println("a = " + a);
+		int b = a;
+		a = 100 + 300;
+		System.out.println("a = " + a);
+
+		System.out.println("------------------------");
+		int balance = 300;
+			//balance = balance + 200;
+		balance += 200;
+
+		System.out.println("balance " + balance);
 	}
 }
