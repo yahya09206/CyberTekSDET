0c493e789996257737fc857683921fe6dec1e0f2
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-28 09:12:44 -0700

    more relational operators

--- Day07_UnaryAndShortHandOperators/RelationalOperators.java
+++ Day07_UnaryAndShortHandOperators/RelationalOperators.java
@@ -1,7 +1,26 @@
 public class RelationalOperators {
 	public static void main(String[] args){
 		// >, >=
-		System.out.println(10 > 11);
-		
+		System.out.println(10 > 11); // false
+		System.out.println(10 >= 10); // true
+		System.out.println(11 >= 9); // true
+		System.out.println(20 >= 200); // false
+
+		// <, <=
+		System.out.println(1000 < 100); // false
+		System.out.println(1000 < 10000); // true
+
+		System.out.println(10000 <= 1000); // false
+		System.out.println(1000 <= 10000); // true
+
+		// ==, !=
+		System.out.println(5 == 6); // false
+		System.out.println(5 != 6); // true
+
+		System.out.println('a' == 'A'); // false
+		System.out.println('a' != 'A'); // true
+
+		System.out.println("Java" == "java"); // false
+		System.out.println("Java" != "java"); //true; not equal
 	}
 }
