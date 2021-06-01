a772309654afaba7cf39b4ffb06241dc8861634c
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-01 09:35:22 -0700

    alternative to b grade

--- Day08_IfStatements/GradeReport2.java
+++ Day08_IfStatements/GradeReport2.java
@@ -2,6 +2,6 @@ public class GradeReport2 {
 	public static void main(String[] args){
 		int score = 95;
 		boolean madeA = score >= 90 && score <= 100;
-		
+		boolean madeB = !madeA && score >= 80;
 	}
 }
