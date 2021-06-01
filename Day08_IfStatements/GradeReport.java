89826e93b8b1bd08ecff92ae28f116527519493b
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-01 09:23:34 -0700

    finished conditions for rest of grades

--- Day08_IfStatements/GradeReport.java
+++ Day08_IfStatements/GradeReport.java
@@ -5,8 +5,17 @@ public class GradeReport {
 		if(score >= 90 && score <= 100){
 			System.out.println("You made an A.");
 		}
-		if(score >= && score < 90){
+		if(score >= 80 && score < 90){
 			System.out.println("You made a B.");
+		}
+		if(score >= 70 && score < 80){
+			System.out.println("You made a C.");
+		}
+		if(score >= 60 && score < 70){
+			System.out.println("You made a D.");
+		}
+		if(score > 60){
+			System.out.println("You are a failure.");
 		}
 	}
 }
