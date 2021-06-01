5cb5fb3e647b9223f71077c7c9b96f0d481d612e
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-01 09:07:51 -0700

    practicing with odd/even

--- Day08_IfStatements/OddOrEven.java
+++ Day08_IfStatements/OddOrEven.java
@@ -1,5 +1,14 @@
 public class OddOrEven {
 	public static void main(String[] args){
-		
+		int number = 100;
+		boolean odd = number % 2 != 0;
+
+		if(odd){
+			System.out.println(number + " is odd number");
+		}
+
+		if(!odd){
+			System.out.println(number + " is even number");
+		}
 	}
 }
