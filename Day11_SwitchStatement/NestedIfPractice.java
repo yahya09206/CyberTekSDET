2040b97b692abc410513dbdcfc0fb5e60848761e
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-07 08:10:05 -0700

    setting up tests for batch types

--- Day11_SwitchStatement/NestedIfPractice.java
+++ Day11_SwitchStatement/NestedIfPractice.java
@@ -1,5 +1,11 @@
 public class NestedIfPractice {
 	public static void main(String[] args){
-		
+
+		 String batchType = "US morning";
+		 String result = "";
+
+		 if(batchType == "US morning" || batchType == "US evening" || batchType == "EU"){
+		 	
+		 }
 	}
 }
