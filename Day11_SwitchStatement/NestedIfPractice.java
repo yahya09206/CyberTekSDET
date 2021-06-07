aa2e96d11705d9b70e2eb33d859dd6480e0a0035
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-07 08:13:41 -0700

    done with tests for which batch

--- Day11_SwitchStatement/NestedIfPractice.java
+++ Day11_SwitchStatement/NestedIfPractice.java
@@ -2,10 +2,19 @@ public class NestedIfPractice {
 	public static void main(String[] args){
 
 		 String batchType = "US morning";
+		 boolean isValid = batchType == "US morning" || batchType == "US evening" || batchType == "EU";
 		 String result = "";
 
-		 if(batchType == "US morning" || batchType == "US evening" || batchType == "EU"){
-		 	
+		 if(isValid){
+		 	if(batchType == "US morning"){
+		 		result = "10am-5pm EST. M, T, Th, F";
+		 	}else if(batchType == "US evening"){
+		 		result = "7pm-10pm EST. M, T, W, Th, S, S";
+		 	}else {
+		 		result = "10am-5pm EST. M, T, W, Th, F";
+		 	}
+		 }else {
+		 	result = "Invalid Batch";
 		 }
 	}
 }
