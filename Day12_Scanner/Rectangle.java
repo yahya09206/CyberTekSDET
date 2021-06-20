64553334993fa873b1fa7c75938d6af344afe2b5
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-19 23:41:36 -0700

    more scanner

---
+++ Day12_Scanner/Rectangle.java
@@ -0,0 +1,23 @@
+public class Rectangle {
+
+    public static void main(String[] args) {
+
+        Scanner  scan = new Scanner(System.in);
+
+        System.out.println("Enter the width of rectangle:");
+        int width = scan.nextInt();
+        System.out.println("Enter the length of rectangle:");
+        int length = scan.nextInt();
+
+        int area = width* length;
+        int perimeter = (width+length) * 2;
+
+        System.out.println("area = " + area);
+        System.out.println("perimeter = " + perimeter);
+
+
+
+    }
+
+
+}
