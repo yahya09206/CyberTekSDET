40adc61bb73bf7173dcb6ec373d5f9a7b566bc0b
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-21 11:58:07 -0700

    finding max num

--- Day19_Loops/MaximumNumber.java
+++ Day19_Loops/MaximumNumber.java
@@ -2,5 +2,16 @@ public class MaximumNumber {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		int max = -2147483648;
+
+		for(int i = 0; i < 5; i++){
+			System.out.println("Enter a number: ");
+			int n = scan.nextInt(); // 1
+
+			if(n > max){
+				max = n;
+			}
+		}
+		System.out.println("max = " + max);
+		scan.close();
 	}
 }
