da0b7fa3863a4839936a9fab2e920d3fad2e18c7
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-21 12:08:07 -0700

    working on finding min number

--- Day19_Loops/MaximumNumber.java
+++ Day19_Loops/MaximumNumber.java
@@ -1,7 +1,9 @@
 public class MaximumNumber {
+
 	public static void main(String[] args){
+
 		Scanner scan = new Scanner(System.in);
-		int max = -2147483648;
+		int max = -2147483647;
 
 		for(int i = 0; i < 5; i++){
 			System.out.println("Enter a number: ");
---
+++ Day19_Loops/MinimumNumber.java
@@ -0,0 +1,19 @@
+public class MinimumNumber {
+
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		int min = 2147483647;
+
+		for(int i = 0; i < 5; i++){
+			System.out.println("Enter a number ");
+			int n = scan.nextInt(); // 100 300 50 400 5
+		}
+
+		if(n < min){
+			min = n;
+		}
+		System.out.println("min = " + min);
+		scan.close();
+	}
+}
