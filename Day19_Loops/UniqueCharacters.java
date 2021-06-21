e881ce175592144be366f4950141760d825641bd
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-21 13:13:27 -0700

    finding unique characters

---
+++ Day19_Loops/UniqueCharacters.java
@@ -0,0 +1,10 @@
+public class UniqueCharacters {
+	public static void main(String[] args){
+		String word = "aabcc";
+		String result = "";
+
+		for(int i = 0; i <= word.length() - 1; i++){
+			
+		}
+	}
+}
