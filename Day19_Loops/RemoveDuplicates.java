f9c35e4a46074f54b8a71a25bafa5ccc7ad66a63
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-21 12:00:06 -0700

    done with remove duplicates

---
+++ Day19_Loops/RemoveDuplicates.java
@@ -0,0 +1,35 @@
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        String word = new Scanner(System.in).next();
+
+        String result = ""; //"ab"
+
+        if(word.length() == 5 ) {
+            if (!result.contains("" + word.charAt(0))) { //'a'
+                result += word.charAt(0);
+            }
+            if (!result.contains("" + word.charAt(1))) { //'a'
+                result += word.charAt(1);
+            }
+            if (!result.contains("" + word.charAt(2))) { //'b'
+                result += word.charAt(2);
+            }
+            if (!result.contains("" + word.charAt(3))) { //'b'
+                result += word.charAt(3);
+            }
+            if (!result.contains("" + word.charAt(4))) { //'c'
+                result += word.charAt(4);
+            }
+
+        }else if(word.length() > 5){
+            result = "Too long";
+        }else{ //word.length() < 5
+            result = "Too Short";
+        }
+
+        System.out.println("result = " + result);
+
+    }
+}
