61d22ae2524b69b2b9c56561b197951ac94d7abe
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-22 13:01:36 -0700

    printing odd numbers using continue statement

--- Day20_Loops/BranchingStatements.java
+++ Day20_Loops/BranchingStatements.java
@@ -1,5 +1,15 @@
 public class BranchingStatements {
 	public static void main(String[] args){
-		
+
+		for(int i = 0; i <= 5; i++){
+			System.out.println(i);
+		}
+
+		for(int i = 0; i <= 10; i++){
+			if(i % 2 == 0){
+				continue;
+			}
+				System.out.print(i + " ");
+		}
 	}
 }
