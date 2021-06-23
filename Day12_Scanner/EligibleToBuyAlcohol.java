d2a253136297fcc3cd469e36579d38146de1bb73
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 12:53:11 -0700

    done with eligible

--- Day12_Scanner/EligibleToBuyAlcohol.java
+++ Day12_Scanner/EligibleToBuyAlcohol.java
@@ -3,7 +3,16 @@ public class EligibleToBuyAlcohol {
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
-        
+
+        System.out.print("Enter your age: ");
+        int age = scan.nextInt();
+
+        if(age >= 21){
+            System.out.println("You are eligible to alcohol");
+        }else {
+            System.out.println("Get out!");
+        }
+
     }
 
 }
