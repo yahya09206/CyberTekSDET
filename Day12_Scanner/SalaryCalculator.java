8fe4fe9594b6c52aed4e1d49e6360c6713e202b7
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 13:08:04 -0700

    done with prompts

--- Day12_Scanner/SalaryCalculator.java
+++ Day12_Scanner/SalaryCalculator.java
@@ -1,10 +1,18 @@
+import java.util.*;
 public class SalaryCalculator {
-
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your hourly rate: ");
+        double hourlyRate = scan.nextDouble();
+
+        System.out.print("How many hours do you work in a week? ");
+        int hours = scan.nextInt();
+
+        System.out.print("How many weeks do you work in a year? ");
+        int weeks = scan.nextInt();
+
+        
 
     }
-
-
-
 }
