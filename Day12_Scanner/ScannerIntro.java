35512bcf68f417806bf2b550097dc728f5c378fe
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-22 20:43:19 -0700

    found the sum of two numbers from user input

--- Day12_Scanner/ScannerIntro.java
+++ Day12_Scanner/ScannerIntro.java
@@ -3,5 +3,11 @@ import java.util.*;
 public class ScannerIntro {
 	public static void main(String[] args){
 
+		//Declaring Scanner
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Please enter two numbers ");
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		System.out.println("The sum of number 1 and number 2 are " + (num1 + num2));
 	}
 }
