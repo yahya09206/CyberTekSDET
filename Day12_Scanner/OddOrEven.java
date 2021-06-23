58f89af9a1609ce6c55200982c4b954119f2d2e0
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 12:54:02 -0700

    re doing oddoreven

--- Day12_Scanner/OddOrEven.java
+++ Day12_Scanner/OddOrEven.java
@@ -1,17 +1,8 @@
+import java.util.*;
 public class OddOrEven {
-
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
-        System.out.println("Enter a number:");
-        int number = scan.nextInt();
-
-        System.out.println(    ( number%2 !=0 )? "Odd": "Even"   );
-
-        System.out.println(    (number>0)?"Positive" :(number< 0)? "Negative" : "Zero"    );
-
-
     }
 
 
