af16469e16005cc5ab27a05d73bf2540355bf724
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-22 20:51:11 -0700

    recording user input for width and length

--- Day12_Scanner/Rectangle.java
+++ Day12_Scanner/Rectangle.java
@@ -5,9 +5,9 @@ public class Rectangle {
     public static void main(String[] args) {
 
         Scanner  scan = new Scanner(System.in);
-
-        
-
+        System.out.println("Enter the width of rectangle: ");
+        int width = scan.nextInt();
+        int length = scan.nextInt();
 
 
     }
