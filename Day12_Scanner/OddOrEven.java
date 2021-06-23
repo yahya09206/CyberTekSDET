77f640ea4be7d802f61d21ea5176bb4b037161f0
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 12:55:39 -0700

    odd or even is working

--- Day12_Scanner/OddOrEven.java
+++ Day12_Scanner/OddOrEven.java
@@ -3,6 +3,10 @@ public class OddOrEven {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scan.nextInt();
+
+        System.out.println((number % 2 != 0)? "Odd": "Even");
     }
 
 
