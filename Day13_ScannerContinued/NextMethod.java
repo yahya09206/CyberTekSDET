01551d408714e866c526c95ea00ba14541865d8b
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 20:10:14 -0700

    praciticing with next method

---
+++ Day13_ScannerContinued/NextMethod.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class NextMethod {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		String name = scan.next();
+	}
+}
