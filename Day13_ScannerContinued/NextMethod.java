865d0a1753671f9f3cee5d07ba34f83c6c9f543b
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 20:12:56 -0700

    full name is working

--- Day13_ScannerContinued/NextMethod.java
+++ Day13_ScannerContinued/NextMethod.java
@@ -3,7 +3,14 @@ public class NextMethod {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter your name: ");
-		String name = scan.next();
+		System.out.print("Enter your first name: ");
+		String firstName = scan.next(); 
+
+		System.out.print("Enter your last name: ");
+		String lastName = scan.next();
+
+		String fullName = firstName + " " + lastName;
+
+		System.out.println("Full Name = " + fullName);
 	}
 }
