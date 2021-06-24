6de9cd6b00321ea2cc3f62c892bdaff8f5883e4b
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 20:26:27 -0700

    next line method is working

--- Day13_ScannerContinued/NextLineMethod.java
+++ Day13_ScannerContinued/NextLineMethod.java
@@ -1,6 +1,11 @@
 import java.util.*;
 public class NextLineMethod {
 	public static void main(String[] args){
-		
+
+		 Scanner scan = new Scanner(System.in);
+		 System.out.print("Enter your job title: ");
+		 String jobTitle = scan.nextLine(); 
+
+		 System.out.println("Job Title: " + jobTitle);
 	}
 }
