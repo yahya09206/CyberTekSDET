aa1add2ab6022c35cfe0fbd720bc0eebce5ce720
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 20:01:22 -0700

    boolean inputs are working

--- Day13_ScannerContinued/ScannerMethods.java
+++ Day13_ScannerContinued/ScannerMethods.java
@@ -3,6 +3,19 @@ public class ScannerMethods {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 
-		
+		System.out.print("Are you a full time employee? ");
+		boolean isFullTime = scan.nextBoolean();
+
+		System.out.print("Are you an adult? ");
+		boolean isAdult = scan.nextBoolean();
+
+		System.out.print("Are you married? ");
+		boolean isMarried = scan.nextBoolean();
+
+		System.out.println("isFullTime " + isFullTime);
+		System.out.println("isAdult " + isAdult);
+		System.out.println("isMarried " + isMarried);
+
+
 	}
 }
