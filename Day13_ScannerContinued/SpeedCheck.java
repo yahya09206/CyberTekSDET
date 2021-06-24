a5d20e8b227dc5640e0ccd499e27313c1e74bb53
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 19:55:50 -0700

    done with speed check

--- Day13_ScannerContinued/SpeedCheck.java
+++ Day13_ScannerContinued/SpeedCheck.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 public class SpeedCheck {
 	public static void main(String[] args){
+		int speedLimit = 35;
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter your current speed: ");
 		int currentSpeed = scan.nextInt();
 
 		System.out.println("Current speed = " + currentSpeed);
+
+		int overTheSpeedLimit = currentSpeed - speedLimit;
+		if(overTheSpeedLimit > 0){
+			System.out.println("You are driving " + overTheSpeedLimit + " over the speed limit. Slow Down!");
+		}
 	}
 }
