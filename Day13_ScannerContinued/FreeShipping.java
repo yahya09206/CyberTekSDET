132803f5b0e2edd5db862e3f32ca34868e447887
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 20:08:30 -0700

    free shipping check is working

--- Day13_ScannerContinued/FreeShipping.java
+++ Day13_ScannerContinued/FreeShipping.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 public class FreeShipping {
 	public static void main(String[] args){
-		
+
+		 Scanner scan = new Scanner(System.in);
+		 System.out.print("Enter the price of the item: ");
+		 double price = scan.nextDouble();
+
+		 System.out.print("Are you a prime member? ");
+		 boolean isPrime = scan.nextBoolean();
+
+		 double totalPrice = (isPrime)? price :(price>=25)? price :price + 3.99;
+
+		 System.out.println("Total Price: = $" + totalPrice);
 	}
 }
