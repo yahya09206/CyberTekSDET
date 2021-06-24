c7d6c068015ba92609781c2fca38d67890b46eac
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-23 19:48:35 -0700

    done with kilometers to miles

--- Day13_ScannerContinued/MilesToKilometers.java
+++ Day13_ScannerContinued/MilesToKilometers.java
@@ -5,5 +5,8 @@ public class MilesToKilometers {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter Miles: ");
 		double miles = scan.nextDouble();
+		double km = miles * 1.609;
+
+		System.out.println(miles + " miles is equal to " + km + " kilometers");
 	}
 }
