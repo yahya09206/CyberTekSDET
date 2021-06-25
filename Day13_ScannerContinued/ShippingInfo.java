ca789bb3ddf5cd833b173a6ca0b89aabef1e48a9
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-24 19:51:26 -0700

    confused by which scanner to use

---
+++ Day13_ScannerContinued/ShippingInfo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ShippingInfo {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("What is your building number? ");
+		int builderNumber = scan.nextInt();	
+
+		scan.nextLine();
+		
+		System.out.print("What is your street address? ");
+		String address = scan.nextLine();
+
+		System.out.print("What city do you live in? ");
+		String city = scan.nextLine();
+
+		System.out.print("What state do you live in? ");
+		String state = scan.next();
+
+		System.out.print("What is your zip code? ");
+		String zipCode = scan.next();
+
+		scan.nextLine();
+		System.out.print("What is your name? ");
+		String fullName = scan.nextLine();
+
+		System.out.println(fullName);
+		System.out.println(address);
+		System.out.println(city + ", " + state + " " + zipCode);
+	}
+}
