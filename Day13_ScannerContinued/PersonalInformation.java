d3c5e0b5fdaf502e7d257694e7e49c8a94fc9906
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-24 19:39:25 -0700

    job title is showing

--- Day13_ScannerContinued/PersonalInformation.java
+++ Day13_ScannerContinued/PersonalInformation.java
@@ -9,7 +9,17 @@ public class PersonalInformation {
 		System.out.print("Enter your gender: ");
 		String gender = scan.next();
 
+		scan.nextLine(); //Clear out scanner
+
 		System.out.print("Enter your full name: ");
 		String fullName = scan.nextLine();
+
+		System.out.print("Enter your job title: ");
+		String jobTitle = scan.nextLine();
+
+		System.out.println("Salary = " + salary);
+		System.out.println("Gender = " + gender);
+		System.out.println("fullName = " + fullName);
+		System.out.println("jobTitle = " + jobTitle);
 	}
 }
