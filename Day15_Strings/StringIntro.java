b1cb18735ef70c7345018522697b4adc5a018446
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-27 14:07:03 -0700

    going thru different packages

---
+++ Day15_Strings/StringIntro.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.String; //lang package doesn't need to be explicity imported
+
+public class StringIntro {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+
+		String str = "CyberTek";
+	}
+}
