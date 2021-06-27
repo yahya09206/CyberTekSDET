c281decea024c1162397dcacdedf832cc3c32b52
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-27 14:23:44 -0700

    comparing strings using new keyword

--- Day15_Strings/StringIntro.java
+++ Day15_Strings/StringIntro.java
@@ -12,10 +12,16 @@ public class StringIntro {
 		System.out.println("--------------------------------");
 
 		String s1 = "Cat";
-		String s2 = "Cat";
+		String s2 = "CAt";
 		String s3 = "Cat";
-
 		System.out.println(s1 == s2);
 
+		System.out.println("--------------------------------");
+		String s4 = new String("Dog"); //New object in java heap is created
+		String s5 = new String("Dog");
+		String s6 = new String("Dog");
+
+		System.out.println(s4 == s5);
+		System.out.println(s5 == s6);
 	}
 }
