8680bf1104a52e928a7a0e95d6ba9af1df85ee7e
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-27 16:59:36 -0700

    done with replace methods

--- Day15_Strings/StringMethods.java
+++ Day15_Strings/StringMethods.java
@@ -48,6 +48,17 @@ public class StringMethods {
 
 		System.out.println("--------------------");
 		System.out.println("replace() Method");
+		String sentence3 = "Python is an easy language";
+		sentence3 = sentence3.replace("Python", "Java");
+		System.out.println(sentence3);
+
+		String sentence4 = "C# is a programming language, I like learning C#, C# is the best";
+		sentence4 = sentence4.replace("C#", "Java");
+		System.out.println(sentence4);
+
+		String sentence5 = "Dog is friendly, Dog is loyal, I love Dogs";
+		sentence5 = sentence5.replace("Dog", "Cat");
+		System.out.println(sentence5);
 
 	}
 }
