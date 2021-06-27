64ec02603152037850cdbf76cbc1fb9182a7bb2e
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-27 15:20:45 -0700

    done with length and concat methods

--- Day15_Strings/StringMethods.java
+++ Day15_Strings/StringMethods.java
@@ -9,7 +9,7 @@ public class StringMethods {
 		String word = "Cybertek School";
 		System.out.println(word.charAt(9));
 		System.out.println("--------------------");
-		System.out.println("Length Method");
+		System.out.println("Length() Method");
 
 		String sentence = "Hello everyone, how are you all today?";
 		int totalNumberOfChars = sentence.length();
@@ -17,6 +17,15 @@ public class StringMethods {
 
 		String s1 = "Cybertek";
 		System.out.println(s1.charAt(0) + " " + (s1.length()-1));
+
+		System.out.println("--------------------");
+		System.out.println("Concat() Method");
+		String firstName = "John";
+		String lastName = "King";
+
+		String fullName = firstName + " " + lastName;
+		String fullName2 = firstName.concat(" ").concat(lastName);
+		System.out.println(fullName2);
 
 
 	}
