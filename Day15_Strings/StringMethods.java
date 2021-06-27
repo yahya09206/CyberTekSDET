b1112530733329226e92962cd3f509296db817a6
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-27 14:41:54 -0700

    playing with charAt method

---
+++ Day15_Strings/StringMethods.java
@@ -0,0 +1,13 @@
+public class StringMethods {
+	public static void main(String[] args){
+
+		String str = "Muhtar";
+		//Get first character from string
+		char ch = str.charAt(0);
+		System.out.println("ch = " + ch);
+
+		System.out.println("--------------------");
+
+
+	}
+}
