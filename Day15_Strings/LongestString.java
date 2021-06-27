2183b5dd882de92c71af1d1a9326c02b34a2fcb5
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-27 15:24:58 -0700

    set up test to check length of two strings

--- Day15_Strings/LongestString.java
+++ Day15_Strings/LongestString.java
@@ -2,5 +2,16 @@ import java.util.Scanner;
 public class LongestString {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter two string");
+		String s1 = scan.nextLine();
+		String s2 = scan.nextLine();
+
+		if(s1.length() > s2.length()){
+			System.out.println(s1);
+		}else {
+			System.out.println(s2);
+		}
+
+		scan.close();
 	}
 }
