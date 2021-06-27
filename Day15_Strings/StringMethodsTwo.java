be1a287ac10f7fa7180fa4f545bae403426886dc
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-27 16:23:06 -0700

    still got it

--- Day15_Strings/StringMethodsTwo.java
+++ Day15_Strings/StringMethodsTwo.java
@@ -1,5 +1,8 @@
 public class StringMethodsTwo {
 	public static void main(String[] args){
-		
+
+		String sentence = "Java Programming Language";
+		String name = sentence.substring(0,4);
+		System.out.println(name);
 	}
 }
