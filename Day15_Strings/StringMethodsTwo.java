0c703e019436304472a4c918bf4f3d562fc47536
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-27 16:31:22 -0700

    practing getting substring in middle of sentence

--- Day15_Strings/StringMethodsTwo.java
+++ Day15_Strings/StringMethodsTwo.java
@@ -4,5 +4,17 @@ public class StringMethodsTwo {
 		String sentence = "Java Programming Language";
 		String name = sentence.substring(0,4);
 		System.out.println(name);
+
+		String s2 = "Cybertek School";
+		String name2 = s2.substring(0,8);
+		System.out.println(name2);
+
+		String name3 = s2.substring(9);
+		System.out.println(name3);
+
+		System.out.println("-----------------------------");
+		String sentence2 = "Java is a programming language";
+		String name4 = sentence2.substring(10);
+		System.out.println(name4);
 	}
 }
