e1960676f6d8902bd836ad510e361581d32b60d4
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-27 16:51:44 -0700

    done with substring method practice

---
+++ Day15_Strings/SubstringPractice.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SubstringPractice {
+	public static void main(String[] args){
+
+		String sentence = "My favorite TV series is The Walking Dead";
+		String tvSeries = sentence.substring(25);
+
+		System.out.println(tvSeries);
+	}
+}
