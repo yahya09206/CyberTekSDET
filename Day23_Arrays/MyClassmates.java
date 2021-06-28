a93c95a4a7e993ef10d4b97ac26cad672bc8b46a
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 09:57:51 -0700

    printing out arrays values using for loops

--- Day23_Arrays/MyClassmates.java
+++ Day23_Arrays/MyClassmates.java
@@ -4,8 +4,20 @@ public class MyClassmates {
 
 		// If we know what the elements are
 		String[] friends = {"Sabit", "Ahmet", "Tugba", "Beryl", "Ahmet"};
+		System.out.println("Size: " + friends.length);
 
+		for(int i = 0; i <= friends.length - 1; i++){
+			System.out.println(friends[i] + " ");
+		}
+
+
+		System.out.println("--------------------------------------");
 		// If we don't know what the elements are that we want to store
 		String[] friends2 = new String[5];
+		System.out.println("Size: " + friends2.length);
+
+		for(int i = 0; i <= friends2.length - 1; i++){
+			System.out.println(friends2[i]);
+		}
 	}
 }
