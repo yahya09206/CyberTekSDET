59b1f171f699fc5e768a635c141cd876f89e3fbf
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 09:51:00 -0700

    building an array using new keyword

--- Day23_Arrays/MyClassmates.java
+++ Day23_Arrays/MyClassmates.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 public class MyClassmates {
 	public static void main(String[] args){
 
-		
+		// If we know what the elements are
+		String[] friends = {"Sabit", "Ahmet", "Tugba", "Beryl", "Ahmet"};
+
+		// If we don't know what the elements are that we want to store
+		String[] friends2 = new String[5];
 	}
 }
