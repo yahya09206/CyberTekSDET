fe204ff5d6c49d8205924ad394ebb4ef6426ddcf
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 12:13:34 -0700

    task to find avergae number

--- Day23_Arrays/MaximumNumber.java
+++ Day23_Arrays/MaximumNumber.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class MaximumNumber {
 	public static void main(String[] args){
 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("How many numbers do you want to enter? ");
 	}
 }
