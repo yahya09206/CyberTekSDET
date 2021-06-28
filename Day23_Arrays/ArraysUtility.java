e71f0d555cdfcd7c42d525293cda6fb00ed044c0
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 12:45:37 -0700

    array is printing

--- Day23_Arrays/ArraysUtility.java
+++ Day23_Arrays/ArraysUtility.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class ArraysUtility {
 	public static void main(String[] args){
 		
+		//toString - used when you want to print entire array
+		int[] array = {1,2,3,4,5,6};
+		System.out.println(array);
+
+		//Formatted array values
+		System.out.println(Arrays.toString(array));
 	}
 }
