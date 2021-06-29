215cc5ab5ee23d593b8d1018ed70cda1d825187f
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 19:46:14 -0700

    taking first letters out of both words

---
+++ Day16_StringContinued/StringPractice2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class StringPractice2 {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter two words: ");
+		String word1 = scan.next();
+		String word2 = scan.next();
+
+		String result = word1.substring(1) + word2.substring(1);
+		System.out.println(result);
+	}
+}
