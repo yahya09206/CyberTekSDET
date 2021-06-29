f42cf1a6abbae23a0c277d5dd4f1ed068c564bce
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 20:14:46 -0700

    done with replace method

--- Day16_StringContinued/StringMethods.java
+++ Day16_StringContinued/StringMethods.java
@@ -4,5 +4,9 @@ public class StringMethods {
 		String email = "Cybertek@yahoo.com";
 		email = email.replace("yahoo", "gmail");
 		System.out.println(email);
+
+		String sentence = "C# is cool, C# is fun";
+		sentence = sentence.replace("C#", "Java");
+		System.out.println(sentence);
 	}
 }
