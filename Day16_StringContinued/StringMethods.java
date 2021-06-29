7f08e0f7db756d3a88b37ff1e6e63cfca9c3cb32
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 20:13:44 -0700

    replace method practice

--- Day16_StringContinued/StringMethods.java
+++ Day16_StringContinued/StringMethods.java
@@ -1,6 +1,8 @@
-import java.util.Scanner;
 public class StringMethods {
 	public static void main(String[] args){
-		
+
+		String email = "Cybertek@yahoo.com";
+		email = email.replace("yahoo", "gmail");
+		System.out.println(email);
 	}
 }
