943fc9b5763a1534005abd5f69cb73fda03e749c
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 20:03:57 -0700

    task to check if 3 words are equal in length

---
+++ Day16_StringContinued/StringPractice4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class StringPractice4 {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter three words: ");
+		String word1 = scan.next();
+		String word2 = scan.next();
+		String word3 = scan.next();
+	}
+}
