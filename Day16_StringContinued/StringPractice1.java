014b24aa345c5b6ad1699d15181c6ae48778f0fd
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 19:37:37 -0700

    switched to using ternary op

--- Day16_StringContinued/StringPractice1.java
+++ Day16_StringContinued/StringPractice1.java
@@ -6,6 +6,12 @@ public class StringPractice1 {
 		System.out.println("Enter a string: ");
 		String word = scan.next();
 
+		//using ternary operator
+		String result = (word.length() == 0)? "String is empty"
+						:(word.length() > 3)? word.substring(word.length() - 3)
+						: word;
+
+
 		if(word.length() == 0){
 			System.out.println("String is empty!");
 		}else if(word.length() > 3){
