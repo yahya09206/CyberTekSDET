fef33c3260fe5c6e44a8e2142336ac9806a3fc75
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 19:32:48 -0700

    string continued

---
+++ Day16_StringContinued/StringPractice1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class StringPractice1 {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String word = scan.next();
+	}
+}
