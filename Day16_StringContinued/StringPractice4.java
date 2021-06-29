10ceb6778f5b8f9edd595ebf1e04f427006f64e3
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 20:08:27 -0700

    done with checking if 3 words are same length

--- Day16_StringContinued/StringPractice4.java
+++ Day16_StringContinued/StringPractice4.java
@@ -7,5 +7,16 @@ public class StringPractice4 {
 		String word1 = scan.next();
 		String word2 = scan.next();
 		String word3 = scan.next();
+
+		boolean allSame = word1.length() == word2.length() && word1.length() == word3.length();
+		boolean notSame = word1.length() != word2.length() && word2.length() != word3.length() && word1.length() == word3.length();
+		///precondition
+		if(allSame){
+			System.out.println("All words are the same");
+		}else if(notSame) {
+			System.out.println("All different length");
+		}else {
+			System.out.println("Not same nor different lengths");
+		}
 	}
 }
