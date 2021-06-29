8ffc66fc14430e51f57ce21da1cbb3394793b1d0
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 20:38:43 -0700

    done with index of method

--- Day16_StringContinued/EmailDomain.java
+++ Day16_StringContinued/EmailDomain.java
@@ -1,5 +1,12 @@
 public class EmailDomain {
 	public static void main(String[] args){
-		
+
+		String email = "Cybertek@yahoo.com";
+
+		int beginningIndex = email.indexOf("@") + 1;
+		int endingIndex = email.indexOf(".");
+		String domain = email.substring(beginningIndex, endingIndex);
+
+		System.out.println("domain = " + domain);
 	}
 }
