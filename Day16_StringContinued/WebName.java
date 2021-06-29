c2095b98ee707ac45fdaca4a0c03a1b4397f0b2d
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-28 20:45:46 -0700

    practice with isEmpty method

---
+++ Day16_StringContinued/WebName.java
@@ -0,0 +1,8 @@
+public class WebName {
+	public static void main(String[] args){
+
+		String url = "www.amazon.com";
+		String name = url.substring(url.indexOf(".") + 1, url.lastIndexOf("."));
+		System.out.println(name);
+	}
+}
