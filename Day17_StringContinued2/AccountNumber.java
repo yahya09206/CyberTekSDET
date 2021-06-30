570b4d45cd69aad2e2fadb2588d2a3ba34bdb153
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-29 19:35:25 -0700

    task for user entering proper account number

---
+++ Day17_StringContinued2/AccountNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AccountNumber {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your account number: ");
+		String account = scan.next();
+
+		if(account.length() == ? || account.length() == 10){
+			
+		}
+	}
+}
