8df82f55f27074609cbeb05f056879327ec8f9e8
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-29 19:47:49 -0700

    set up prompt to ask for users name

--- Day17_StringContinued2/Initial.java
+++ Day17_StringContinued2/Initial.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 public class Initial {
 	public static void main(String[] args){
-		
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your first name: ");
+		String firstName = scan.next();
+		System.out.println("Enter your last name: ");
+		String lastName = scan.next();
 	}
 }
