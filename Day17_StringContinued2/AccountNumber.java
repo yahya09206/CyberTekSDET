64281850eaecebe56ff47cbb7c393c2aad4b9f95
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-29 19:38:08 -0700

    check for acount starting with five with length of ten complete

--- Day17_StringContinued2/AccountNumber.java
+++ Day17_StringContinued2/AccountNumber.java
@@ -8,8 +8,14 @@ public class AccountNumber {
 
 		if(account.length() == 7 || account.length() == 10){
 			if(account.charAt(0) == '2' && account.length() == 7){
-				System.out.println("Valid account");
+				System.out.println("Valid Account Number");
+			}else if(account.charAt(0) == '5' && account.length() == 10){
+				System.out.println("Valid Account Number");
+			}else {
+				System.out.println("Invalid");
 			}
+		}else {
+			System.out.println("Invalid Account Number")
 		}
 	}
 }
