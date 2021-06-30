bfaf980ff1ce21a6a159c536498efcb162f8e267
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-29 20:03:08 -0700

    task to ask for full name of user at once and print initials

---
+++ Day17_StringContinued2/Initial2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Initial2 {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter your full name");
+	}
+}
