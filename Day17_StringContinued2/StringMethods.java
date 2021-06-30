5c5337d7906f5a4ce2c4da64e3b6b2552cb4e0be
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-29 20:33:18 -0700

    checking if string has specific word using contains method

--- Day17_StringContinued2/StringMethods.java
+++ Day17_StringContinued2/StringMethods.java
@@ -1,6 +1,10 @@
 public class StringMethods {
 	public static void main(String[] args){
 
-		
+		String sentence = "My favorite programming languages are: C#, Java, JavaScript";
+		boolean hasPython = sentence.contains("Python");
+		System.out.println(hasPython);
+		boolean hasJava = sentence.contains("Java");
+		System.out.println(hasJava);
 	}
 }
