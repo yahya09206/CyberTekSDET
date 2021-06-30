d13daa544a29af4c5e6ba453116c86a6c6e113d1
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-29 20:19:26 -0700

    prompt for username

--- Day17_StringContinued2/Login.java
+++ Day17_StringContinued2/Login.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 public class Login {
 	public static void main(String[] args){
-		
+
+		String correctUserName = "Cybertek@gmail.com";
+		String correctPassword = "Cybertek123";
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your username: ");
+		String userName = scan.next();
 	}
 }
