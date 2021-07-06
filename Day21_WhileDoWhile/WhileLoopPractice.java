445679a5f898192083186b8eddec84e3889ea176
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-05 20:03:41 -0700

    program to sum numbers using while loop

--- Day21_WhileDoWhile/WhileLoopPractice.java
+++ Day21_WhileDoWhile/WhileLoopPractice.java
@@ -1,7 +1,8 @@
 public class WhileLoopPractice {
 	public static void main(String[] args){
 
-		String sentence = "Java is fun, Java is cool";
+		String sentence = "Java Java Java Java";
+		String originalSentence = sentence; // to keep original sentence
 		int countJava = 0;
 
 		// if statement only does once
