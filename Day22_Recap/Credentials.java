0ed4636016404e1f532f1dd172f442e1283d7934
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-05 20:29:34 -0700

    set up conditional to check if a match or not

--- Day22_Recap/Credentials.java
+++ Day22_Recap/Credentials.java
@@ -12,5 +12,12 @@ public class Credentials {
 		System.out.println("Enter your password: ");
 		String p = scan.next();
 
+		if(u.equals(correctUsername) && p.equals(correctPassword)){
+			System.out.println("Logged In");
+		}else {
+			System.out.println("username or password is incorrect, please re-enter");
+			System.out.println("Enter your username:");
+		}
+
 	}
 }
