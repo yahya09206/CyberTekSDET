1ebb61fd4532e41a076f2d2aed500b5fea7502ed
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-05 20:27:10 -0700

    import scanner class and declared correct credential variables

--- Day22_Recap/Credentials.java
+++ Day22_Recap/Credentials.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Credentials {
 	public static void main(String[] args){
-		
+
+		String correctUsername = "cybertek";
+		String correctPassword = "cybertek12345";
+
 	}
 }
