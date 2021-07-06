f249d97f94cd51fcca61654788d21587ea047a25
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-05 20:19:07 -0700

    switched to printing out only evens up to 100

--- Day21_WhileDoWhile/WhileLoopPractice2.java
+++ Day21_WhileDoWhile/WhileLoopPractice2.java
@@ -2,7 +2,11 @@ public class WhileLoopPractice2 {
 	public static void main(String[] args){
 		int i = 0;
 		while(i <= 100){
+			if(i % 2 == 0){
+				System.out.print(i + " ");
+			}
 			i++;
 		}
+		System.out.println();
 	}
 }
