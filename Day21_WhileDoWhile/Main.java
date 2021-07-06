35fcd6512338bda61b2e890312d973ff438cfbe4
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-05 19:53:14 -0700

    more basic while loop practice

--- Day21_WhileDoWhile/Main.java
+++ Day21_WhileDoWhile/Main.java
@@ -1,5 +1,14 @@
 public class Main {
 	public static void main(String[] args){
-		
+
+		if(true){
+			System.out.println("Hello batch 23");
+		}
+
+		System.out.println("--------------------------");
+		// while loop: a continous if statement
+		while(true){
+			System.out.println("Hello batch 23");
+		}
 	}
 }
