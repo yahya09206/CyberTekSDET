856913af069e1b855335f765231dcee977d6cba0
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-05 19:55:50 -0700

    using while loop to count

--- Day21_WhileDoWhile/WhileLoopPractice.java
+++ Day21_WhileDoWhile/WhileLoopPractice.java
@@ -2,5 +2,11 @@ public class WhileLoopPractice {
 	public static void main(String[] args){
 
 		String sentence = "Java is fun, Java is cool";
+		int countJava = 0;
+
+		// if statement only does once
+		while(sentence.contains("Java")){
+
+		}
 	}
 }
