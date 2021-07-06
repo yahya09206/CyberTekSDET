954a655d579a59b6d3453656191095281a8a24b5
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-05 20:07:38 -0700

    sum of numbers without for loop and break statement

--- Day21_WhileDoWhile/SumOfNumbers.java
+++ Day21_WhileDoWhile/SumOfNumbers.java
@@ -3,14 +3,23 @@ public class SumOfNumbers {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
+		int sum = 0;
+		int n = 0;
+		// while(true){
+		// 	System.out.println("Enter a number: ");
+		// 	int num = scan.nextInt();
 
-		while(true){
+		// 	if(n < 0){
+		// 		break; //exits loop
+		// 	}
+
+		// 	sum += n;
+		// }
+		// System.out.println("Sum = " sum);
+
+		while(n >= 0){
 			System.out.println("Enter a number: ");
 			int num = scan.nextInt();
-
-			if(n < 0){
-				break; //exits loop
-			}
 
 			sum += n;
 		}
