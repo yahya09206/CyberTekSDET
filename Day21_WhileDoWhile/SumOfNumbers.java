c000c8742b206f7f8745a8672ea1361606ce6220
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-05 20:04:52 -0700

    setting up while loop to take input of numbers

---
+++ Day21_WhileDoWhile/SumOfNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SumOfNumbers {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+
+		while(true){
+			System.out.println("Enter a number: ");
+			int num = scan.nextInt();
+
+			
+		}
+	}
+}
