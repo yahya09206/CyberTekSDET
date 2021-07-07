d72b311b24278dab1c6a2723c0c665e15c96fc99
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-06 21:57:32 -0700

    imported scanner and declared needed variables

--- Day22_Recap/Calculator.java
+++ Day22_Recap/Calculator.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Calculator {
 	public static void main(String[] args){
-		
+
+		Scanner scan = new Scanner(System.in);
+		int num1;
+		int num2;
+		char operator;
 	}
 }
