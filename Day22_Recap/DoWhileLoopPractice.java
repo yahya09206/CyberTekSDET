d46093812a555b9e86f9c3b2421661a5a0e3ff10
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-06 20:50:52 -0700

    provided check to see if num is divisible by 2 equals 0

--- Day22_Recap/DoWhileLoopPractice.java
+++ Day22_Recap/DoWhileLoopPractice.java
@@ -1,5 +1,14 @@
 public class DoWhileLoopPractice {
 	public static void main(String[] args){
-		
+
+		int i = 0;
+		int sum = 0;
+		do {
+			if(i % 2 == 0){
+				sum += i;
+			}
+			System.out.println(i);
+			i++;
+		}while(i <= 10);
 	}
 }
