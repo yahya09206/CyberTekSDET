828a5d05dd4133f85fff4523f8709e8be5c30ce6
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-06 20:41:49 -0700

    comparison of while and do while loop

---
+++ Day22_Recap/DoWhileLoop.java
@@ -0,0 +1,15 @@
+public class DoWhileLoop {
+	public static void main(String[] args){
+
+		int i = 10;
+		while(i < 5){
+			System.out.println("Hello CyberTek");
+		}
+
+		System.out.println("-------------------------------");
+
+		do{
+			System.out.println("Hello CyberTek");
+		}while(i < 5);
+	}
+}
