46cad3ebdf6f5fd4ca533f8c5af90c24787b9d31
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-06 21:43:20 -0700

    recording both numbers

--- Day22_Recap/SumOfTwoNumbers.java
+++ Day22_Recap/SumOfTwoNumbers.java
@@ -2,5 +2,7 @@ import java.util.Scanner;
 public class SumOfTwoNumbers {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
+		int n1 = scan.nextInt();
+		int n2 = scan.nextInt();
 	}
 }
