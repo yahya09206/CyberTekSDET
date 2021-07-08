610a10e972991b5de00fdb85fc9a92f21cc6b47e
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-07 20:21:00 -0700

    set up while loop to keep re prompting user for proper answer

--- Day22_Recap/YesOrNo.java
+++ Day22_Recap/YesOrNo.java
@@ -4,5 +4,18 @@ public class YesOrNo {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Will you marry me?");
 		String answer = scan.next().toLowerCase();
+
+		if(answer.equals("yes")){
+			System.out.println("Congrats");
+		}
+
+		if(answer.equals("no")){
+			System.out.println("RIP");
+		}
+
+		if(!(answer.equals("yes") || answer.equals("no"))){
+			System.out.println("Invalid Entry, please re-enter");
+			answer.scan.next().toLowerCase();
+		}
 	}
 }
