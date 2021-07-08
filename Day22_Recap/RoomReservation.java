aeb68428c4418f593bb0d8aa65da41ced7205b95
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-07 20:28:34 -0700

    prompt user for which type of room they want

--- Day22_Recap/RoomReservation.java
+++ Day22_Recap/RoomReservation.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 public class RoomReservation {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
-		
+		int totalPrice = 0;
+
 		System.out.println("Welcome to CyberTek Hotel");
 		System.out.println("Would you like to reserve a room?");
+		String answer = scan.next().toLowerCase();
+
+		if(answer.equals("yes")){
+			System.out.println("Which bed type do you want?");
+			String
+		}
 	}
 }
