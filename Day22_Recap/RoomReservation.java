771f950fed95b3db9c79142126b6fb0e0c007e0d
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-07 20:27:23 -0700

    welcome message and ask user if they want a room

--- Day22_Recap/RoomReservation.java
+++ Day22_Recap/RoomReservation.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 public class RoomReservation {
 	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Welcome to CyberTek Hotel");
+		System.out.println("Would you like to reserve a room?");
 	}
 }
