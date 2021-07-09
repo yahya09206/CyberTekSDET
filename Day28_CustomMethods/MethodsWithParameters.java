3d470d4b9bde94a7d1eafe4e37c47e06f8970d6d
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-08 21:49:54 -0700

    condition to check if age is greater than or equal to 21

--- Day28_CustomMethods/MethodsWithParameters.java
+++ Day28_CustomMethods/MethodsWithParameters.java
@@ -3,7 +3,12 @@ public class MethodsWithParameters {
 
 	}
 
-	public static void eligible(){
-		
+	public static void eligible(int age){
+		if (age >= 21) {
+			System.out.println("eligible");
+		}else {
+			System.out.println("Not eligible");
+		}
+
 	}
 }
