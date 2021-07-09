d8fe09510a1fd721659d0834afb1ca64daf36166
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-08 20:51:47 -0700

    starting without methods

--- Day28_CustomMethods/PracticeWithoutMethod.java
+++ Day28_CustomMethods/PracticeWithoutMethod.java
@@ -1,5 +1,20 @@
 public class PracticeWithoutMethod {
 	public static void main(String[] args){
-		
+
+		//Step 3
+		for(int i = 0; i < 5; i++){
+			System.out.println("Hello");
+		}
+
+		//Step 2
+		System.out.println("Muhtar");
+
+		//Step 3
+		for(int i = 0; i < 5; i++){
+			System.out.println("Hello");
+		}
+
+		//Step 4
+		System.out.println("");
 	}
 }
