0b9b26a3123d891346ed0832f48b302831e3fd11
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-08 22:00:16 -0700

    for loop with condition to check if number is odd

--- Day28_CustomMethods/VoidMethodPractice.java
+++ Day28_CustomMethods/VoidMethodPractice.java
@@ -7,7 +7,9 @@ public class VoidMethodPractice {
 	public static void printOddNumbers(int from, int to){
 
 		for(int i = from; i <= to; i++){
-			
+			if(i % 2 !=){
+				System.out.print(i + " ");
+			}
 		}
 	}
 }
