4344d5779c6b8a0af0951b3f236bbe7b92f0d707
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-08 22:01:27 -0700

    calling method with different param inputs

--- Day28_CustomMethods/VoidMethodPractice.java
+++ Day28_CustomMethods/VoidMethodPractice.java
@@ -1,15 +1,15 @@
 public class VoidMethodPractice {
 	public static void main(String[] args){
 
-
+		printOddNumbers(1, 150);
 	}
 
 	public static void printOddNumbers(int from, int to){
-
 		for(int i = from; i <= to; i++){
-			if(i % 2 !=){
+			if(i % 2 != 0){
 				System.out.print(i + " ");
 			}
 		}
+		System.out.println();
 	}
 }
