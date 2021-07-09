0bd90271b2670a167da6b0d09b1b91e71cc35f29
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-08 21:00:10 -0700

    called method in main method

--- Day28_CustomMethods/CustomMethodIntro.java
+++ Day28_CustomMethods/CustomMethodIntro.java
@@ -1,11 +1,12 @@
 public class CustomMethodIntro {
 	public static void main(String[] args){
 
+		printHello5Times();
 
 	}
 
 	public static void printHello5Times(){
-		
+
 		for(int i = 0; i < 5; i++){
 			System.out.println("Hello");
 		}
