094c2e5cfe6028112276f8b421406fa1c9b465b6
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 20:09:08 -0700

    condition to check if element has been seen before

--- Day24_ArraysContinued/FrequencyOfElements.java
+++ Day24_ArraysContinued/FrequencyOfElements.java
@@ -4,5 +4,11 @@ public class FrequencyOfElements {
 		int[] arr = {1,1,2,3,3,4};
 		int element = arr[0];
 		int count = 0;
+
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] == element){ //if the element occurs in the array
+				count++;
+			}
+		}
 	}
 }
