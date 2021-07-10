6aa59af1aa0f0eb0c9c7a218aad46b2a60a0c1e6
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 19:36:05 -0700

    creeated array with names

--- Day24_ArraysContinued/ClassMates.java
+++ Day24_ArraysContinued/ClassMates.java
@@ -1,5 +1,6 @@
 public class ClassMates {
 	public static void main(String[] args){
-		
+
+		String[] classmates = {"Gulistan", "Fruza", "Memet", "Muhtar", "Tamara", "Abbos", "Shirin", "Robinson", "Shirin"};
 	}
 }
