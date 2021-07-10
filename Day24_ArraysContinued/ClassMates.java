ab00d166f51ab1991626481ae4de6c15c00d162c
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 19:40:51 -0700

    added comments

--- Day24_ArraysContinued/ClassMates.java
+++ Day24_ArraysContinued/ClassMates.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class ClassMates {
 	public static void main(String[] args){
 
+		//Print first 3 characters of elements
 		String[] classmates = {"Gulistan", "Fruza", "Memet", "Muhtar", "Tamara", "Abbos", "Shirin", "Robinson", "Shirin"};
 
 		System.out.println(Arrays.toString(classmates));
