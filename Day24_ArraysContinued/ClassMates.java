928a0eb60797518a46d70223357e0d140ef7e9ec
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 19:38:51 -0700

    had to import arrays class

--- Day24_ArraysContinued/ClassMates.java
+++ Day24_ArraysContinued/ClassMates.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 public class ClassMates {
 	public static void main(String[] args){
 
 		String[] classmates = {"Gulistan", "Fruza", "Memet", "Muhtar", "Tamara", "Abbos", "Shirin", "Robinson", "Shirin"};
 
 		System.out.println(Arrays.toString(classmates));
-		
+
+
 	}
 }
