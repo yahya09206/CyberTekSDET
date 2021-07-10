9decdb2ad1f69e8aef36d5f0aa9261f0d242773c
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 19:37:34 -0700

    trying to print out the elements inside of array

--- Day24_ArraysContinued/ClassMates.java
+++ Day24_ArraysContinued/ClassMates.java
@@ -2,5 +2,8 @@ public class ClassMates {
 	public static void main(String[] args){
 
 		String[] classmates = {"Gulistan", "Fruza", "Memet", "Muhtar", "Tamara", "Abbos", "Shirin", "Robinson", "Shirin"};
+
+		System.out.println(Arrays.toString(classmates));
+		
 	}
 }
