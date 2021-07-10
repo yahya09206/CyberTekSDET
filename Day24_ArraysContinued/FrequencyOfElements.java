5819a0b5aabe887db289bb90043ce42ddb455366
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 20:11:58 -0700

    unique elements are printing

--- Day24_ArraysContinued/FrequencyOfElements.java
+++ Day24_ArraysContinued/FrequencyOfElements.java
@@ -3,15 +3,14 @@ public class FrequencyOfElements {
 	public static void main(String[] args){
 		int[] arr = {1,1,2,3,3,4};
 
-		for(int j = 0; j < arr.length; i++){
-			int element = arr[0];
+		for(int j = 0; j < arr.length; j++){
+			int element = arr[j];
 			int count = 0; //store frequency of element
 			for(int i = 0; i < arr.length; i++){
 				if(arr[i] == element){ //if the element occurs in the array
 					count++;
 				}
 			}
-
 			if(count == 1){//to check if element is unique
 				System.out.println(element);
 			}
