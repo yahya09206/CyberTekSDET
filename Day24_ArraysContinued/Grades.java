6259b266301a65ee1f959f8407c037468d950ceb
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 19:43:09 -0700

    declared neccesary variables

--- Day24_ArraysContinued/Grades.java
+++ Day24_ArraysContinued/Grades.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 public class Grades {
 	public static void main(String[] args){
-		
+
+		String[] names = {"Anna", "Nancy", "Sarah"};
+		int [] scores = {90, 75, 80};
+		char[] grades = new char[names.length];
 	}
 }
