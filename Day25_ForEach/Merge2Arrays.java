a5487c6806e571f7a4e2dd49692fcecfb79f93ff
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-10 13:12:38 -0700

    made sure new array will have the same amount of space as the other two combined

--- Day25_ForEach/Merge2Arrays.java
+++ Day25_ForEach/Merge2Arrays.java
@@ -4,6 +4,6 @@ public class Merge2Arrays {
 		int[] arr1 = {1,2,3,4},
 				arr2 = {5,6};
 
-		int[] arr3 = 
+		int[] arr3 = new int[arr.length + arr2.length]; //Adding length of both arrays so it can be the same size of the two added together
 	}
 }
