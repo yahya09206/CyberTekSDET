c7b30bf445026919ebf2f29e45db87bc95e8fda5
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 20:11:17 -0700

    nested first for loop inside another loop to keep track of elements

--- Day24_ArraysContinued/FrequencyOfElements.java
+++ Day24_ArraysContinued/FrequencyOfElements.java
@@ -2,12 +2,18 @@ import java.util.Arrays;
 public class FrequencyOfElements {
 	public static void main(String[] args){
 		int[] arr = {1,1,2,3,3,4};
-		int element = arr[0];
-		int count = 0;
 
-		for(int i = 0; i < arr.length; i++){
-			if(arr[i] == element){ //if the element occurs in the array
-				count++;
+		for(int j = 0; j < arr.length; i++){
+			int element = arr[0];
+			int count = 0; //store frequency of element
+			for(int i = 0; i < arr.length; i++){
+				if(arr[i] == element){ //if the element occurs in the array
+					count++;
+				}
+			}
+
+			if(count == 1){//to check if element is unique
+				System.out.println(element);
 			}
 		}
 	}
