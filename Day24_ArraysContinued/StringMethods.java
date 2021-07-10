daafb4dfcd1481a4d4a1d1c326610ae78061e251
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 20:40:44 -0700

    printing out sentence inside of array

--- Day24_ArraysContinued/StringMethods.java
+++ Day24_ArraysContinued/StringMethods.java
@@ -1,7 +1,12 @@
 import java.util.Arrays;
 public class StringMethods {
 	public static void main(String[] args){
+		String sentence = "I love learning java programming";
 
+		//Split sentence by spaces
+		String[] words = sentence.split(" ");
+		//Print array in regular order
+		System.out.println(Arrays.toString(words));
 	}
 }
 /*
