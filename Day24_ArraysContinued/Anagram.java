41b4548011627657aa316fded8623af0b53bbcc0
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 20:59:51 -0700

    checks to see if anaggram after sorting both char arrays

--- Day24_ArraysContinued/Anagram.java
+++ Day24_ArraysContinued/Anagram.java
@@ -3,5 +3,19 @@ public class Anagram {
 	public static void main(String[] args){
 		String s1 = "heart";
 		String s2 = "earth";
+
+		char[] ch1 = s1.toCharArray();
+		char[] ch2 = s2.toCharArray();
+
+		//Sort arrays
+		Arrays.sort(ch1);
+		Arrays.sort(ch2);
+
+		System.out.println(Arrays.toString(ch1));
+		System.out.println(Arrays.toString(ch2));
+
+		boolean isAnagram = Arrays.equals(ch1, ch2);
+		System.out.println("isAnagram " + isAnagram);
+
 	}
 }
