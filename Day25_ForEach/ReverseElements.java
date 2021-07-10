0bcdbb4ee513f9d540d6d1a6b284a202a7a190cf
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-10 13:01:17 -0700

    called the reversed string outside of loop

--- Day25_ForEach/ReverseElements.java
+++ Day25_ForEach/ReverseElements.java
@@ -10,5 +10,7 @@ public class ReverseElements {
 		for (int i = element.length() - 1; i >= 0; i--) {
 			reverse += element.charAt(i);// gets the characters from the string element
 		}
+
+		array[0] = reverse;
 	}
 }
