347ec07b4b37ae05a96783be1914e04bd6fd1468
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 19:44:14 -0700

    conditions to check grade and corresponding letter

--- Day24_ArraysContinued/Grades.java
+++ Day24_ArraysContinued/Grades.java
@@ -3,7 +3,14 @@ public class Grades {
 	public static void main(String[] args){
 
 		String[] names = {"Anna", "Nancy", "Sarah"};
-		int [] scores = {90, 75, 80};
+		int[] scores = {90, 75, 80};
 		char[] grades = new char[names.length];
+
+		int score = scores[0];
+		if(score >= 90){
+			grades[0] = 'A';
+		}else if(score >=){
+			grades[0] = 'B';
+		}
 	}
 }
