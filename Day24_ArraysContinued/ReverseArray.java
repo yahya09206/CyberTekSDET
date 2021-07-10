8ed9679ab214221ccd5e8ff70db2925446135565
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 20:17:49 -0700

    arrays are printing in reverse order

--- Day24_ArraysContinued/ReverseArray.java
+++ Day24_ArraysContinued/ReverseArray.java
@@ -3,5 +3,9 @@ public class ReverseArray {
 	public static void main(String[] args){
 		int[] array = {1,2,3,4,5};
 		int[] reversedArray = new int[array.length];
+
+		for(int i = array.length-1; i >= 0; i--){
+			System.out.println(array[i]);
+		}
 	}
 }
