943eb430ae73d2f7808198673e6353603c033761
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 20:16:56 -0700

    declared array of elemtns and new array to store reversed elements

--- Day24_ArraysContinued/ReverseArray.java
+++ Day24_ArraysContinued/ReverseArray.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 public class ReverseArray {
 	public static void main(String[] args){
-		
+		int[] array = {1,2,3,4,5};
+		int[] reversedArray = new int[array.length];
 	}
 }
