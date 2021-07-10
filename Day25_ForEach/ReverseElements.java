1a75cf81170293a88eca8ef1a29e9a969fe398d7
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-10 13:05:07 -0700

    updated comments

--- Day25_ForEach/ReverseElements.java
+++ Day25_ForEach/ReverseElements.java
@@ -8,12 +8,13 @@ public class ReverseElements {
 		for(int j = 0; j < array.length; j++){
 			//Start at j
 			String element = array[j];
+			// empty string
 			String reverse = "";
-
+			//loop to reverse each element in array
 			for (int i = element.length() - 1; i >= 0; i--) {
 				reverse += element.charAt(i);// gets the characters from the string element
 			}
-
+			//assigned reversed element to the index of the array
 			array[j] = reverse;
 		}
 		System.out.println(Arrays.toString(array));
