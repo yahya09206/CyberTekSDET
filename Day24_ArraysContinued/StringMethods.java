c63ec23e16da13f2fafd928b910f8fe908914f6a
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 20:38:52 -0700

    practice with split() method

--- Day24_ArraysContinued/StringMethods.java
+++ Day24_ArraysContinued/StringMethods.java
@@ -1,6 +1,13 @@
 import java.util.Arrays;
 public class StringMethods {
 	public static void main(String[] args){
-		
+
 	}
 }
+/*
+String methods:
+	split(value): returns string array, splits the string by given value
+	example:
+		sentence = i love programming
+		sentence.split(" "); ==> {"I", "love", "Java", "programming"}
+*/
