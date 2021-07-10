321ab2d29c9e2b08cd0cbf7f0783f962102a638d
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 19:55:55 -0700

    switched to using ternary operator

--- Day24_ArraysContinued/Grades.java
+++ Day24_ArraysContinued/Grades.java
@@ -7,19 +7,22 @@ public class Grades {
 		char[] grades = new char[names.length];
 
 		for(int i = 0; i <= grades.length - 1; i++){
-			// int[] scores = scores[0];
-			if(scores[i] >= 90){
-				grades[i] = 'A';
-			}else if(scores[i] >= 80){
-				grades[i] = 'B';
-			}else if(scores[i] >= 70){
-				grades[i] = 'C';
-			}else if(scores[i] >= 60){
-				grades[i] = 'D';
-			}else {
-				grades[i] = 'F';
-			}
-			System.out.println(names[i] + ": " + scores[i] + ": " + grades[i]);
+			// // int[] scores = scores[0];
+			// if(scores[i] >= 90){
+			// 	grades[i] = 'A';
+			// }else if(scores[i] >= 80){
+			// 	grades[i] = 'B';
+			// }else if(scores[i] >= 70){
+			// 	grades[i] = 'C';
+			// }else if(scores[i] >= 60){
+			// 	grades[i] = 'D';
+			// }else {
+			// 	grades[i] = 'F';
+			// }
+		//Using ternary operator
+		grades[i] = (scores[i] >= 90)? 'A' :(scores[i] >= 80)? 'B' :(scores[i] >= 70)? 'C' :(scores[i] >= 60)? 'D' :'F';
+		System.out.println(names[i] + ": " + scores[i] + ": " + grades[i]);
 		}
+
 	}
 }
