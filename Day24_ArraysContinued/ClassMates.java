0bf5e26dbd166c9818071f4d34941eef1a6f7c21
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-09 19:42:05 -0700

    program to print grades and store in new array

--- Day24_ArraysContinued/ClassMates.java
+++ Day24_ArraysContinued/ClassMates.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class ClassMates {
 	public static void main(String[] args){
---
+++ Day24_ArraysContinued/Grades.java
@@ -0,0 +1,6 @@
+import java.util.Arrays;
+public class Grades {
+	public static void main(String[] args){
+		
+	}
+}
