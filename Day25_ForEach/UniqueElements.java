f1e84ccc6e957e4eb8d4579602077bd3beefbcae
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-12 20:57:54 -0700

    updated comments

--- Day25_ForEach/UniqueElements.java
+++ Day25_ForEach/UniqueElements.java
@@ -3,6 +3,7 @@ public class UniqueElements {
 	public static void main(String[] args){
 		String[] words = {"java", "java", "python", "c#"};
 
+		//iterate thru array
 		for (String each : words) {
 			int count = 0;
 			for (String word : words) {
