4f75e352f2260936d2f61a6ed980ef437c579a48
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-12 20:22:01 -0700

    printing out array values using foreach loop

--- Day25_ForEach/ForEachLoopIntro.java
+++ Day25_ForEach/ForEachLoopIntro.java
@@ -1,6 +1,17 @@
 import java.util.Arrays;
 public class ForEachLoopIntro {
 	public static void main(String[] args){
-		
+
+		int[] numbers = {1,2,3,4,5};
+
+		for(int i = 0; i < numbers.length; i++){
+			System.out.println(numbers[i]);
+		}
+
+		System.out.println("----------------------------");
+		//For each loop already does the iteration for you that starts from 0 to last index(fixed). can't use to reverse values. can only use with data structures
+		for (int i : numbers ) {
+			System.out.print(i);
+		}
 	}
 }
