86ba1766f6110904afb43f3a7cb494689cef5f38
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-12 20:45:33 -0700

    done reverse

--- Day25_ForEach/ReverseElement2.java
+++ Day25_ForEach/ReverseElement2.java
@@ -1,6 +1,14 @@
 import java.util.Arrays;
 public class ReverseElement2 {
 	public static void main(String[] args){
-		
+
+		String[] array = {"Java", "Python", "C#"};
+		for (String element : array) {
+			String reverse = "";
+			for (int i = element.length() - 1; i >= 0; i--) {
+				reverse += element.charAt(i);// gets the characters from the string element
+			}
+			System.out.println(reverse);
+		}
 	}
 }
