00ab3ae9b313cc5cd0ded08f0c5c1581171448ec
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-12 20:37:59 -0700

    merged arrays with foreach are printing

--- Day25_ForEach/ForEachLoopPractice.java
+++ Day25_ForEach/ForEachLoopPractice.java
@@ -2,7 +2,20 @@ import java.util.Arrays;
 public class ForEachLoopPractice {
 	public static void main(String[] args){
 
-		String[] arr1 = {"A", "B", "C", "D"};
+		String[] arr1 = {"A", "B", "C", "D"},
 				 arr2 = {"E", "F"};
+		String[] arr3 = new String[arr1.length + arr2.length];
+
+		int j = 0;
+		//one foreach per data structure
+		for(String each : arr1){
+			arr3[j++] = each;
+		}
+
+		for (String each : arr2) {
+			arr3[j++] = each;
+		}
+
+		System.out.println(Arrays.toString(arr3));
 	}
 }
