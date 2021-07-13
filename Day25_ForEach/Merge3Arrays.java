ba866056af5b19db4f87f398bc6fd0de3886d70a
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-12 20:06:22 -0700

    declared 3 arrays and their values

--- Day25_ForEach/Merge3Arrays.java
+++ Day25_ForEach/Merge3Arrays.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
 public class Merge3Arrays {
 	public static void main(String[] args){
+		int[] arr1 = {1,2,3,11};
+		int[] arr2 = {4,5,6};
+		int[] arr3 = {7,8,9,12,13,14};
+
 		
 	}
 }
