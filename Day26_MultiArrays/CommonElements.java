ae8b5082e8e4510f91d9cafc7128a8a0dbd941ca
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-13 20:43:12 -0700

    check for one known element

--- Day26_MultiArrays/CommonElements.java
+++ Day26_MultiArrays/CommonElements.java
@@ -5,6 +5,11 @@ public class CommonElements {
 		int[] arr1 = {1,2,3,4,5};
 		int[] arr2 = {4,5,6,7,8,9,10,11};
 
-		
+		//iterate thru first array
+		for (int each : arr1 ) {
+			if (each == 4) {
+				System.out.println(each);
+			}
+		}
 	}
 }
