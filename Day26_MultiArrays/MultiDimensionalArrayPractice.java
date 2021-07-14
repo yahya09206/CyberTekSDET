c6672ccc18a6593712afbb42730494ed92a810a4
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-13 20:51:34 -0700

    declared 2d array

--- Day26_MultiArrays/MultiDimensionalArrayPractice.java
+++ Day26_MultiArrays/MultiDimensionalArrayPractice.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 public class MultiDimensionalArrayPractice {
 	public static void main(String[] args){
-		
+
+		int[][] arr2D = {{1,2,3}, {4,5,6,7,8}, {9,10,11,12,12}};
 	}
 }
