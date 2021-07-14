1a8ce2c75e23346cd19aa8ea3f74ddb004fdb4d3
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-13 20:45:10 -0700

    using regular for loop to iterate

--- Day26_MultiArrays/CommonElements.java
+++ Day26_MultiArrays/CommonElements.java
@@ -5,11 +5,16 @@ public class CommonElements {
 		int[] arr1 = {1,2,3,4,5};
 		int[] arr2 = {4,5,6,7,8,9,10,11};
 
-		//iterate thru first array
-		for (int each : arr1 ) {
-			if (each == 4) {
-				System.out.println(each);
+		for (int element : arr2 ) {
+			//iterate thru first array
+			for (int each : arr1 ) {
+				if (each == element) {
+					System.out.println(each);
+				}
 			}
 		}
+
+		System.out.println("----------------------------------------");
+		
 	}
 }
