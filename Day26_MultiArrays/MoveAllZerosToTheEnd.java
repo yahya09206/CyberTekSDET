e577f5ad9d16b5e1e414db1d8708153a4ba31d37
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-13 19:34:06 -0700

    new array is being printed

--- Day26_MultiArrays/MoveAllZerosToTheEnd.java
+++ Day26_MultiArrays/MoveAllZerosToTheEnd.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
 public class MoveAllZerosToTheEnd {
 	public static void main(String[] args){
-		
+
+		int[] array = {10,0,5,0,1,0};
+		int[] result = new int[array.length];
+
+		System.out.println(Arrays.toString(result));
 	}
 }
