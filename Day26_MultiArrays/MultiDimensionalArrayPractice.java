42ef707e9e395628b5037b479444fe635bab3150
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-13 20:57:02 -0700

    array is print last elements first

--- Day26_MultiArrays/MultiDimensionalArrayPractice.java
+++ Day26_MultiArrays/MultiDimensionalArrayPractice.java
@@ -2,6 +2,15 @@ import java.util.Arrays;
 public class MultiDimensionalArrayPractice {
 	public static void main(String[] args){
 
-		int[][] arr2D = {{1,2,3}, {4,5,6,7,8}, {9,10,11,12,12}};
+		int[][] arr2D = {{1,2,3}, {4,5,6,7,8}, {9,10,11,12,13}};
+
+		//iterate thru whole array from right to left
+		for (int i = arr2D.length - 1; i >= 0; i--) {
+			//iterate thru inner arrays
+			for (int j = 0; j < arr2D[i].length; j++) { //j = each element index
+				System.out.print(arr2D[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
