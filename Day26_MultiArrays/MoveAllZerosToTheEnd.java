aa48f1c9fc57373afd9d94430e4e99d75adaf912
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-13 19:40:26 -0700

    using regular for loop to do same

--- Day26_MultiArrays/MoveAllZerosToTheEnd.java
+++ Day26_MultiArrays/MoveAllZerosToTheEnd.java
@@ -5,10 +5,15 @@ public class MoveAllZerosToTheEnd {
 		int[] array = {10,0,5,0,1,0};
 		int[] result = new int[array.length];
 
-		int i = 0;
-		for(int each : array){
-			if(each != 0){
-				result[i++] = each;
+		//int i = 0;
+		// for(int each : array){
+		// 	if(each != 0){
+		// 		result[i++] = each;
+		// 	}
+		// }
+		for (int i = 0, j = 0; i < array.length; i++) {
+			if (array[i] != 0) {
+				result[j++] = array[i];
 			}
 		}
 		System.out.println(Arrays.toString(result));
