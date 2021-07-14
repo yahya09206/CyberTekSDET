51f9e449110f5fab47c217bd19ae653478bdcba3
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-13 20:18:52 -0700

    printing second element from 3d array

--- Day26_MultiArrays/MultiDimensionalArray2.java
+++ Day26_MultiArrays/MultiDimensionalArray2.java
@@ -2,7 +2,9 @@ import java.util.Arrays;
 public class MultiDimensionalArray2 {
 	public static void main(String[] args){
 
-		int[][][] nums = {{{1,2,3}, {4,5}}, {{6,7,8}, {9,10}}};
+		int[][][] arr3D = { {{1,2,3}, {4,5}}, {{6,7,8},{9,10}} };
+
+		System.out.println(Arrays.deepToString(arr3D[1]));
 
 	}
 }
