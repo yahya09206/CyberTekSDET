5ca895060336eae04ec553eae238a45e0739ff57
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-13 20:19:27 -0700

    printing whole 3d array

--- Day26_MultiArrays/MultiDimensionalArray2.java
+++ Day26_MultiArrays/MultiDimensionalArray2.java
@@ -4,6 +4,7 @@ public class MultiDimensionalArray2 {
 
 		int[][][] arr3D = { {{1,2,3}, {4,5}}, {{6,7,8},{9,10}} };
 
+		System.out.println(Arrays.deepToString(arr3D));
 		System.out.println(Arrays.deepToString(arr3D[1]));
 
 	}
