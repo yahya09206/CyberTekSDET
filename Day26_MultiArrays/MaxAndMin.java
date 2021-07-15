19b01f9247fd153333a660145a04412e009b5045
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-14 21:02:19 -0700

    for each loop then checking if element is greater than current number

--- Day26_MultiArrays/MaxAndMin.java
+++ Day26_MultiArrays/MaxAndMin.java
@@ -5,7 +5,14 @@ public class MaxAndMin {
 		int[][] numbers = {{10,20,30}, {40,50,60,70,80}};
 
 		int max = int[0][0];
-		
+
+		for (int[] each1D : numbers ) {//reps each of the 1d array in 2d array
+			for (int element : each1D ) {
+				if (element > max) {
+					max = element;
+				}
+			}
+		}
 		System.out.println("---------------------------------");
 	}
 }
