d84baa5fa18882e021b877438753856f22a739d2
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-15 20:09:28 -0700

    init new two element array

---
+++ Day27_Recap/ArraysPractice.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class ArraysPractice {
+	public static void main(String[] args){
+		//declare array
+		int[] array = new int[2];
+		array[0] = 10;
+		array[1] = 20;
+	}
+}
