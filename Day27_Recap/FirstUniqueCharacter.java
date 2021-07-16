7868ad0704adb182cb687f0502c70ab721edda96
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-15 20:47:28 -0700

    first unique char is printing

--- Day27_Recap/FirstUniqueCharacter.java
+++ Day27_Recap/FirstUniqueCharacter.java
@@ -2,11 +2,12 @@ import java.util.Arrays;
 public class FirstUniqueCharacter {
 	public static void main(String[] args){
 
-		String str = "aaaaabccddefggggh";
+		String str = "aaaaab ccddefggggh";
 
-		for (char each : str.toCharArray()) {
+		for (char each : str.replace(" ", "").toCharArray()) {
 			if (str.indexOf(each) == str.lastIndexOf(each)) {
-				
+				System.out.println(each);
+				break;
 			}
 		}
 	}
