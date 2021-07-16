f90ac938c312a3c5ca9ffcedbf683e2a8e74c588
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-15 22:06:56 -0700

    combining two arrays using copyOf method

--- Day27_Recap/ArraysUtility.java
+++ Day27_Recap/ArraysUtility.java
@@ -1,6 +1,25 @@
 import java.util.Arrays;
 public class ArraysUtility {
 	public static void main(String[] args){
-		
+
+		int[] nums = {1,2,3};
+		int[] number = Arrays.copyOf(nums, 5);
+		System.out.println(Arrays.toString(number));
+
+		System.out.println("------------------------------------");
+		//Loop to merge two arrays using one loop
+		int[] arr1 = {1,2,3,4};
+		int[] arr2 = {5,6,7,8,9};
+
+		//int[] arr3 = new int[arr1.length + arr2.length];
+		int[] arr3 = Arrays.copyOf(arr1, arr1.length + arr2.length);
+		System.out.println(Arrays.toString(arr3));
+
+		int i = arr1.length;
+		for (int each : arr2) {
+			//store elements of arr2 starting from where arr1 ended
+			arr3[i++] = each;
+		}
+		System.out.println(Arrays.toString(arr3));
 	}
 }
