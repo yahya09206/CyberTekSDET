a9ea80a9a85bd9cdd3921939edc7ab7224f10314
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-15 21:46:15 -0700

    declared elements for 2d array

--- Day27_Recap/MultiDimensionalArray.java
+++ Day27_Recap/MultiDimensionalArray.java
@@ -2,5 +2,6 @@ import java.util.Arrays;
 public class MultiDimensionalArray {
 	public static void main(String[] args){
 		//2 dimensional array has to contain 1 dimensional array
+		String[][] scrumTeams = {{"Komiljon", "Yuliang"}, {"Sabir", "Tamara"}, {"Tugba", "Hilal", "Dilem"}};
 	}
 }
