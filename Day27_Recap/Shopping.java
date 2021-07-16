8677a0d89906c5c171fdd8628701d8f23aab130d
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-15 20:56:12 -0700

    loop to find first index of gloves

--- Day27_Recap/Shopping.java
+++ Day27_Recap/Shopping.java
@@ -6,6 +6,12 @@ public class Shopping {
 		double[] prices = {99.99, 150.0, 9.99, 250.0, 439.50, 39.99};
 		int[] itemIDs = {12345, 12346, 12347, 12348, 12349, 12350};
 
-		
+		//1. find out the first index of gloves
+		for (int i = 0; i < items.length; i++) {
+			if (items[i].equals("Gloves")) {
+				System.out.println(i);
+				break;
+			}
+		}
 	}
 }
