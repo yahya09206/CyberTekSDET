dc25e229fbe0cc9fcd915ea2d7a3602bb2b31e96
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-15 22:12:13 -0700

    updated comments

--- Day27_Recap/ArraysUtility.java
+++ Day27_Recap/ArraysUtility.java
@@ -15,6 +15,7 @@ public class ArraysUtility {
 		int[] arr3 = Arrays.copyOf(arr1, arr1.length + arr2.length);
 		System.out.println(Arrays.toString(arr3));
 
+		//declar variable to start from end of arr1
 		int i = arr1.length;
 		for (int each : arr2) {
 			//store elements of arr2 starting from where arr1 ended
