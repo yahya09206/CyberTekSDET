3fbc96c3c3b1619d777b443b942a937f082887ca
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-15 20:30:20 -0700

    switching to use for eahc loop

--- Day27_Recap/FirstUniqueElement.java
+++ Day27_Recap/FirstUniqueElement.java
@@ -6,10 +6,28 @@ public class FirstUniqueElement {
 		int firstUniqueElement = 0;
 
 		//outer loop
-		for (int j = 0; j < nums.length; j++) {//repeat inner loop to find frequency for all elements
+		// for (int j = 0; j < nums.length; j++) {//repeat inner loop to find frequency for all elements
+		// 	//iterate thru loop
+		// 	int frequency = 0;
+		// 	for (int i = 0; i < nums.length; i++) { //find frequency of one element
+		// 		if (nums[i] == nums[j]) {
+		// 			frequency++;	
+		// 		}
+		// 	}
+		// 	//check if frequency = 1
+		// 	if (frequency == 1) {
+		// 		firstUniqueElement = nums[j]; 
+		// 		//exit loop after finding first uniqe element 
+		// 		break;
+		// 	}
+		// }
+
+		//using for each loop
+		//outer loop
+		for (int each : nums) {//repeat inner loop to find frequency for all elements
 			//iterate thru loop
 			int frequency = 0;
-			for (int i = 0; i < nums.length; i++) { //find frequency of one element
+			for (int elements : nums) { //find frequency of one element
 				if (nums[i] == nums[j]) {
 					frequency++;	
 				}
