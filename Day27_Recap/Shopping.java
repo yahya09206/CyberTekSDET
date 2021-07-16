0c99aa1cdb49f3264b4ce938da39557e5b7eae2e
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-15 20:54:55 -0700

    init list of items price and id

--- Day27_Recap/Shopping.java
+++ Day27_Recap/Shopping.java
@@ -1,6 +1,11 @@
 import java.util.Arrays;
 public class Shopping {
 	public static void main(String[] args){
+
+		String[] items = {"Shoes", "Jacket", "Gloves", "Airpods", "iPad", "iPhone 12 case"};
+		double[] prices = {99.99, 150.0, 9.99, 250.0, 439.50, 39.99};
+		int[] itemIDs = {12345, 12346, 12347, 12348, 12349, 12350};
+
 		
 	}
 }
