3fb0325cb0b0e3e2284f78135afc0a83c05133bc
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-15 21:48:25 -0700

    printing out values from 2d array

--- Day27_Recap/MultiDimensionalArray.java
+++ Day27_Recap/MultiDimensionalArray.java
@@ -1,7 +1,14 @@
 import java.util.Arrays;
 public class MultiDimensionalArray {
 	public static void main(String[] args){
+		String[] scrum1 = {"Komiljon", "Yuliang"};
+		String[] scrum2 = {"Sabir", "Tamara"};
+		String[] scrum3 = {"Tugba", "Hilal", "Dilem"};
+
 		//2 dimensional array has to contain 1 dimensional array
-		String[][] scrumTeams = {{"Komiljon", "Yuliang"}, {"Sabir", "Tamara"}, {"Tugba", "Hilal", "Dilem"}};
+		String[][] scrumTeams = {scrum1, scrum2, scrum3};
+
+		//Print out 2d array using toDeepString
+		System.out.println(Arrays.deepToString(scrumTeams));
 	}
 }
