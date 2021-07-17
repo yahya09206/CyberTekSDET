4a9a69163a3640476a4b0166824ff832b90ddb17
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-16 20:26:28 -0700

    printing reversed string using return value

--- Day29_ReturnMethods/ReturnMethods.java
+++ Day29_ReturnMethods/ReturnMethods.java
@@ -2,5 +2,15 @@ import java.util.Arrays;
 public class ReturnMethods {
 	public static void main(String[] args){
 
+		System.out.println(reverse("Java"));
+	}
+
+	public static String reverse(String str){
+		String result = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			result += str.charAt(i);
+		}
+		//System.out.println(result);
+		return result;
 	}
 }
