e09160ecfba6f6d8fc13396a1b00d4380a62ed2f
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-17 16:49:57 -0700

    re doing grade method with return value

--- Day29_ReturnMethods/ReturnMethodsPractice2.java
+++ Day29_ReturnMethods/ReturnMethodsPractice2.java
@@ -2,10 +2,32 @@ import java.util.Arrays;
 public class ReturnMethodsPractice2 {
 	public static void main(String[] args){
 
+		grade(95);
 
 	}
 
-	public static void grade(int score){
+	// public static void grade(int score){
+
+	// 	if (score > 100 || score < 0) {
+	// 		System.out.println("Invalid");
+	// 		return; // can use return statement to exit method
+	// 	}
+
+	// 	if(score >= 90){
+	// 		System.out.println("A");
+	// 	}else if(score >= 80){
+	// 		System.out.println("B");
+	// 	}else if(score >= 70){
+	// 		System.out.println("C");
+	// 	}else if(score >= 60){
+	// 		System.out.println("D");
+	// 	}else {
+	// 		System.out.println("F");
+	// 	}
+	// }
+
+	//same grade method but with return value
+	public static String grade(int score){
 		
 	}
 }
