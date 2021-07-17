1b0d655843cbd9b5047ca1d42c6c309137ac5752
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-16 20:53:12 -0700

    done with first half of recap

--- Day29_ReturnMethods/ReturnMethodsPractice.java
+++ Day29_ReturnMethods/ReturnMethodsPractice.java
@@ -2,15 +2,19 @@ import java.util.Arrays;
 public class ReturnMethodsPractice {
 	public static void main(String[] args){
 
+		int maxNum = max(10, 20);
+		System.out.println(maxNum * 2);
+
 	}
 
 	public static int max(int a, int b){
-		int max = 0;
-		if(a > b){
-			max = a;
-		}else {
-			max = b;
-		}
-		return max;
+		// int max = 0;
+		// if(a > b){
+		// 	max = a;
+		// }else {
+		// 	max = b;
+		// }
+		// return max;
+		return (a > b)? a : b;
 	}
 }
