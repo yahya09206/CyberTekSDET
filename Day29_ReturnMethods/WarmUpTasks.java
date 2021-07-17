6f3466eaf734cb3059a4b01ff258a839a23ab5e7
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-16 19:42:42 -0700

    check for positive

--- Day29_ReturnMethods/WarmUpTasks.java
+++ Day29_ReturnMethods/WarmUpTasks.java
@@ -7,6 +7,10 @@ public class WarmUpTasks {
 
 	//method to check if integer is positive, negative or zero
 	public static void posNegZero(int number){
-		
+
+		//check if number is positive
+		if (number > 0) {
+			System.out.println(number + " is positive number");
+		}
 	}
 }
