db3dd68c828d810a8f49a03797a92b3355b21337
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-16 20:04:52 -0700

    printing out full name inside of method

--- Day29_ReturnMethods/WarmUpTasks.java
+++ Day29_ReturnMethods/WarmUpTasks.java
@@ -51,7 +51,9 @@ public class WarmUpTasks {
 
 		firstName = firstName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase();
 		lastName = lastName.substring(0,1).toUpperCase() + lastName.substrinlasttoLowerCase();
-		
+
+		String fullName = firstName + lastName;
+		System.out.println("fullName = " + fullName);
 
 
 	}
