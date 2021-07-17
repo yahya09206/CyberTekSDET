98594659e5327dbe39c7b3038d18ed9634fa53fa
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-16 20:37:42 -0700

    palindrome method is working

--- Day29_ReturnMethods/ReturnMethods.java
+++ Day29_ReturnMethods/ReturnMethods.java
@@ -3,6 +3,7 @@ public class ReturnMethods {
 	public static void main(String[] args){
 
 		System.out.println(reverse("Java"));
+		isPalindrome("level");
 	}
 
 	public static String reverse(String str){
@@ -16,7 +17,7 @@ public class ReturnMethods {
 
 	public static void isPalindrome(String str){
 		String reversedStr = reverse(str);
-		if (reversedStr.equals(str)) {
+		if (reversedStr.equalsIgnoreCase(str)) {
 			System.out.println(str + " is palindrome");
 		}else {
 			System.out.println(str + " is not palindrome");
