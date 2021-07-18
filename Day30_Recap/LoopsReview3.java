b76fb896619469ee110cce6d8f7fbf4b1d212607
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-18 16:11:42 -0700

    added more of the same word

--- Day30_Recap/LoopsReview3.java
+++ Day30_Recap/LoopsReview3.java
@@ -1,13 +1,13 @@
 public class LoopsReview3 {
 	public static void main(String[] args){
-		String sentence = "I love Java, Java is fun";
+		String sentence = "I love Java, Java is fun, Java Java Java";
 		String word = "Java";
 		int count = 0;
-		while(sentence.contains("Java")){
-			sentence.replaceFirst("Java", "");
+		while(sentence.contains(word)){
+			sentence = sentence.replaceFirst("Java", "");
 			count++;
 		}
-
+		System.out.println(count);
 
 	}
 }
