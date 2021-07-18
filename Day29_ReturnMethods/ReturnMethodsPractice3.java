e5b410c651a5f2fa8b3977537582c401f1a7601b
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-17 17:11:09 -0700

    var to contain final result

--- Day29_ReturnMethods/ReturnMethodsPractice3.java
+++ Day29_ReturnMethods/ReturnMethodsPractice3.java
@@ -1,6 +1,11 @@
 import java.util.Arrays;
 public class ReturnMethodsPractice3 {
 	public static void main(String[] args){
-		
+
+
+	}
+	public static String removeDuplicates(String str){
+		String result = "";
+		return "";
 	}
 }
