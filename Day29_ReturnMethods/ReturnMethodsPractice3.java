dbbb3ffaccc19e7695d2cc363599f0320c114180
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-17 17:14:25 -0700

    updated comments

--- Day29_ReturnMethods/ReturnMethodsPractice3.java
+++ Day29_ReturnMethods/ReturnMethodsPractice3.java
@@ -6,8 +6,11 @@ public class ReturnMethodsPractice3 {
 	}
 	public static String removeDuplicates(String str){
 		String result = ""; //"ABC"
+		//split string by spaces
 		for( String each : str.split(" ")){
+			//if character isn't not contained
 			if (!result.contains(each)) {
+				//add to result
 				result += each;
 			}
 		}
