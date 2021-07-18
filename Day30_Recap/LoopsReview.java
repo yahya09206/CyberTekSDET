9e8ce0cda1ffc35365a4973503d6a64343868f85
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-18 15:34:56 -0700

    printing character plus its number

--- Day30_Recap/LoopsReview.java
+++ Day30_Recap/LoopsReview.java
@@ -30,11 +30,19 @@ public class LoopsReview {
 		String str = "Java";
 
 		for (int i = 0; i <= str.length() - 1; i++) {
-			System.out.println(str.charAt(i));
+			System.out.print(str.charAt(i) + " ");
 		}
+		System.out.println();
 		//reverse order
 		for (int i = str.length() - 1; i >= 0; i--) {
-			System.out.println(str.charAt(i));
+			System.out.print(str.charAt(i) + " ");
+		}
+
+		System.out.println();
+		System.out.println("-----------------------------------");
+		//creating more than one variable inside of for loop
+		for(int i = 1, ch = 65; i <= 26; i++, ch++){
+			System.out.println(i + " " + (char) + ch);
 		}
 	}
 }
