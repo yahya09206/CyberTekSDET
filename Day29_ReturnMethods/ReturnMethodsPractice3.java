0800d6518241874df2d54bb22c19c213fd484088
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-17 17:17:01 -0700

    fixing

--- Day29_ReturnMethods/ReturnMethodsPractice3.java
+++ Day29_ReturnMethods/ReturnMethodsPractice3.java
@@ -2,6 +2,13 @@ import java.util.Arrays;
 public class ReturnMethodsPractice3 {
 	public static void main(String[] args){
 
+		String str = "AAABBBBBBCCCCC";
+		String[] arr = str.split("");
+		System.out.println(Arrays.toString(arr));
+
+		char[] arr2 = str.toCharArray();
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arr2));
 
 	}
 	public static String removeDuplicates(String str){
