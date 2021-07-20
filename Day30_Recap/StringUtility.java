c438e5ca5a614070e2f319c59bd889eacf0116ec
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-19 19:54:42 -0700

    changing method return type

--- Day30_Recap/StringUtility.java
+++ Day30_Recap/StringUtility.java
@@ -3,11 +3,11 @@ public class StringUtility {
 		reverse("what it do babay");
 	}
 
-	public static void reverse(String str){
+	public static String reverse(String str){
 		String reverse = "";
 		for (int i = str.length() - 1; i >= 0; i--) {
 			reverse += str.charAt(i);
 		}
-		System.out.println(reverse);
+		//System.out.println(reverse);
 	}
 }
