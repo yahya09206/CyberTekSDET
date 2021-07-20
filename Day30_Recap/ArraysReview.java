03b948cb7c5fc892c8ff537c1aff3715b494d248
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-19 18:42:01 -0700

    printing out 10 null items

--- Day30_Recap/ArraysReview.java
+++ Day30_Recap/ArraysReview.java
@@ -7,9 +7,9 @@ public class ArraysReview {
 			   item3 = "Apple",
 			   item4 = "Chicken",
 			   item5 = "Papers";
-
+		//declare array of 10 items
 		String[] items = new String[10];
 
-		System.out.println();
+		System.out.println(Arrays.toString(items));
 	}
 }
