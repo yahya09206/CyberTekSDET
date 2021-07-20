9fdb3d0ccb09e1b04e8eeb5900bb5b1343fa814f
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-19 18:40:29 -0700

    array of 10 items

---
+++ Day30_Recap/ArraysReview.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class ArraysReview {
+	public static void main(String[] args){
+		//variable of strings
+		String item = "Eggs",
+			   item2 = "Milk",
+			   item3 = "Apple",
+			   item4 = "Chicken",
+			   item5 = "Papers";
+
+		String[] items = new String[10];
+
+		System.out.println();
+	}
+}
