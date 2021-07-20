5de87e35f29dc45b24202fe4727b8fd6f2b40951
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-19 19:06:40 -0700

    new array is printing values of both groups together

--- Day30_Recap/ArraysReview.java
+++ Day30_Recap/ArraysReview.java
@@ -37,5 +37,22 @@ public class ArraysReview {
 		//merge arrays
 		String[] group1 = {"Sophie", "Feruza", "Ekaterina"};
 		String[] group2 = {"Shazia", "Bilguun", "Danka", "Mucahit"};
+
+		//array to store group1 and 2 values
+		String[] group3 = new String[group1.length + group2.length];
+
+		//variable to keep track of index
+		int k = 0;
+		//use for loop to loop
+		for (int i = 0; i <= group1.length - 1; i++) {
+			group3[k++] = group1[i];
+		}
+
+		for (int j = 0; j <= group2.length - 1; j++) {
+			group3[k++] = group2[j];
+		}
+
+		System.out.println(Arrays.toString(group3));
+
 	}
 }
