b5f6ddc94ad33a921e0d70e01965a9ed2850675e
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-19 19:18:41 -0700

    finding min and max using math.max class

--- Day30_Recap/ForEachLoop.java
+++ Day30_Recap/ForEachLoop.java
@@ -6,5 +6,19 @@ public class ForEachLoop {
 		for(int each : numbers){
 			System.out.println(each);
 		}
+
+		System.out.println("-------------------------");
+		//find max number from array
+		int max = numbers[0];
+		int min = numbers[0];
+		for (int number : numbers) {
+			if (number > max) {
+				max = number;
+			}//use math.max method to find max
+			max = Math.max(number, max);
+			min = Math.min(number, min);
+		}
+		System.out.println(max);
+		System.out.println(min);
 	}
 }
