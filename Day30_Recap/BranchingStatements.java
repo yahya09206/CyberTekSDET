d8d3ef834112e2a2950471d2995a84576ce9be54
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-19 18:36:08 -0700

    difference between break and return

--- Day30_Recap/BranchingStatements.java
+++ Day30_Recap/BranchingStatements.java
@@ -4,10 +4,11 @@ public class BranchingStatements {
 		for (int i = 0; i <= 5; i++) {
 			if (i == 3) {
 				//break after i = 3
-				//break;
-				return;
+				break;//only exits loop
+				return;//exits whole method
 			}
 			System.out.println(i);
 		}
+		System.out.println("Hello Everyone");
 	}
 }
