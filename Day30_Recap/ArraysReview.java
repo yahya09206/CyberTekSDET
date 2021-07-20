a9d339aad91c7262c175c54d1003fe777bfd915a
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-19 18:47:07 -0700

    looping thru array of ints

--- Day30_Recap/ArraysReview.java
+++ Day30_Recap/ArraysReview.java
@@ -17,5 +17,10 @@ public class ArraysReview {
 
 		int[] numbers = {1,2,3,4,5,6,7,8,9,10};
 		System.out.println(Arrays.toString(numbers));
+
+		//loop thru array
+		for (int i = 0; i <= numbers.length - 1; i++) {
+			System.out.println(numbers[i]);
+		}
 	}
 }
