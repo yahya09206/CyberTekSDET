15803022b5d1f3877de6c0a24bedf03fbd7569b5
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-19 19:50:36 -0700

    method is working to reverse string

--- Day30_Recap/StringUtility.java
+++ Day30_Recap/StringUtility.java
@@ -1,9 +1,13 @@
 public class StringUtility {
 	public static void main(String[] args){
-
+		reverse("what it do babay");
 	}
 
 	public static void reverse(String str){
 		String reverse = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reverse += str.charAt(i);
+		}
+		System.out.println(reverse);
 	}
 }
