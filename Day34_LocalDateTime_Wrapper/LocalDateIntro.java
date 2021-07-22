b2ca8a039e1e4ea6da3e93d2739778991e22099b
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-22 07:27:25 -0700

    print date for today

--- Day34_LocalDateTime_Wrapper/LocalDateIntro.java
+++ Day34_LocalDateTime_Wrapper/LocalDateIntro.java
@@ -3,7 +3,13 @@ import java.time.LocalDate;
 public class LocalDateIntro {
 	public static void main(String[] args){
 
+		//local date for random day
 		LocalDate eid = LocalDate.of(2021, 7, 20);
+		System.out.println(eid);
+
+		//local date for today
+		LocalDate today = LocalDate.now();
+		System.out.println(today);
 
 	}
 }
