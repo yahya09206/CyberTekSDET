1cf1c2cdf86d10c011e915606bcd992ebb87910f
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-22 08:00:29 -0700

    printing out current time

--- Day34_LocalDateTime_Wrapper/LocalTimeIntro.java
+++ Day34_LocalDateTime_Wrapper/LocalTimeIntro.java
@@ -1,6 +1,9 @@
 import java.time.LocalTime;
+
 public class LocalTimeIntro {
 	public static void main(String[] args){
-
+		//time right now
+		LocalTime now = LocalTime.now();
+		System.out.println(now);
 	}
 }
