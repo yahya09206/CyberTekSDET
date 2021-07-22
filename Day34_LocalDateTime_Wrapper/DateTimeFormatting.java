13c16faa94bc8316e1adc2658ac187dc09ff233d
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-22 08:58:57 -0700

    entered format type

--- Day34_LocalDateTime_Wrapper/DateTimeFormatting.java
+++ Day34_LocalDateTime_Wrapper/DateTimeFormatting.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 public class DateTimeFormatting {
 	public static void main(String[] args){
-		DateTimeFormatter
+		DateTimeFormatter DF = DateTimeFormatter.ofPattern("MM/dd/YYYY");
 		LocalDate today = LocalDate.now();
 		System.out.println(today);
 	}
