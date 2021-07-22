e3a9d4b59d7a616e07ad7d87894170fa608e575c
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-21 23:01:24 -0700

    code to remove instances of word

--- Day31_Recap/WarmUpTasks2.java
+++ Day31_Recap/WarmUpTasks2.java
@@ -1,5 +1,14 @@
 public class WarmUpTasks2 {
 	public static void main(String[] args){
-		
+
+		String sentence = "Java Java JaVA";
+		String word = "java";
+
+		int count = 0;
+
+		if (sentence.contains(word)) {
+			sentence = sentence.replace(word, "");
+			count++
+		}
 	}
 }
