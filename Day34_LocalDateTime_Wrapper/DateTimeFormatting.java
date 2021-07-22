233c49530c83707c051556d0e9b9c80e555d3495
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-22 09:01:39 -0700

    date is being formatted

--- Day34_LocalDateTime_Wrapper/DateTimeFormatting.java
+++ Day34_LocalDateTime_Wrapper/DateTimeFormatting.java
@@ -3,7 +3,10 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeFormatting {
 	public static void main(String[] args){
 		DateTimeFormatter DF = DateTimeFormatter.ofPattern("MM/dd/YYYY");
+
 		LocalDate today = LocalDate.now();
 		System.out.println(today);
+		today.format(DF);
+		System.out.println(today.format(DF));
 	}
 }
