6388f76f014d2bee074ab6c4d730103090bad524
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-21 20:55:31 -0700

    for each loop and condition to check if index of character exists

--- Day31_Recap/WarmUpTasks.java
+++ Day31_Recap/WarmUpTasks.java
@@ -7,7 +7,15 @@ public class WarmUpTasks {
 	public static String removeDuplicates(String str){
 		//store result into new string
 		String result = "";
-		
+
+		//for each loop to change string elements into chars
+		for (char ch : str.toCharArray() ) {
+			//check if index of char is less than 0
+			if (result.indexOf(ch) < 0) {
+				//if char is not contained in result, then concat the char
+				result += ch;
+			}
+		}
 
 		return result;
 	}
