a173e40c0c5017fd0a6d79d46a8c91d32a5be16c
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-22 09:13:18 -0700

    printing time with am or pm

--- Day34_LocalDateTime_Wrapper/DateTimeFormatting.java
+++ Day34_LocalDateTime_Wrapper/DateTimeFormatting.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+
 public class DateTimeFormatting {
 	public static void main(String[] args){
 		DateTimeFormatter DF = DateTimeFormatter.ofPattern("MM/dd/YYYY EEEE");
@@ -12,8 +13,11 @@ public class DateTimeFormatting {
 
 		System.out.println("-----------------------------------");
 		//format time
-		DateTimeFormatter TF = DateTimeFormatter.ofPattern("hh:mm");
+		DateTimeFormatter TF = DateTimeFormatter.ofPattern("hh:mm a");
 		LocalTime currentTime = LocalTime.now();
 		System.out.println(currentTime.format(TF));
+		LocalTime time1 = LocalTime.of(17, 25);
+		System.out.println(time1);
+		System.out.println(time1.format(TF));
 	}
 }
