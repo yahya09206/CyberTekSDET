72f400e3726dd05b4e8d45f2756d7becbbe20f19
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-22 08:57:32 -0700

    imported time formatter

--- Day34_LocalDateTime_Wrapper/DateTimeFormatting.java
+++ Day34_LocalDateTime_Wrapper/DateTimeFormatting.java
@@ -1,5 +1,9 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 public class DateTimeFormatting {
 	public static void main(String[] args){
-		
+		DateTimeFormatter
+		LocalDate today = LocalDate.now();
+		System.out.println(today);
 	}
 }
