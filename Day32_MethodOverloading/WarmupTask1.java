dd10283934d314fba3fd382da74f838e57eb3385
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-23 22:20:08 -0700

    overloaded method for adding 3 numbers

--- Day32_MethodOverloading/WarmupTask1.java
+++ Day32_MethodOverloading/WarmupTask1.java
@@ -19,4 +19,8 @@ public class WarmUpTask1 {
 	}
 
 	//--------------------Method overloading-------------------------
+	public static int sum(int a, int b){ return a + b};
+
+	public static int sum(int a, int b, int c){return a + b + c};
+
 }
