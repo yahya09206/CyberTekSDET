c2b311312a17180ce40d62fab3d756107055ff97
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-23 22:11:27 -0700

    methods to add 3 and 4 numbers

--- Day32_MethodOverloading/WarmupTask1.java
+++ Day32_MethodOverloading/WarmupTask1.java
@@ -2,7 +2,16 @@ public class WarmUpTask1 {
 	public static void main(String[] args){
 		
 	}
-	public static int sumOf2Number(int a, int b){
+	public static int sumOf2Numbers(int a, int b){
 		return a + b;
+
+	}
+
+	public static int sumof3Numbers(int a, int b, int c){
+		return a + b + c;
+	}
+
+	public static int sumof4Numbers(int a, int b, int c, int d){
+		return a + b + c + d;
 	}
 }
