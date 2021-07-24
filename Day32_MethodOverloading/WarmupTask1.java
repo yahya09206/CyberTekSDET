41f58c09840d2162e607b18ec1d58402aec387a6
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-23 22:16:44 -0700

    replacing current methods with overloaded ones

--- Day32_MethodOverloading/WarmupTask1.java
+++ Day32_MethodOverloading/WarmupTask1.java
@@ -1,6 +1,9 @@
 public class WarmUpTask1 {
 	public static void main(String[] args){
 		
+		int sum1 = sumOf2Numbers(10, 12);
+		int sum2 = sumof3Numbers(100, 200, 300);
+		int sum3 = sumof4Numbers(1000, 2000, 3000, 4000);
 	}
 	public static int sumOf2Numbers(int a, int b){
 		return a + b;
@@ -14,4 +17,6 @@ public class WarmUpTask1 {
 	public static int sumof4Numbers(int a, int b, int c, int d){
 		return a + b + c + d;
 	}
+
+	//--------------------Method overloading-------------------------
 }
