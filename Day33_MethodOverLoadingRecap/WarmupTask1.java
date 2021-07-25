c0edd7bd10b32b18195aa20e40d2afc176ea8bb7
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 14:48:44 -0700

    overloading for string

--- Day33_MethodOverLoadingRecap/WarmupTask1.java
+++ Day33_MethodOverLoadingRecap/WarmupTask1.java
@@ -12,4 +12,13 @@ public class WarmupTask1 {
 		}
 		return result;
 	}
+
+	//method to reverse array and return int
+	public static String[] reverse(String[] array){
+		String[] result = new String[array.length];
+		for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+			result[j] = array[i];
+		}
+		return result;
+	}
 }
