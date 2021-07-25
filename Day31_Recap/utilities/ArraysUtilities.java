5980e660a1a69a1b2ed8f5187d61b8572feb1417
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 15:00:39 -0700

    copied reverse methods to utilities package

--- Day31_Recap/utilities/ArraysUtilities.java
+++ Day31_Recap/utilities/ArraysUtilities.java
@@ -213,4 +213,40 @@ public class ArraysUtilities {
 		}
 		return result;
 	}
+
+	//method to reverse array and return int
+	public static int[] reverse(int[] array){
+		int[] result = new int[array.length];
+		for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+			result[j] = array[i];
+		}
+		return result;
+	}
+
+	//method to reverse array and return string
+	public static String[] reverse(String[] array){
+		String[] result = new String[array.length];
+		for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+			result[j] = array[i];
+		}
+		return result;
+	}
+
+	//method to reverse array and return double
+	public static double[] reverse(double[] array){
+		double[] result = new double[array.length];
+		for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+			result[j] = array[i];
+		}
+		return result;
+	}
+
+	//method to reverse array and return char
+	public static char[] reverse(char[] array){
+		char[] result = new char[array.length];
+		for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+			result[j] = array[i];
+		}
+		return result;
+	}
 }
