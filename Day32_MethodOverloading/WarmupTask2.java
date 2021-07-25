cbeb5704d4503b236540a078f7d15e700e85b69f
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-24 21:44:28 -0700

    method call to add new element to array

--- Day32_MethodOverloading/WarmupTask2.java
+++ Day32_MethodOverloading/WarmupTask2.java
@@ -2,6 +2,9 @@ import java.util.Arrays;
 public class WarmUpTask2 {
 	public static void main(String[] args){
 
+		int[] array = {1,2,3,4,5,6,7,8,9};
+		int element = 10;
+		addInteger(array, element);
 
 	}
 
