39ef0f4bc91d0da26aa3974e4dc7c05300b13b7c
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 14:48:14 -0700

    forgot to declare var to increment elements

--- Day33_MethodOverLoadingRecap/WarmupTask1.java
+++ Day33_MethodOverLoadingRecap/WarmupTask1.java
@@ -7,8 +7,8 @@ public class WarmupTask1 {
 	//method to reverse array and return int
 	public static int[] reverse(int[] array){
 		int[] result = new int[array.length];
-		for (int i = array.length - 1; i >= 0; i--) {
-			result[i] = array;
+		for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+			result[j] = array[i];
 		}
 		return result;
 	}
