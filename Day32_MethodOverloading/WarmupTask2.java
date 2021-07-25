e2ae2194f8e04bec01d4ea22b3bdbc74bdd0cadb
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-24 21:46:13 -0700

    add element array is working

--- Day32_MethodOverloading/WarmupTask2.java
+++ Day32_MethodOverloading/WarmupTask2.java
@@ -1,15 +1,19 @@
 import java.util.Arrays;
-public class WarmUpTask2 {
+public class WarmupTask2 {
 	public static void main(String[] args){
 
 		int[] array = {1,2,3,4,5,6,7,8,9};
 		int element = 10;
-		addInteger(array, element);
+		System.out.println(Arrays.toString(array));
+		array = addInteger(array, element);
+		System.out.println(Arrays.toString(array));
+
 
 	}
 
 	public static int[] addInteger(int[] array, int number){
 
+		int i = 0;
 		int[] result = new int[array.length + 1];
 		for (int each : array ) {
 			result[i++] = each;
