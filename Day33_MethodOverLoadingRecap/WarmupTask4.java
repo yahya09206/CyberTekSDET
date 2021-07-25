9063d60dd9dcfce23ecb51052eb4563cbb41614f
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 15:53:40 -0700

    removed all duplicates

--- Day33_MethodOverLoadingRecap/WarmupTask4.java
+++ Day33_MethodOverLoadingRecap/WarmupTask4.java
@@ -1,6 +1,14 @@
 import java.util.Arrays;
 public class WarmupTask4 {
 	public static void main(String[] args){
-		
+		String str = "aaabbbbbbcc";
+		String result = "";
+		for (int i = 0; i < str.length() - 1; i++) {
+			char each = str.charAt(i);
+			if (!result.contains("" + each)) {
+				result += each;
+			}
+		}
+		System.out.println(result);
 	}
 }
