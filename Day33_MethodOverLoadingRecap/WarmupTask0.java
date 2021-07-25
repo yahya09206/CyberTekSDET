ecea27df19e83c37358230d00fd11f9e2ab624d2
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 14:17:45 -0700

    return result at end

--- Day33_MethodOverLoadingRecap/WarmupTask0.java
+++ Day33_MethodOverLoadingRecap/WarmupTask0.java
@@ -6,6 +6,16 @@ public class WarmUpTask0 {
 
 	//method to remove element at given index of the array
 	public static int[] remove(int[] array, int index){
-		
+		//new array with size of original length minus 1
+		int[] result = new int[array.length-1];
+		//loop to iterate all indexes
+		for (int i = 0, j = 0; i < array.length; i++) {
+			if (i == index) {//moves all elements of array except the element at given index
+				continue;
+			}else {
+				result[j++] = array[i];
+			}
+		}
+		return result;
 	}
 }
