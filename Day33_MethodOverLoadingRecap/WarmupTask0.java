51d0434550efc55100f0b2e13d40839445bfefb7
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 14:23:45 -0700

    overloading remove method

--- Day33_MethodOverLoadingRecap/WarmupTask0.java
+++ Day33_MethodOverLoadingRecap/WarmupTask0.java
@@ -13,7 +13,27 @@ public class WarmupTask0 {
 	public static int[] remove(int[] array, int index){
 		if(index < 0 || index > array.length - 1){//if the given index number is invalid
 			System.err.println("Invalid index");
-			return array;
+			System.exit(0);
+
+		}
+		//new array with size of original length minus 1
+		int[] result = new int[array.length-1];
+		//loop to iterate all indexes
+		for (int i = 0, j = 0; i < array.length; i++) {
+			if (i == index) {//moves all elements of array except the element at given index
+				continue;
+			}else {
+				result[j++] = array[i];
+			}
+		}
+		return result;
+	}
+
+	//method to remove element at given index of the array
+	public static int[] remove(int[] array, int index){
+		if(index < 0 || index > array.length - 1){//if the given index number is invalid
+			System.err.println("Invalid index");
+			System.exit(0);
 
 		}
 		//new array with size of original length minus 1
