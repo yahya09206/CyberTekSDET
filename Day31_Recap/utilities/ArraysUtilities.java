f87abde55614d8e664c5c956e2434efd561bac3e
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-24 22:20:47 -0700

    added array methods to arrays utilities

--- Day31_Recap/utilities/ArraysUtilities.java
+++ Day31_Recap/utilities/ArraysUtilities.java
@@ -2,4 +2,45 @@ package utilities;
 import java.util.Arrays;
 public class ArraysUtility {
 	
+	public static int[] addElement(int[] array, int number){
+		int i = 0;
+		int[] result = new int[array.length + 1];
+		for (int each : array ) {
+			result[i++] = each;
+		}
+		result[i] = number;
+		//using copyOf() method
+		// int[] result = Arrays.copyOf(array, array.length + 1);
+		// result[result.length - 1] = number;
+		return result;
+	}
+
+	//method to add double to end of array
+	public static double[] addElement(double[] array, double number){
+		int i = 0;
+		double[] result = new double[array.length + 1];
+		for (double each : array ) {
+			result[i++] = each;
+		}
+		result[i] = number;
+		return result;
+	}
+
+	//method to add string to end of array
+	public static String[] addElement(String[] array, String str){
+		int i = 0;
+		String[] result = new String[array.length + 1];
+		for (String each : array) {
+			result[i++] = each;
+		}
+		result[i] = str;
+		return result;
+	}
+
+	//method to add char to array
+	public static char[] addElement(char[] array, char ch){
+		 char[] result = Arrays.copyOf(array, array.length + 1);
+		 result[result.length - 1] = ch;
+		 return result;
+	}
 }
