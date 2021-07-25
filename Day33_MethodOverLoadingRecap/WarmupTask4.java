7d02e37ee638a3e8356007cc65cae3ddfb57984b
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 16:04:46 -0700

    overloaded unique method

--- Day33_MethodOverLoadingRecap/WarmupTask4.java
+++ Day33_MethodOverLoadingRecap/WarmupTask4.java
@@ -1,3 +1,4 @@
+import utilities.ArraysUtilities;
 import java.util.Arrays;
 public class WarmupTask4 {
 	public static void main(String[] args){
@@ -10,5 +11,62 @@ public class WarmupTask4 {
 			}
 		}
 		System.out.println(result);
+
+		System.out.println("---------------------------------");
+		int[] array = {1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,5,5,5,5,5};
+		int[] r1 = removeDup(array);
+		System.out.println(Arrays.toString(r1));
+	}
+
+	//method to remove duplicate ints from array
+	public static int[] removeDup(int[] array){
+		int[] newArr = {};
+
+		for (int each : array) {
+			//if element is not contained in new array then add element to array
+			if (!ArraysUtilities.contains(newArr, each)) {
+				newArr = ArraysUtilities.addElement(newArr, each);
+			}
+		}
+		return newArr;
+	}
+
+	//method to remove duplicate ints from array
+	public static double[] removeDup (double[] array){
+	 double[] newArr = {};
+
+		for  (double each : array) {
+			//if element is not contained in new array then add element to array
+			if (!ArraysUtilities.contains(newArr, each)) {
+				newArr = ArraysUtilities.addElement(newArr, each);
+			}
+		}
+		return newArr;
+	}
+
+	//method to remove duplicate ints from array
+	public static String[] removeDup(String[] array){
+		String[] newArr = {};
+
+		for (String each : array) {
+			//if element is not contained in new array then add element to array
+			if (!ArraysUtilities.contains(newArr, each)) {
+				newArr = ArraysUtilities.addElement(newArr, each);
+			}
+		}
+		return newArr;
+	}
+
+	//method to remove duplicate ints from array
+	public static char[] removeDup(char[] array){
+		char[] newArr = {};
+
+		for (char each : array) {
+			//if element is not contained in new array then add element to array
+			if (!ArraysUtilities.contains(newArr, each)) {
+				newArr = ArraysUtilities.addElement(newArr, each);
+			}
+		}
+		return newArr;
 	}
 }
