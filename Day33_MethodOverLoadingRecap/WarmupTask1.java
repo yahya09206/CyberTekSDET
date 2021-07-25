a3494bd18a9ff2a434524410cdfd6cd32643d2d3
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 14:49:34 -0700

    overloaded to return double and char

--- Day33_MethodOverLoadingRecap/WarmupTask1.java
+++ Day33_MethodOverLoadingRecap/WarmupTask1.java
@@ -13,9 +13,27 @@ public class WarmupTask1 {
 		return result;
 	}
 
-	//method to reverse array and return int
+	//method to reverse array and return string
 	public static String[] reverse(String[] array){
 		String[] result = new String[array.length];
+		for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+			result[j] = array[i];
+		}
+		return result;
+	}
+
+	//method to reverse array and return double
+	public static double[] reverse(double[] array){
+		double[] result = new double[array.length];
+		for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+			result[j] = array[i];
+		}
+		return result;
+	}
+
+	//method to reverse array and return int
+	public static char[] reverse(char[] array){
+		char[] result = new char[array.length];
 		for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
 			result[j] = array[i];
 		}
