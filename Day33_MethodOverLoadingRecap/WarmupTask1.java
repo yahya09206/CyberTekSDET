7f5fdc50f4a4e6d916689df504567286c34d5ca6
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 14:47:24 -0700

    method to reverse array and return int

--- Day33_MethodOverLoadingRecap/WarmupTask1.java
+++ Day33_MethodOverLoadingRecap/WarmupTask1.java
@@ -1,5 +1,15 @@
 import utilities.ArraysUtilities;
 import java.util.Arrays;
 public class WarmupTask1 {
-	
+	public static void main(String[] args){
+
+	}
+	//method to reverse array and return int
+	public static int[] reverse(int[] array){
+		int[] result = new int[array.length];
+		for (int i = array.length - 1; i >= 0; i--) {
+			result[i] = array;
+		}
+		return result;
+	}
 }
