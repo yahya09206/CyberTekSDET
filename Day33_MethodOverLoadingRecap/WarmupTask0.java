93cc40244aa4e964b5003308a07ab1e8c1e8a647
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 14:13:29 -0700

    setting up method

--- Day33_MethodOverLoadingRecap/WarmupTask0.java
+++ Day33_MethodOverLoadingRecap/WarmupTask0.java
@@ -1,6 +1,11 @@
 import java.util.Arrays;
 public class WarmUpTask0 {
 	public static void main(String[] args){
+
+	}
+
+	//method to remove element at given index of the array
+	public static int[] remove(int[] array, int index){
 		
 	}
 }
