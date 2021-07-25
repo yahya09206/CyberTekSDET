4107dca60924a27cbfa3e5aa5d2272150a96ea96
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-25 15:22:43 -0700

    overloaded frequency methods for all types

--- Day33_MethodOverLoadingRecap/WarmupTask3.java
+++ Day33_MethodOverLoadingRecap/WarmupTask3.java
@@ -4,5 +4,47 @@ public class WarmupTask3 {
 
 	}
 
-	
+	//method to find frequency of int inside of array
+	public static int frequency(int[] array, int element){
+		int count = 0;
+		for (int each : array) {
+			if (each == element) {
+				count++;
+			}
+		}
+		return count;
+	}	
+
+	//method to find frequency of int inside of array
+	public static double frequency(double[] array, double element){
+		int count = 0;
+		for (double each : array) {
+			if (each == element) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	//method to find frequency of int inside of array
+	public static String frequency(String[] array, String element){
+		int count = 0;
+		for (String each : array) {
+			if (each.equals(element)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	//method to find frequency of int inside of array
+	public static char frequency(char[] array, char element){
+		int count = 0;
+		for (char each : array) {
+			if (each == element) {
+				count++;
+			}
+		}
+		return count;
+	}
 }
