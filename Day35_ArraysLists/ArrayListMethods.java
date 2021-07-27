5ff88cac6bc89175425920f4fa6df98119925af3
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-26 20:16:40 -0700

    added to list and replaced

--- Day35_ArraysLists/ArrayListMethods.java
+++ Day35_ArraysLists/ArrayListMethods.java
@@ -4,7 +4,17 @@ public class ArrayListMethods {
 	public static void main(String[] args){
 		//init AL and add elements to it
 		ArrayList<String> groceryList = new ArrayList<>();
+		groceryList.add("Eggs");
+		groceryList.add("Milk");
+		groceryList.add("Water");
+		groceryList.add("Vinegar");
+		groceryList.add("Bread");
+		System.out.println(groceryList);
+		groceryList.set(0, "Toilet Paper");
+		groceryList.set(3, "Soda");
+		System.out.println(groceryList);
+		//remove element using remove() method
+
 
 	}
-
 }
