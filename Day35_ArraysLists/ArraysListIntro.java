08d4cf272727cc7644145341fa009f213d7343de
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-26 19:56:22 -0700

    get index using .get() method

--- Day35_ArraysLists/ArraysListIntro.java
+++ Day35_ArraysLists/ArraysListIntro.java
@@ -4,6 +4,7 @@ public class ArraysListIntro {
 	public static void main(String[] args){
 		int[] nums = new int[10];//fixed size of 10
 		System.out.println(Arrays.toString(nums));
+		System.out.println("--------------------------");
 		//ArrayList declaration
 		ArrayList<Integer> numbers = new ArrayList<>();//size is 0
 		//add to arraylist
@@ -12,6 +13,8 @@ public class ArraysListIntro {
 		numbers.add(30);
 		numbers.add(40);
 		System.out.println(numbers);
+		//get index from array list using .get() method
+		System.out.println(numbers.get(2));
 
 	}
 }
