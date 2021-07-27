32264411e21b784a8151fc368f888f7975d3cf56
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-26 20:07:18 -0700

    AL of strings

--- Day35_ArraysLists/ArrayListMethods.java
+++ Day35_ArraysLists/ArrayListMethods.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 public class ArrayListMethods {
-	
+	public static void main(String[] args){
+		//init AL and add elements to it
+		ArrayList<String> groceryList = new ArrayList<>();
+
+	}
+
 }
