7f2a49b543a634d41c9e223770470e3158a7a779
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-27 19:52:13 -0700

    practice with addAll() method

---
+++ Day35_ArraysLists/BulkOperations.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class BulkOperations {
+	public static void main(String[] args){
+		//use addAll() to add multiple elements at once
+		ArrayList<Integer> list = new ArrayList<>();
+	}
+}
