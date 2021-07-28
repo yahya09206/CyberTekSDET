206ddb6f79b0efed9143db652928f1005815f812
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-27 19:53:55 -0700

    adding elements from one AL to another

--- Day35_ArraysLists/BulkOperations.java
+++ Day35_ArraysLists/BulkOperations.java
@@ -2,7 +2,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class BulkOperations {
 	public static void main(String[] args){
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
 		//use addAll() to add multiple elements at once
 		ArrayList<Integer> list = new ArrayList<>();
+		//list.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
+		list.addAll(numbers);
+		System.out.println(list);
 	}
 }
