4541d41aa38106432ae0483019b5d172342c343d
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-27 19:48:53 -0700

    using regular equals and strict equals to compare

--- Day35_ArraysLists/ArrayListMethods.java
+++ Day35_ArraysLists/ArrayListMethods.java
@@ -104,6 +104,15 @@ public class ArrayListMethods {
 
 		System.out.println("-----------------------------------------");
 		//equals method()
+		ArrayList<Integer> n1 = new ArrayList<>();
+		n1.add(10);
+		n1.add(20);
+
+		ArrayList<Integer> n2 = new ArrayList<>();
+		n2.add(10);
+		n2.add(20);
+		System.out.println(n1 == n2);
+		System.out.println(n1.equals(n2));
 
 
 
