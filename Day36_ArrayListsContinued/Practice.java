954ce0e8303ba637a7510070d091bc2cf3adaf16
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-28 20:26:55 -0700

    loop to go thru and check if appears less than 5 times

--- Day36_ArrayListsContinued/Practice.java
+++ Day36_ArrayListsContinued/Practice.java
@@ -4,5 +4,12 @@ public class Practice {
 	public static void main(String[] args){
 		ArrayList<Integer> list = new ArrayList<>();
 		list.addAll(Arrays.asList(1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,6,6,6,7,7,7,8,8));
+		//loop to remove
+		for (inti = 0; i < list.size(); i++) {
+			if (list.get(i) < 5) {
+				list.remove(i);
+			}
+		}
+		System.out.println(list);
 	}
 }
