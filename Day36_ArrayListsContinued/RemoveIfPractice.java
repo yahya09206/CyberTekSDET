efc810eaa867da093b0e021f0f95672e409a7f78
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-28 22:27:12 -0700

    done for the night

---
+++ Day36_ArrayListsContinued/RemoveIfPractice.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class RemoveIfPractice {
+	public static void main(String[] args){
+		ArrayList<Integer>  scores = new ArrayList<>();
+		scores.addAll( Arrays.asList(100, 90, 75, 85, 65, 85, 55, 45, 73, 73, 35, 47));
+		ArrayList<Integer> gradeOfA
+		ArrayList<Integer> gradeOfA = new ArrayList<>(); // 90 ~ 100
+		ArrayList<Integer> gradeOfB = new ArrayList<>(); // 80 ~ 89
+		ArrayList<Integer> gradeOfC = new ArrayList<>(); // 70 ~ 79
+		ArrayList<Integer> gradeOfD = new ArrayList<>(); // 60 ~ 69
+		ArrayList<Integer> gradeOfF = new ArrayList<>(); // 0 ~ 59
+	}
+}
