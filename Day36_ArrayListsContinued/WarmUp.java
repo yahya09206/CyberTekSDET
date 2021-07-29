361b6214d3d40e92c531cf7ed7c67cce04803446
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-28 20:07:29 -0700

    printing al with added index at end

--- Day36_ArrayListsContinued/WarmUp.java
+++ Day36_ArrayListsContinued/WarmUp.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class WarmUp {
 	public static void main(String[] args){
-		
+		ArrayList<Integer> list = new ArrayList<>();
+		list.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+		System.out.println(list);
+		//list.set() to get last element
+		list.set(list.size() - 1, 0);
+		System.out.println(list);
 	}
 }
