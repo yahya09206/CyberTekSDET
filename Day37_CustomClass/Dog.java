dc67a953bc29742d0e638aebd7b1c4e35b5c75cc
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-29 09:08:15 -0700

    added three more methods

--- Day37_CustomClass/Dog.java
+++ Day37_CustomClass/Dog.java
@@ -7,8 +7,20 @@ public class Dog {
 	public String color;
 	public String gender;
 
-	public void eat(){
-		System.out.println(name + " is eating");
+	public void eat(String food){
+		System.out.println(name + " is eating " + food);
+	}
+
+	public void drink(String drink){
+		System.out.println(name + " is drinking " + drink);
+	}
+
+	public void bark(){
+		System.out.println(name + " is barking");
+	}
+
+	public void play(){
+		System.out.println(name + " is playing");
 	}
 
 
