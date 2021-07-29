cdfdc842ba523a198d30fc79ebf0162a0d59f3f7
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-28 20:39:07 -0700

    trippy

--- Day36_ArrayListsContinued/Practice.java
+++ Day36_ArrayListsContinued/Practice.java
@@ -6,7 +6,7 @@ public class Practice {
 		list.addAll(Arrays.asList(1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,6,6,6,7,7,7,8,8));
 		//loop to remove
 		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i) % 2) {
+			if (list.get(i) % 2 != 0) {
 				list.remove(i);
 			}
 		}
@@ -14,5 +14,9 @@ public class Practice {
 		System.out.println("------------------------------");
 		ArrayList<Integer> list2 = new ArrayList<>();
 		list2.addAll(Arrays.asList(1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,6,6,6,7,7,7,8,8));
+		//p represent each element in list then give condition
+		//list2.removeIf(p -> p < 5);
+		list2.removeIf(each -> each % 2 != 0);
+		System.out.println(list2);
 	}
 }
