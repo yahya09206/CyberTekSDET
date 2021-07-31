d8b40dd3a158b389c5220e2ee2836ba431f83fec
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-30 21:45:45 -0700

    printing out grades for each score

--- Day36_ArrayListsContinued/RemoveIfPractice.java
+++ Day36_ArrayListsContinued/RemoveIfPractice.java
@@ -5,15 +5,23 @@ public class RemoveIfPractice {
 		ArrayList<Integer>  scores = new ArrayList<>();
 		scores.addAll( Arrays.asList(100, 90, 75, 85, 65, 85, 55, 45, 73, 73, 35, 47));
 		ArrayList<Integer> gradeOfA = new ArrayList<>(scores); // 90 ~ 100
-		gradeOfA.removeIf(p -> p < 90);
+		gradeOfA.removeIf(p -> !(p >= 90 && p <= 100));
 		System.out.println("gradeOfA = " + gradeOfA);
 
 		ArrayList<Integer> gradeOfB = new ArrayList<>(scores); // 80 ~ 89
-		gradeOfB.removeIf(p -> p >= 90 || p < 80);
+		gradeOfB.removeIf(p -> !(p >= 80 && p <= 89));
 		System.out.println("gradeOfB = " + gradeOfB);
 
-		ArrayList<Integer> gradeOfC = new ArrayList<>(); // 70 ~ 79
-		ArrayList<Integer> gradeOfD = new ArrayList<>(); // 60 ~ 69
+		ArrayList<Integer> gradeOfC = new ArrayList<>(scores); // 70 ~ 79
+		gradeOfC.removeIf(p -> !(p >= 70 && p <= 79));
+		System.out.println("gradeOfC = " + gradeOfC);
+
+		ArrayList<Integer> gradeOfD = new ArrayList<>(scores); // 60 ~ 69
+		gradeOfD.removeIf(p -> !(p >= 60 && p <= 69));
+		System.out.println("gradeOfD = " + gradeOfD);
+
 		ArrayList<Integer> gradeOfF = new ArrayList<>(); // 0 ~ 59
+		gradeOfF.removeIf(p -> !(p >= 0 && p <= 59));
+		System.out.println("gradeOfF = " + gradeOfF);
 	}
 }
