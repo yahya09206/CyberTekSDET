eb6a1c99b100ec40607e6268939e6df7e4fc0723
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-30 21:38:26 -0700

    removing all grades that are not A

--- Day36_ArrayListsContinued/RemoveIfPractice.java
+++ Day36_ArrayListsContinued/RemoveIfPractice.java
@@ -3,8 +3,9 @@ public class RemoveIfPractice {
 	public static void main(String[] args){
 		ArrayList<Integer>  scores = new ArrayList<>();
 		scores.addAll( Arrays.asList(100, 90, 75, 85, 65, 85, 55, 45, 73, 73, 35, 47));
-		ArrayList<Integer> gradeOfA
 		ArrayList<Integer> gradeOfA = new ArrayList<>(); // 90 ~ 100
+		gradeOfA.removeIf(p -> p >= 90 && p <= 100);
+		System.out.println("gradeOfA = " + gradeOfA);
 		ArrayList<Integer> gradeOfB = new ArrayList<>(); // 80 ~ 89
 		ArrayList<Integer> gradeOfC = new ArrayList<>(); // 70 ~ 79
 		ArrayList<Integer> gradeOfD = new ArrayList<>(); // 60 ~ 69
