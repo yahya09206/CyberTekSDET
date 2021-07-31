edc001120798415617a8b894943d3cd4858472d6
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-30 21:52:42 -0700

    sorting list in reverse order

--- Day36_ArrayListsContinued/CollectionUtility.java
+++ Day36_ArrayListsContinued/CollectionUtility.java
@@ -5,5 +5,11 @@ public class CollectionUtility {
 	public static void main(String[] args){
 		ArrayList<Integer> list = new ArrayList<>();
 		list.addAll(Arrays.asList(10,10,5,0,-1,30,40,15));
+
+		//sort
+		Collections.sort(list);
+		Collections.reverse(list);
+
+		System.out.println(list);
 	}
 }
