684c5f42250d0e994a8c6ee913a8818227de688a
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-30 21:40:45 -0700

    only printing grade of B

--- Day36_ArrayListsContinued/RemoveIfPractice.java
+++ Day36_ArrayListsContinued/RemoveIfPractice.java
@@ -7,7 +7,11 @@ public class RemoveIfPractice {
 		ArrayList<Integer> gradeOfA = new ArrayList<>(scores); // 90 ~ 100
 		gradeOfA.removeIf(p -> p < 90);
 		System.out.println("gradeOfA = " + gradeOfA);
-		ArrayList<Integer> gradeOfB = new ArrayList<>(); // 80 ~ 89
+
+		ArrayList<Integer> gradeOfB = new ArrayList<>(scores); // 80 ~ 89
+		gradeOfB.removeIf(p -> p >= 90 || p < 80);
+		System.out.println("gradeOfB = " + gradeOfB);
+
 		ArrayList<Integer> gradeOfC = new ArrayList<>(); // 70 ~ 79
 		ArrayList<Integer> gradeOfD = new ArrayList<>(); // 60 ~ 69
 		ArrayList<Integer> gradeOfF = new ArrayList<>(); // 0 ~ 59
