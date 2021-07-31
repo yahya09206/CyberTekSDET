36159bdb689bdfb0c988161b1541be982f7bddcf
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-30 21:51:36 -0700

    sorting using collections class

---
+++ Day36_ArrayListsContinued/CollectionUtility.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+public class CollectionUtility {
+	public static void main(String[] args){
+		ArrayList<Integer> list = new ArrayList<>();
+		list.addAll(Arrays.asList(10,10,5,0,-1,30,40,15));
+	}
+}
