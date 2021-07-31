d9504726ca950b794b34dcfd3777de27d75adb18
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-30 21:39:40 -0700

    printing only grades that are A

--- Day36_ArrayListsContinued/RemoveIfPractice.java
+++ Day36_ArrayListsContinued/RemoveIfPractice.java
@@ -1,10 +1,11 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 public class RemoveIfPractice {
 	public static void main(String[] args){
 		ArrayList<Integer>  scores = new ArrayList<>();
 		scores.addAll( Arrays.asList(100, 90, 75, 85, 65, 85, 55, 45, 73, 73, 35, 47));
-		ArrayList<Integer> gradeOfA = new ArrayList<>(); // 90 ~ 100
-		gradeOfA.removeIf(p -> p >= 90 && p <= 100);
+		ArrayList<Integer> gradeOfA = new ArrayList<>(scores); // 90 ~ 100
+		gradeOfA.removeIf(p -> p < 90);
 		System.out.println("gradeOfA = " + gradeOfA);
 		ArrayList<Integer> gradeOfB = new ArrayList<>(); // 80 ~ 89
 		ArrayList<Integer> gradeOfC = new ArrayList<>(); // 70 ~ 79
