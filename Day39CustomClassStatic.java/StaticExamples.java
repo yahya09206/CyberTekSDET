bdb69e2e677ab423387aec278f7773f6f0f0e9be
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-03 12:58:06 -0700

    practice with static

---
+++ Day39CustomClassStatic.java/StaticExamples.java
@@ -0,0 +1,13 @@
+class CybertekStudent {
+	public String name, gender;
+	public int age, ID;
+
+	public String schoolName;
+}
+
+public class StaticExample {
+	public static void main(String[] args){
+		CybertekStudent student1 = new CybertekStudent();
+		CybertekStudent student2 = new CybertekStudent();
+	}
+}
