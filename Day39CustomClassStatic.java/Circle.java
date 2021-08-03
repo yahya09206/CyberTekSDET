9961d39fe0f16fdfcca33103e711623ff4ca0630
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-03 11:44:27 -0700

    declare field for PI

--- Day39CustomClassStatic.java/Circle.java
+++ Day39CustomClassStatic.java/Circle.java
@@ -1,9 +1,14 @@
 public class {
 	public double radius;
 	public double diameter;
+	// public double PI = 3.14;
 
 	public void setInfo(double radius){
 		this.radius = radius;
 		diameter = 2 * radius;
+	}
+
+	public double area(){
+		return radius * radius * Math.PI;
 	}
 }
