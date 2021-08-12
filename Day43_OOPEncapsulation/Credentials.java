cad2e305b999013887d35e0cdad09b636ceccd29
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-12 12:51:44 -0700

    created getters for fields

--- Day43_OOPEncapsulation/Credentials.java
+++ Day43_OOPEncapsulation/Credentials.java
@@ -1,4 +1,13 @@
 public class Credentials {
 
-	String username, password;
+	private String username, password;
+
+	public String getUsername(){
+		return username;
+	}
+
+	public String getPassword(){
+		return password;
+	}
+
 }
