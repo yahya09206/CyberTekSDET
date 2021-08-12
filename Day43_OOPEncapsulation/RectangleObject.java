9b38d6a2059efa46b316247b3d1e0466ce4298c0
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-12 12:45:47 -0700

    comparing both rect objects

--- Day43_OOPEncapsulation/RectangleObject.java
+++ Day43_OOPEncapsulation/RectangleObject.java
@@ -2,11 +2,16 @@ public class RectangleObject {
 	public static void main(String[] args){
 
 		Rectangle r1 = new Rectangle(-1, -3);
+
+		//System.out.println(r1);
 		r1.setLength(5);
 		r1.setWidth(6);
 
 		System.out.println(r1);
-		 r1.setLength(5);
-		 r1.setWidth(6);
+
+		Rectangle r2 = new Rectangle(6, 5);
+		System.out.println(r2);
+
+		System.out.println(r1.equals(r2));
 	}
 }
