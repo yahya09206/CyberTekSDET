f9921bedc4429f137b1e8af0a998c29802403bb8
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-12 12:43:30 -0700

    setting up new rect object

--- Day43_OOPEncapsulation/RectangleObject.java
+++ Day43_OOPEncapsulation/RectangleObject.java
@@ -2,7 +2,11 @@ public class RectangleObject {
 	public static void main(String[] args){
 
 		Rectangle r1 = new Rectangle(-1, -3);
+		r1.setLength(5);
+		r1.setWidth(6);
 
 		System.out.println(r1);
+		 r1.setLength(5);
+		 r1.setWidth(6);
 	}
 }
