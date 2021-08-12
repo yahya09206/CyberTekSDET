99b76430be2eadf5850b0ed0532673bec2ff29cc
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-12 12:08:51 -0700

    setters and getters for fields

--- Day43_OOPEncapsulation/Rectangle.java
+++ Day43_OOPEncapsulation/Rectangle.java
@@ -2,5 +2,19 @@ public class Rectangle {
 
 	private double length, width;
 
-	
+	public double getLength(){
+		return length;
+	}
+
+	public void setLength(double length){
+		this.length = length;
+	}
+
+	public double getWidth(){
+		return width;
+	}
+
+	public void setWidth(double width){
+		this.width = width;
+	}
 }
