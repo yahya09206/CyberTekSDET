f318422bddab2397d8889e6b7cbed53275933c1a
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-12 13:03:27 -0700

    updated comments

--- Day43_OOPEncapsulation/Credentials.java
+++ Day43_OOPEncapsulation/Credentials.java
@@ -25,10 +25,15 @@ public class Credentials {
 				r3 = false,
 				r4 = false;
 		for (char each : password.toCharArray()) {
+			//check if pw contains letters
 			if(Character.isLetter(each)){
 				r2 = true;
-			}else if(Character.isLetterOrDigit(each)){
+				//check if pw contains special character
+			}else if(!Character.isLetterOrDigit(each)){
 				r3 = true;
+			}else {
+				//check if pw contains at least one digit
+				r4 = true;
 			}
 		}
 	}
