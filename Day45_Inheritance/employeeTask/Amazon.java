5a5feaa9ac048d5107b0157d36a279c347d74cf7
Yahya <yahyahussein91@gmail.com>
2021-08-16 13:40:14 -0700

    eat method for each object

--- Day45_Inheritance/employeeTask/Amazon.java
+++ Day45_Inheritance/employeeTask/Amazon.java
@@ -1,12 +1,17 @@
 public class Amazon {
 	public static void main(String[] args){
+
 		//new instance of tester
 		Tester tester = new Tester("James", 'M', 28, 11, "SDET", 120000);
+		tester.eat("steak");
 
 		//new instance of developer
 		Developer developer = new Developer("Selda", 'F', 27, 12, "SDE", 150000);
+		developer.eat("Java");
 
 		//new instance of driver 
 		Driver driver = new Driver("John", 'M', 68, 14, "Truck Driver", 85000);
+		driver.eat("donuts");
+
 	}
 }
