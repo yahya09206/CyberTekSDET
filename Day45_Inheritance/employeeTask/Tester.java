d744c74e7bd113b0ff4fd888d1f4f553358b234c
Yahya <yahyahussein91@gmail.com>
2021-08-16 13:29:18 -0700

    copied over tester class

--- Day45_Inheritance/employeeTask/Tester.java
+++ Day45_Inheritance/employeeTask/Tester.java
@@ -1,3 +1,10 @@
-public class Tester extends Employee {
-	public Tester()
+public class Tester extends Employee { // Tester Is An Employee, Tester Is A person
+
+    public Tester(String name, char gender, int age, long id, String jobTitle, double salary) {
+        super(name, gender, age, id, jobTitle, salary);
+    }
+
+    public void testing(){
+        System.out.println(jobTitle+" "+name+" is testing");
+    }
 }
