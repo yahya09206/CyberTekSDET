fb6bb5b4bff72ab415f6d0b45f59089d5640475a
Yahya <yahyahussein91@gmail.com>
2021-08-16 12:29:48 -0700

    constructor from animal class into dog class

--- Day45_Inheritance/animalTask/Dog.java
+++ Day45_Inheritance/animalTask/Dog.java
@@ -1,7 +1,7 @@
-public class Dog extends Animal  { // Dog Is An animal
-
-    
-
+public class Dog extends Animal  {
+   public Dog(String name, String breed, char gender, String size, int age, String color){
+      super(name, breed, gender, size, age, color)
+   }
     public void bark(){
         System.out.println(name+" is barking");
     }
