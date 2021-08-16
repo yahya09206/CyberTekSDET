e0e6499fe7b2194c0cfb50cc525a557d10c4dd1f
Yahya <yahyahussein91@gmail.com>
2021-08-16 13:32:43 -0700

    method for driver class

--- Day45_Inheritance/employeeTask/Driver.java
+++ Day45_Inheritance/employeeTask/Driver.java
@@ -1,6 +1,10 @@
 public class Driver extends Employee {
-	
+
 	public Driver(String name, char gender, int age, long id, String jobTitle, double salary) {
         super(name, gender, age, id, jobTitle, salary);
+    }
+
+    public void drive(){
+    	System.out.println(name + " is driving");
     }
 }
