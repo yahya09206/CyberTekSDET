ecd6dbb3b737e0086cb7ad7e729528e81a82dd76
Yahya <yahyahussein91@gmail.com>
2021-08-16 13:31:21 -0700

    constructor for driver class

---
+++ Day45_Inheritance/employeeTask/Driver.java
@@ -0,0 +1,6 @@
+public class Driver extends Employee {
+	
+	public Driver(String name, char gender, int age, long id, String jobTitle, double salary) {
+        super(name, gender, age, id, jobTitle, salary);
+    }
+}
