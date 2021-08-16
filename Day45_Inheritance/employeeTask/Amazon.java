b356704d07d070d9bfe168d43a8d7d60d9d07b7b
Yahya <yahyahussein91@gmail.com>
2021-08-16 13:41:17 -0700

    wow no errors

--- Day45_Inheritance/employeeTask/Amazon.java
+++ Day45_Inheritance/employeeTask/Amazon.java
@@ -4,14 +4,19 @@ public class Amazon {
 		//new instance of tester
 		Tester tester = new Tester("James", 'M', 28, 11, "SDET", 120000);
 		tester.eat("steak");
+		tester.sleep();
 
 		//new instance of developer
 		Developer developer = new Developer("Selda", 'F', 27, 12, "SDE", 150000);
 		developer.eat("Java");
+		developer.sleep();
 
 		//new instance of driver 
 		Driver driver = new Driver("John", 'M', 68, 14, "Truck Driver", 85000);
 		driver.eat("donuts");
+		driver.sleep();
+
+		
 
 	}
 }
