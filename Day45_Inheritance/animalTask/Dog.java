8c65ab2dce081bd9033791964a6ff00eeef383bf
Yahya <yahyahussein91@gmail.com>
2021-08-16 12:27:30 -0700

    removed constructor

--- Day45_Inheritance/animalTask/Dog.java
+++ Day45_Inheritance/animalTask/Dog.java
@@ -1,8 +1,6 @@
 public class Dog extends Animal  { // Dog Is An animal
 
-    public Dog(String name, String breed, char gender, String size, int age, String color){
-       setInfo(name, breed, gender, size, age, color);
-    }
+    
 
     public void bark(){
         System.out.println(name+" is barking");
