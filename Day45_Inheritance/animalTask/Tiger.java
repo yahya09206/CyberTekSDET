92f9195c2dc53381f73ad9c3d68ebad3f8301fb0
Yahya <yahyahussein91@gmail.com>
2021-08-16 12:33:36 -0700

    completed tiger class

---
+++ Day45_Inheritance/animalTask/Tiger.java
@@ -0,0 +1,10 @@
+public class Tiger extends Animal{ // Tiger Is An animal
+
+    public Tiger(String name, String breed, char gender, String size, int age, String color) {
+        super(name, breed, gender, size, age, color);
+    }
+
+    public void roar(){
+        System.out.println(name +" is roaring");
+    }
+}
