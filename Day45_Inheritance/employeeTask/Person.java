2cc515baa8e6af7b9debf30533a3991460e14d8c
Yahya <yahyahussein91@gmail.com>
2021-08-16 13:01:29 -0700

    created constructor for person class

--- Day45_Inheritance/employeeTask/Person.java
+++ Day45_Inheritance/employeeTask/Person.java
@@ -1,4 +1,12 @@
 public class Person {
 
 	public String name;
+	public final char gender;
+	public int age;
+
+	public Person(String name, char gender, int age){
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+	}
 }
