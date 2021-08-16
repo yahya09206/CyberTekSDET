8ae2ec177b547f2fd70553cfccc212f7f0e65772
Yahya <yahyahussein91@gmail.com>
2021-08-16 13:02:51 -0700

    done with reqs for person class

--- Day45_Inheritance/employeeTask/Person.java
+++ Day45_Inheritance/employeeTask/Person.java
@@ -1,24 +1,24 @@
 public class Person {
 
-	public String name;
-	public final char gender;
-	public int age;
+	public String 		name;
+	public final char 	gender;
+	public int 			age;
 
 	public Person(String name, char gender, int age){
-		this.name = name;
+		this.name 	= name;
 		this.gender = gender;
-		this.age = age;
+		this.age 	= age;
 	}
 
 	public void eat(String food){
-        System.out.println(name +" is eating "+food);
+        System.out.println(name + " is eating " +food);
     }
 
     public void drink(String drink){
-        System.out.println(name +" is drinking "+drink);
+        System.out.println(name + " is drinking " + drink);
     }
 
     public void sleep(){
-        System.out.println(name+" is sleeping");
+        System.out.println(name + " is sleeping");
     }
 }
