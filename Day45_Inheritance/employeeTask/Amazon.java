275be46b0e0afd46c026fd3c92f4d07b78df196d
Yahya <yahyahussein91@gmail.com>
2021-08-16 13:37:19 -0700

    new instance of developer

--- Day45_Inheritance/employeeTask/Amazon.java
+++ Day45_Inheritance/employeeTask/Amazon.java
@@ -1,6 +1,9 @@
 public class Amazon {
 	public static void main(String[] args){
+		//new instance of tester
+		Tester tester = new Tester("James", 'M', 28, 11, "SDET", 120000);
 
-		Tester tester = new Tester("James", "M", 28, 11, "SDET", 120000);
+		//new instance of developer
+		Developer developer = new Developer("Selda", 'F', 27, 12, "SDE", 150000);
 	}
 }
