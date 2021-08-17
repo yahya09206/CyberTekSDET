9c9da6b62feac3ab5f578af2db3f87ff6bd711a5
Yahya <yahyahussein91@gmail.com>
2021-08-17 07:26:41 -0700

    constructor and set fields

--- Day46_Inheritance/carTask/Car.java
+++ Day46_Inheritance/carTask/Car.java
@@ -1,8 +1,19 @@
 public class Car {
 
 	public String brand, model;
-	public int 	year;
+	public final int year;
 	public double price;
 	public String color;
 	public long miles;
+
+	public Car(String brand, String model, int year, double price, String color, long miles){
+		this.brand = brand;
+		this.model = model;
+		this.year = year;
+		this.price = price;
+		this.color = color;
+		this.miles = miles;
+	}
+
+
 }
