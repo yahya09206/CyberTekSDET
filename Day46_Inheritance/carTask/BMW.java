716ffdaeb736002fa1bf4debd18b1e3163da4851
Yahya <yahyahussein91@gmail.com>
2021-08-17 07:43:17 -0700

    another subclass of car called bmw

---
+++ Day46_Inheritance/carTask/BMW.java
@@ -0,0 +1,7 @@
+public class BMW {
+
+
+	public BMW(String brand, String model, int year, double price, String color, long miles){
+		super(brand, model, year, price, color, miles);
+	}
+}
--- Day46_Inheritance/carTask/Toyota.java
+++ Day46_Inheritance/carTask/Toyota.java
@@ -1,5 +1,9 @@
 public class Toyota extends Car{
 
+	//don't need to put inside constructor because it is static
+	boolean static boolean isReliable = true;
+
 	public Toyota(String brand, String model, int year, double price, String color, long miles){
 		super(brand, model, year, price, color, miles);
+	}
 }
