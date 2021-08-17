835503234372943f12ee2c73ffb7592b1cfb7deb
Yahya <yahyahussein91@gmail.com>
2021-08-17 08:01:10 -0700

    added instance method

--- Day46_Inheritance/carTask/Tesla.java
+++ Day46_Inheritance/carTask/Tesla.java
@@ -1,6 +1,12 @@
 public class Tesla {
 
+	public static boolean isElectric = true;
+
 	public Tesla(String model, int year, double price, String color, long miles){
 		super("Tesla", model, year, price, color, miles);
+	}
+
+	public void autoPilot(){
+		System.out.println(brand + " " + model + " is driving by itself");
 	}
 }
