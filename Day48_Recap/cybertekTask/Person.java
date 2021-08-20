d3d8c51c47e0c685678fa586b4f46467afbadf07
Yahya <yahyahussein91@gmail.com>
2021-08-20 07:50:21 -0700

    setters and getters for fields

--- Day48_Recap/cybertekTask/Person.java
+++ Day48_Recap/cybertekTask/Person.java
@@ -11,10 +11,34 @@ public class Person {
 		private final LocalDate dOB;
 
 		public Person(String name, char gender, int age, LocalDate dOB){
-			this.name = name;
+			setName(name);
 			this.gender = gender;
-			this.age = age;
 			this.dOB = dOB;
+			this.age = LocalDate.now().getYear() - this.dOB.getYear();
+		}
+
+		public String getName(){
+			return name;
+		}
+
+		public void setName(){
+			this.name = name;
+		}
+
+		public char getGender(){
+			return gender;
+		}
+
+		public void setGender(){
+			this.gender = gender;
+		}
+
+		public int getAge(){
+			return age;
+		}
+
+		public void setAge(){
+			this.age = age;
 		}
 	}
 }
