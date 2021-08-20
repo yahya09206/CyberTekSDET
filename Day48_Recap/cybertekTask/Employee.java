42283f7f609e20de6b3c35b30d80234bed8f757e
Yahya <yahyahussein91@gmail.com>
2021-08-20 08:00:11 -0700

    add constructor

--- Day48_Recap/cybertekTask/Employee.java
+++ Day48_Recap/cybertekTask/Employee.java
@@ -3,4 +3,11 @@ public class Employee extends Person {
 	private int employeeID;
 	private String jobTitle;
 	private double salary;
+
+	public Employee(String name, char gender, LocalDate dOB, int employeeID, String jobTitle, double salary){
+		super(name, gender, dOB);
+		this.employeeID = employeeID;
+		this.jobTitle = jobTitle;
+		this.salary = salary;
+	}
 }
