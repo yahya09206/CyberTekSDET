404a84f69f2f42c95d1f75444ab79adb8a15c075
Yahya <yahyahussein91@gmail.com>
2021-08-20 08:28:11 -0700

    student extends person

--- Day48_Recap/cybertekTask/Student.java
+++ Day48_Recap/cybertekTask/Student.java
@@ -1,6 +1,7 @@
-public class Student {
+public class Student extends Person{
 	private String schoolName;
-	
+	private int id;
+
 	public Student(String name, char gender, LocalDate dOB){
 		super(name, gender, dOB);
 	}
