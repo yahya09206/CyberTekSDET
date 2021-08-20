017df970dc7923c4573055c369d23df99117589f
Yahya <yahyahussein91@gmail.com>
2021-08-20 08:44:24 -0700

    extending from employee and constructor

--- Day48_Recap/cybertekTask/Developer.java
+++ Day48_Recap/cybertekTask/Developer.java
@@ -1,3 +1,6 @@
 public class Developer extends Employee {
-	
+
+	public Developer(String name, char gender, LocalDate dOB, int employeeId, String jobTitle, double salary){
+		super(name, gender, dOB, employeeId, jobTitle, salary);
+	}
 }
