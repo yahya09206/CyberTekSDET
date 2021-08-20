fe4ff4cf3dcc35c02f595939e58752356576eebc
Yahya <yahyahussein91@gmail.com>
2021-08-20 08:42:09 -0700

    method overriding

--- Day48_Recap/cybertekTask/Tester.java
+++ Day48_Recap/cybertekTask/Tester.java
@@ -3,4 +3,9 @@ public class Tester extends Employee{
 	public Tester(String name, char gender, LocalDate dOB, int employeeId, String jobTitle, double salary){
 		super(name, gender, dOB, employeeId, jobTitle, salary);
 	}
+
+	@Override
+	public void work(){
+		System.out.println(getJobTitle() + " " + getName() + " is testing");
+	}
 }
