920633077f18e7ccf30e52a89596f012d9df7e4f
Yahya <yahyahussein91@gmail.com>
2021-08-20 08:48:15 -0700

    last subclass of employee which is teacher

---
+++ Day48_Recap/cybertekTask/Teacher.java
@@ -0,0 +1,6 @@
+public class Teacher extends Employee {
+
+	public Teacher(String name, char gender, LocalDate dOB, int employeeId, String jobTitle, double salary){
+		super(name, gender, dOB, employeeId, jobTitle, salary);
+	}
+}
