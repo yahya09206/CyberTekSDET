d36c58c5797ee514c0627e645fdf8a6707a38c90
Yahya <yahyahussein91@gmail.com>
2021-08-20 07:45:19 -0700

    declare fields for person class

---
+++ Day48_Recap/cybertekTask/Person.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+public class Person {
+	public static void main(String[] args){
+
+		private String name;
+		private char gender;
+		private int age;
+		private LocalDate DoB;
+	}
+}
