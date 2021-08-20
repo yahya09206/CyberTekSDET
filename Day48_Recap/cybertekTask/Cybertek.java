18943ad19921aeec0a0116945562184c9e7fabc8
Yahya <yahyahussein91@gmail.com>
2021-08-20 09:02:09 -0700

    creating objects

---
+++ Day48_Recap/cybertekTask/Cybertek.java
@@ -0,0 +1,11 @@
+public class Cybertek {
+
+	public static void main(String[] args){
+		Person person = new Person();
+		Employee employee = new Employee();
+		Tester tester = new Tester();
+		Developer developer = new Developer();
+		Teacher teacher = new Teacher();
+		Student student = new Student();
+	}
+}
