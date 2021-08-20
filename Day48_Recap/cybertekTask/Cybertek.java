9eaceaf2a064ead71c1b16a9a3c196682a5f5115
Yahya <yahyahussein91@gmail.com>
2021-08-20 09:06:20 -0700

    done creating objects

--- Day48_Recap/cybertekTask/Cybertek.java
+++ Day48_Recap/cybertekTask/Cybertek.java
@@ -1,11 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 public class Cybertek {
 
 	public static void main(String[] args){
-		Person person = new Person();
-		Employee employee = new Employee();
-		Tester tester = new Tester();
-		Developer developer = new Developer();
-		Teacher teacher = new Teacher();
-		Student student = new Student();
+
+		Tester tester = new Tester("Yasin", 'M', LocalDate.of(1995, 5, 28), 13, "Tester", 125000);
+		Developer developer = new Developer("Mucahit", 'M', LocalDate.of(1992, 6, 16), 12, "Java Dev", 120000);
+		Teacher teacher = new Teacher("Asiya", 'F', LocalDate.of(1996, 8, 19), 18, "Softskill Teacher", 95000);
+		Student student = new Student("Yulia", 'F', LocalDate(1996, 7, 19), "Cybertek", 29);
 	}
 }
