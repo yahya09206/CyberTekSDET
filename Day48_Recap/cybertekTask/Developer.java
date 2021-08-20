b68d3a8d994dc2ab6406f0d1f0c4397f2b36f7ef
Yahya <yahyahussein91@gmail.com>
2021-08-20 08:46:51 -0700

    more overriding methods

--- Day48_Recap/cybertekTask/Developer.java
+++ Day48_Recap/cybertekTask/Developer.java
@@ -3,4 +3,14 @@ public class Developer extends Employee {
 	public Developer(String name, char gender, LocalDate dOB, int employeeId, String jobTitle, double salary){
 		super(name, gender, dOB, employeeId, jobTitle, salary);
 	}
+
+	@Override
+	public void work(){
+		System.out.println(getJobTitle() + " " + getName() + " is coding");
+	}
+
+	@Override
+	public void attendMeeting(){
+		System.out.println(getJobTitle() + " " + getName() + " is attending scrum meeting");
+	}
 }
