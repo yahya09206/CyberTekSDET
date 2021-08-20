4ead772557ad96c5e8d217e0de7e06480ac3724f
Yahya <yahyahussein91@gmail.com>
2021-08-20 07:53:13 -0700

    added another method

--- Day48_Recap/cybertekTask/Person.java
+++ Day48_Recap/cybertekTask/Person.java
@@ -44,5 +44,13 @@ public class Person {
 		public void eat(String food){
 			System.out.println(name + " is eating " + food);
 		}
+
+		public void drink(String drink){
+			System.out.println(name + " is drinking " + drink);
+		}
+
+		public void sleep(){
+			System.out.println(name + " is sleeping");
+		}
 	}
 }
