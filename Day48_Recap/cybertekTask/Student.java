0b4d09f3e0f9e44c1ed2f92ec1eda0cf209798d0
Yahya <yahyahussein91@gmail.com>
2021-08-20 08:31:24 -0700

    getters and setters

--- Day48_Recap/cybertekTask/Student.java
+++ Day48_Recap/cybertekTask/Student.java
@@ -2,7 +2,25 @@ public class Student extends Person{
 	private String schoolName;
 	private int id;
 
-	public Student(String name, char gender, LocalDate dOB){
+	public Student(String name, char gender, LocalDate dOB, String schoolName, int id){
 		super(name, gender, dOB);
+		setSchoolName(schoolName);
+		setId(id);
+	}
+
+	public String getSchoolName(){
+		return schoolName;
+	}
+
+	public void setSchoolName(){
+		this.schoolName = schoolName;
+	}
+
+	public id getId(){
+		return id;
+	}
+
+	public void setId(){
+		this.id = id;
 	}
 }
