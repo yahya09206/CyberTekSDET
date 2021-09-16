bccc05b32ba9bfc527a53f82c34d066f057a0ebb
Yahya <yahyahussein91@gmail.com>
2021-09-16 12:13:34 -0700

    adding different from collections

--- Day55_OOP_CollectionIntro/CollectionIntro.java
+++ Day55_OOP_CollectionIntro/CollectionIntro.java
@@ -2,7 +2,10 @@ import java.util.Collections;
 public class CollectionsIntro {
 	public static void main(String[] args){
 
-		List<Integer> list;
+		List<Integer> list1 = new ArrayList<>();
+		List<Integer> list2 = new LinkedList<>();
+		List<Integer> list3 = new Stack<>();
+		List<Integer> list4 = new Vector<>();
 
 		Set<Integer> set;
 
