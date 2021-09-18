acf9a658e7854f85d8d9bb98203a32a6571cdfd2
Yahya <yahyahussein91@gmail.com>
2021-09-18 00:08:03 -0700

    added static and instance method

---
+++ Day41_CustomClass_Constructors/utilities/Data.java
@@ -0,0 +1,14 @@
+public class Data {
+
+	public String str1, str2;
+
+	public void method1(){}
+
+	public void method2(){}
+
+	public static String str3, str4;
+
+	public static void method3(){}
+
+	public static void method4(){}
+}
