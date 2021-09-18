f5d7693a8fd64ba6af3444ff11bf3b8931e9cbdf
Yahya <yahyahussein91@gmail.com>
2021-09-18 01:19:17 -0700

    created third tester object

--- Day41_CustomClass_Constructors/ScrumTask/BankOfAmerica.java
+++ Day41_CustomClass_Constructors/ScrumTask/BankOfAmerica.java
@@ -3,8 +3,10 @@ public class BankOfAmerica {
 
 		Tester tester1 = new Tester("John", 'M', "QA", 10, 100000);
 		Tester tester2 = new Tester("Abbos", 'M', "SM", 11, 110000);
+		Tester tester3 = new Tester("Selda", 'F', "SM", 12, 120000);
 
 		System.out.println(tester1);
 		System.out.println(tester2);
+		System.out.println(tester3);
 	}
 }
