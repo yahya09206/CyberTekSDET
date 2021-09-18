54967ba0f16a786560b7149e7bb6404644adc465
Yahya <yahyahussein91@gmail.com>
2021-09-18 01:40:43 -0700

    lamda to check if id matches then remove

--- Day41_CustomClass_Constructors/ScrumTeam.java
+++ Day41_CustomClass_Constructors/ScrumTeam.java
@@ -1,5 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ScrumTeam {
 
-	public String PO, BA, SM;
-	
+	public String PO;
+	public String BA;
+	public String SM;
+	public ArrayList<Tester> testers;
+	public ArrayList<Developer> developers;
+
+	public ScrumTeam(String PO, String BA, String SM){
+		this.PO = PO;
+		this.BA = BA;
+		this.SM = SM;
+		testers = new ArrayList<>();
+		developers = new ArrayList<>();
+	}
+
+	public String toString() {
+        return "ScrumTeam{" +
+                "PO= '" + PO + '\'' +
+                ", BA= '" + BA + '\'' +
+                ", SM= '" + SM + '\'' +
+                ", number of testers= " + testers.size() +
+                ", number of developers= " + developers.size() +
+                '}';
+    }
+
+    public void hireTester(Tester tester){
+    	testers.add(tester);
+    }
+
+    public void hireTester(Tester[] testers){
+    	this.testers.addAll(Arrays.asList(testers));
+    }
+
+    public void removeTesteR(int id){
+    	testers.removeIf(tester -> p.id == id);
+    }
 }
