209364808169e194c07263df467abbfc1e04a845
Yahya <yahyahussein91@gmail.com>
2021-09-17 23:26:56 -0700

    dont know what i just deleted

---
+++ Day40_CustomClassStaticCont/ExecutionOfStaticBlock.java
@@ -0,0 +1,15 @@
+public class ExecutionOfStaticBlock {
+
+	// will always be first to run, doesn't matter placement
+	static {
+		System.out.println("Static Block");
+	}
+
+	public static void method1(){
+		System.out.println("Method 1");
+	}
+
+	public static void main(String[] args){
+		System.out.println("Main Method");
+	}
+}
--- Day40_CustomClassStaticCont/Test.java
+++ Day40_CustomClassStaticCont/Test.java
@@ -3,5 +3,11 @@ public class Test {
 	public static void main(String[] args){
 
 		System.out.println(StaticBlock.list);
+		StaticBlock.list.set(0, "Z");
+
+		System.out.println(StaticBlock.list);
+		System.out.println("-----------------------------------");
+
+		ExecutionOfStaticBlock.method1();
 	}
 }
