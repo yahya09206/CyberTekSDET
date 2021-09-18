31ba02b8197c1f81a5cf4ff0c355c75920fc21b5
Yahya <yahyahussein91@gmail.com>
2021-09-18 01:17:20 -0700

    creating objects of tester class

---
+++ Day41_CustomClass_Constructors/ScrumTask/BankOfAmerica.java
@@ -0,0 +1,7 @@
+public class BankOfAmerica {
+	public static void main(String[] args){
+
+		Tester tester1 = new Test("John", 'M', "QA", 10, 100000);
+		Tester tester2 = new Test("Abbos", 'M', "SM", 11, 110000);
+	}
+}
