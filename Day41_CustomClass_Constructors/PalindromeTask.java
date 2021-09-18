4839d49e3e6afa0ff30d01b642ea5dc95027274e
Yahya <yahyahussein91@gmail.com>
2021-09-18 00:19:52 -0700

    wow it worked on first try

--- Day41_CustomClass_Constructors/PalindromeTask.java
+++ Day41_CustomClass_Constructors/PalindromeTask.java
@@ -1,8 +1,9 @@
-import utilities.StringUtility;
+import static utilities.StringUtility.reverse;
 
 public class PalindromeTask {
 	public static void main(String[] args){
 
 		String word = "Level";
+		System.out.println(reverse(word));
 	}
 }
