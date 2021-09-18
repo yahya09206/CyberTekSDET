1ca8600e3860e62a52a009ab001440749f7d3974
Yahya <yahyahussein91@gmail.com>
2021-09-18 00:09:22 -0700

    creating objs of both static and instance

--- Day41_CustomClass_Constructors/ImportStatements.java
+++ Day41_CustomClass_Constructors/ImportStatements.java
@@ -1,5 +1,16 @@
-import java.util.Arrays;
-public class ImportStatements {
+import utilities.Data;
 
-	
+public class ImportStatements {
+	public static void main(String[] args){
+
+		Data obj = new Data();
+		System.out.println(obj.str1);
+		System.out.println(obj.str2);
+
+		System.out.println(Data.str3);
+		System.out.println(Data.str4);
+
+
+	}
+
 }
