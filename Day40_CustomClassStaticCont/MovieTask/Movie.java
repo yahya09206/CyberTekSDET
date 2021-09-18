d0dbd1ce434476540b5bc36dd6e1f7b0af36f49a
Yahya <yahyahussein91@gmail.com>
2021-09-17 22:30:38 -0700

    done with movie class

---
+++ Day40_CustomClassStaticCont/MovieTask/Movie.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+public class Movie {
+
+	// declare fields
+	public String title, country, genre, director;
+	public LocalDate releaseDate;
+	public ArrayList<String> casts = new ArrayList<>();
+
+	public void setInfo(String title, String country, String genre, String director, LocalDate releaseDate){
+		this.title = title;
+		this.country = country;
+		this.genre = genre;
+		this.director = director;
+		this.releaseDate = releaseDate;
+	}
+
+	public void addCast(String cast){
+		casts.add(cast);
+	}
+
+	public void addCast(String[] casts){
+		this.casts.addAll(Arrays.asList(casts));
+	}
+
+	public void addCast(ArrayList<String> casts){
+		this.casts.addAll(casts);
+	}
+
+	public String toString() { // January/01/2021 Tuesday
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM/dd/YYYY EEEE");
+        return "Movie{" +
+                "title= '" + title + '\'' +
+                ", country= '" + country + '\'' +
+                ", genre= '" + genre + '\'' +
+                ", director= '" + director + '\'' +
+                ", releaseDate= " + releaseDate.format(df) +
+                ", total number of casts= " + casts.size() +
+                '}';
+    }
+}
