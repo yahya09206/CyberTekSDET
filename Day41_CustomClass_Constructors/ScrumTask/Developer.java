392927be5b9df5e6113b7c31222e763406d266d7
Yahya <yahyahussein91@gmail.com>
2021-09-18 01:22:18 -0700

    developer class

---
+++ Day41_CustomClass_Constructors/ScrumTask/Developer.java
@@ -0,0 +1,11 @@
+public class Developer {
+
+	public String name;
+	public static char gender;
+	public String jobTitle;
+	public int id;
+	public static double salary;
+
+	public static boolean isEmployed;
+	public static boolean hatesTester;
+}
