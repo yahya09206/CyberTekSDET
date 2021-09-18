ee824ee27273cf500016ab14b5e3a07e543fcbc2
Yahya <yahyahussein91@gmail.com>
2021-09-18 00:11:05 -0700

    importing str3

--- Day41_CustomClass_Constructors/ImportStatements.java
+++ Day41_CustomClass_Constructors/ImportStatements.java
@@ -1,14 +1,17 @@
-import utilities.Data;
+//import utilities.Data;
+import static utilities.Data.str3;
 
 public class ImportStatements {
 	public static void main(String[] args){
 
-		Data obj = new Data();
-		System.out.println(obj.str1);
-		System.out.println(obj.str2);
+		// Data obj = new Data();
+		// System.out.println(obj.str1);
+		// System.out.println(obj.str2);
 
-		System.out.println(Data.str3);
-		System.out.println(Data.str4);
+		// System.out.println(Data.str3);
+		// System.out.println(Data.str4);
+
+		System.out.println("-------------------------------");
 
 
 	}
