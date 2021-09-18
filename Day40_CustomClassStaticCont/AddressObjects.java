93901f57d15b1d524162f7e0a815975f7f68b163
Yahya <yahyahussein91@gmail.com>
2021-09-17 22:04:16 -0700

    done with address class

---
+++ Day40_CustomClassStaticCont/AddressObjects.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class AddressObjects {
+	public static void main(String[] args){
+
+		Address address = new Address();
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter your building number");
+		String buildingNumber = scan.next();
+
+		scan.nextLine();
+		System.out.println("Enter your street name");
+		String streetName = scan.nextLine();
+
+		System.out.println("Enter your city name");
+		String city = scan.nextLine();
+
+		System.out.println("Enter your state name");
+		String state = scan.nextLine();
+
+		System.out.println("Enter your zipCode");
+		int zipCode = scan.nextInt();
+
+		address.setInfo(buildingNumber, streetName, city, state, zipCode);
+		System.out.println(address);
+	}
+}
