8480178b534c5b935e5d1a27ea54c3c92f96c217
Yahya <yahyahussein91@gmail.com>
2021-09-19 14:39:49 -0700

    created two objects and called methods on them

--- Day42_ConstructorAndGarbageCollection/warmupTasks/CybertekStudentObjects.java
+++ Day42_ConstructorAndGarbageCollection/warmupTasks/CybertekStudentObjects.java
@@ -1,5 +1,13 @@
 public class CybertekStudentObjects {
 	public static void main(String[] args){
-		
+
+		CybertekStudent student1 = new CybertekStudent("John", 29, 'M');
+		CybertekStudent student1 = new CybertekStudent("Feruza", 20, 'F');
+
+		System.out.println(student1);
+		System.out.println(student2);
+
+		student1.attendClass();
+		student2.replit();
 	}
 }
