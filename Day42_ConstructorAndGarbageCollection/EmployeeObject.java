afdbda3111c8121f727f72647aa506ef8866d8cd
Yahya <yahyahussein91@gmail.com>
2021-09-19 15:37:41 -0700

    done creating employee objects

--- Day42_ConstructorAndGarbageCollection/EmployeeObject.java
+++ Day42_ConstructorAndGarbageCollection/EmployeeObject.java
@@ -4,7 +4,13 @@ public class EmployeeObject {
 		Employee employee1 = new Employee("Johnny");
 		System.out.println(employee1);
 
-		Employee employee2 = new Employee("Johnny", "SDET", 2344, 125000);
+		Employee employee2 = new Employee("Johnny", "SDET");
 		System.out.println(employee2);
+
+		Employee employee3 = new Employee("Johnny", "SDET", 2344);
+		System.out.println(employee3);
+
+		Employee employee4 = new Employee("Johnny", "QA", 45332, 220000);
+		System.out.println(employee4);
 	}
 }
