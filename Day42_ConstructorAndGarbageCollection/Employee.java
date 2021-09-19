c51d36f5bc5c2c975cf0cfe010ed532836d03cab
Yahya <yahyahussein91@gmail.com>
2021-09-19 15:24:20 -0700

    contructor to set name and job title

--- Day42_ConstructorAndGarbageCollection/Employee.java
+++ Day42_ConstructorAndGarbageCollection/Employee.java
@@ -1,12 +1,15 @@
 public class Employee {
-	public static void main(String[] args){
 
-		public String name, jobTitle;
-		public int id;
-		public double salary;
+	public String name, jobTitle;
+	public int id;
+	public double salary;
 
-		public Employee(String name){
-			this.name = name;
-		}
+	public Employee(String name){
+		this.name = name;
+	}
+
+	public Employee(String name, String jobTitle){
+		this.name = name;
+		this.jobTitle = jobTitle;
 	}
 }
