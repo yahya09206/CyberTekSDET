0c879085c5d7ebf633d95b1fe8c090bbb777104e
Yahya <yahyahussein91@gmail.com>
2021-09-19 14:14:37 -0700

    created constructor to init fields

--- Day42_ConstructorAndGarbageCollection/warmupTasks/Item.java
+++ Day42_ConstructorAndGarbageCollection/warmupTasks/Item.java
@@ -3,4 +3,12 @@ public class Item {
 	public String name;
 	public double unitPrice;
 	public int quantity;
+
+	public Item(String name, double unitPrice, int quantity){
+
+	}
+	
+	public void calcCost(){
+
+	}
 }
