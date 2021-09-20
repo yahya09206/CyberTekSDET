b822fdcfeb0366d183c2ecb5bcd52620c498f89f
Yahya <yahyahussein91@gmail.com>
2021-09-19 22:49:07 -0700

    done with browser task

--- Day47_Recap/BrowserTest/BrowserObject.java
+++ Day47_Recap/BrowserTest/BrowserObject.java
@@ -8,5 +8,19 @@ public class BrowserObject {
 		firefox.open();
 		firefox.close();
 		firefox.backward();
+
+		Opera opera = new Opera();
+		opera.open();
+		opera.close();
+
+		Edge edge = new Edge();
+        edge.open();
+        edge.close();
+
+        System.out.println("-------------------------------------");
+
+        Safari safari = new Safari();
+        safari.open();
+        safari.close();
 	}
 }
