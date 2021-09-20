fc5f854df7b49d77d7d30db6cc79e1f3394de43c
Yahya <yahyahussein91@gmail.com>
2021-09-19 21:38:25 -0700

    practice with overloading main method

--- Day47_Recap/FinalKeyword.java
+++ Day47_Recap/FinalKeyword.java
@@ -1,5 +1,13 @@
 public class FinalKeyword {
 	public static void main(String[] args) {
+
+	}
+
+	public final static void main(Integer[] args){
+
+	}
+
+	final static void main(Double[] args){
 		
 	}
 }
