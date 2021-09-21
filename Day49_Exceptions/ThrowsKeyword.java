72499208f527f73d608b52412e8686742e858e79
Yahya <yahyahussein91@gmail.com>
2021-09-20 19:41:33 -0700

    day49 on exceptions

--- Day49_Exceptions/ThrowsKeyword.java
+++ Day49_Exceptions/ThrowsKeyword.java
@@ -1,29 +1,6 @@
 public class ThrowsKeyword {
 	public static void main(String[] args) throws InterruptedException{
 
-		System.out.println("Test started");
-		System.out.println("Step 1");
-		sleep(2.5);
-
-		System.out.println("Step 2");
-		slee(2.5);
-
-		System.out.println("test Completed");
+		
 	}
-
-	public static void sleep(double seconds) throws InterruptedException {
-
-		long milliSeconds = long (seconds * 1000);
-		Thread.sleep(milliSeconds);
-	}
-
-	// public static void sleep(double seconds){
-
-	// 	long milliSeconds = long (seconds * 1000);
-	// 	try {
-	// 		Thread.sleep(milliSeconds);
-	// 	}catch (InterruptedException e){
-	// 		e.printStackTrace();
-	// 	}
-	// }
 }
