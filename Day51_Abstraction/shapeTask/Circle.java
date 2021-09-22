282abb03a50ec4a8172e538b8545fe925b8144cb
Yahya <yahyahussein91@gmail.com>
2021-09-21 20:23:02 -0700

    overribe area method from abstract class

---
+++ Day51_Abstraction/shapeTask/Circle.java
@@ -0,0 +1,23 @@
+public class Circle extends Shape{
+
+	private double r;
+	public final static double PI = 3.14;
+
+	public double getR(){
+		return r;
+	}
+
+	public void setR(double r){
+		this.r = r;
+	}
+
+	public Circle(double r){
+		super("Circle");
+		this.r = r;
+	}
+
+	@Override
+	public double area(){
+		return r * r * PI;
+	}
+}
