c092e3137efbaf4c16d765dc2449105b86ef56b6
Yahya <yahyahussein91@gmail.com>
2021-09-23 11:22:17 -0700

    added elements to queues

--- day56_Collections/QueuePractice.java
+++ day56_Collections/QueuePractice.java
@@ -1,7 +1,19 @@
 package day56_Collections;
 
+import java.util.*;
+
 public class QueuePractice {
     public static void main(String[] args) {
+
+        Queue<Integer> q1 = new PriorityQueue<>();
+        q1.addAll(Arrays.asList(1,2,3,4,5,1,2,3,4,5));
+
+        Queue<Integer> q2 = new ArrayDeque<>();
+        q2.addAll(Arrays.asList(1,2,3,4,5,1,2,3,4,5));
+
+        Queue<Integer> q3 = new LinkedList<>();
+        q3.addAll(Arrays.asList(1,2,3,4,5,1,2,3,4,5));
+
 
     }
 }
