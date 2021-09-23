ecdbdd92317fd7f5a20d5b6fff0282a37feadbc6
Yahya <yahyahussein91@gmail.com>
2021-09-23 09:01:04 -0700

    different objects with set

--- day56_Collections/SetInterfacePractice.java
+++ day56_Collections/SetInterfacePractice.java
@@ -1,7 +1,17 @@
 package day56_Collections;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class SetInterfacePractice {
     public static void main(String[] args) {
 
+        Set<String> items = new HashSet<>();
+
+        Set<String> items2 = new LinkedHashSet<>();
+
+        Set<String> items3 = new TreeSet<>();
     }
 }
