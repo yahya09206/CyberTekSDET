cfba163c3bc22e0a703108a4b430468fa4d349af
Yahya <yahyahussein91@gmail.com>
2021-09-23 12:45:43 -0700

    removed all elements less than 17

--- day56_Collections/IterablePractice2.java
+++ day56_Collections/IterablePractice2.java
@@ -1,9 +1,6 @@
 package day56_Collections;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class IterablePractice2 {
     public static void main(String[] args) {
@@ -21,7 +18,28 @@ public class IterablePractice2 {
                 it.remove();
             }
         }
-        System.out.println(names);
+        System.out.println("names = " + names);
 
+        System.out.println("-------------------------------------------");
+        List<String> names2=new ArrayList<>();
+        names2.addAll(Arrays.asList("Ahmed","ahmed", "aHmEd",
+                "John", "Erjon", "Daniel", "Mustafa", "Mohammed",
+                "Ahmed", "ahmed","Yuliia", "Chris"));
+
+        names2.removeIf(p -> p.equalsIgnoreCase("ahmed"));
+        System.out.println("names2 = " + names2);
+
+        System.out.println("-------------------------------------------");
+        Set<Integer> set = new HashSet<>();
+        set.addAll(Arrays.asList(10,9,11,1,2,3,4,5,6,7,12,12,14,15,16,18,17,25,20));
+
+        // remove all elements that are less than 17:
+        // don't have to use i++ because it will increment itself
+        for (Iterator<Integer> i = set.iterator(); i.hasNext();){
+            if (i.next() < 17){
+                i.remove();
+            }
+        }
+        System.out.println("set = " + set);
     }
 }
