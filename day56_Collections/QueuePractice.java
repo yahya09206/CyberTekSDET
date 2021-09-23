ccd5f8627f96e3fe945772baf2093a5f708ec46d
Yahya <yahyahussein91@gmail.com>
2021-09-23 11:34:31 -0700

    using poll method

--- day56_Collections/QueuePractice.java
+++ day56_Collections/QueuePractice.java
@@ -14,6 +14,23 @@ public class QueuePractice {
         Queue<Integer> q3 = new LinkedList<>();
         q3.addAll(Arrays.asList(1,2,3,4,5,1,2,3,4,5));
 
+        System.out.println("q1 = " + q1);
+        System.out.println("q2 = " + q2);
+        System.out.println("q3 = " + q3);
+
+        System.out.println( ((LinkedList) q3) .get(2));
+        // System.out.println( ((LinkedList) q1).get(2));
+
+        System.out.println("--------------------------------------------");
+        q1.poll();
+
+        System.out.println("q1 = " + q1);
+
+        q2.poll();
+        System.out.println("q2 = " + q2);
+
+        q3.poll();
+        System.out.println("q3 = " + q3);
 
     }
 }
