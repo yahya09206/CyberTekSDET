95306261e919eaf930556d6a4d797e8bf2c9ab2a
Yahya <yahyahussein91@gmail.com>
2021-09-23 09:29:28 -0700

    choosing which data structure to remove dups and keep same order

--- day56_Collections/SetInterfacePractice.java
+++ day56_Collections/SetInterfacePractice.java
@@ -6,7 +6,8 @@ public class SetInterfacePractice {
     public static void main(String[] args) {
 
         String[] arr = {"Wooden Spoon", "Book", "Pen", "Phone",
-                "Wooden Spoon", "Wooden Spoon", "Wooden Spoon", "Milk", "Eggs", "Coke", "Paper Towels"};
+                        "Wooden Spoon", "Wooden Spoon", "Wooden Spoon",
+                        "Milk", "Eggs", "Coke", "Paper Towels"};
 
         Set<String> items1 = new HashSet<>();
         items1.addAll(Arrays.asList(arr));
@@ -23,5 +24,16 @@ public class SetInterfacePractice {
         System.out.println("Items1 = " + items1);
         System.out.println("Items2 = " + items2);
         System.out.println("Items3 = " + items3);
+
+//        String str = null;
+//        str.equals("Cybertek");
+//        System.out.println(str);
+
+        System.out.println("-------------------------------------------");
+        List<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,12,3,4,5,6,1,2,3,4,5));
+
+        list = new ArrayList<>(new LinkedHashSet<Integer>(list));
+        System.out.println(list);
     }
 }
