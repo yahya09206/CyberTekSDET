16f4891a8817d35c0b8852c2d39e9492987f4981
Yahya <yahyahussein91@gmail.com>
2021-09-23 08:51:07 -0700

    practicing popping with for loop

--- day56_Collections/ListPractice.java
+++ day56_Collections/ListPractice.java
@@ -1,6 +1,8 @@
 package day56_Collections;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
 import java.util.Vector;
 
 public class ListPractice {
@@ -11,10 +13,23 @@ public class ListPractice {
         arrayList.size();
 
         Vector<Integer> vector = new Vector<>();
-        System.out.print(vector.size());
+        System.out.println(vector.size());
 
 
+        // last in first out
+        Stack<Integer> stack = new Stack<>();
+        stack.addAll(Arrays.asList(1,2,3,4,5,6,7));
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
 
+        int a2 = stack.pop();
+        System.out.println(stack);
+
+        for (int i = 0; i < stack.size(); i++){
+            stack.pop();
+            System.out.println(stack);
+        }
     }
 
     public void method1(){
