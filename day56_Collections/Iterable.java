0c09dc3a47aae83044c766475f71827d52a2c0dc
Yahya <yahyahussein91@gmail.com>
2021-09-23 11:55:12 -0700

    correct way of removing object from collection type

--- day56_Collections/Iterable.java
+++ day56_Collections/Iterable.java
@@ -2,6 +2,7 @@ package day56_Collections;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Iterable {
@@ -17,6 +18,16 @@ public class Iterable {
 
         System.out.println("list1 = " + list1);
 
+        System.out.println("-----------------------------------------------");
 
+        // apply Iterable to arraylist
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,1,2,3,4,5,1,2,3,4,5));
+
+        Iterator<Integer> it = list2.iterator();
+        while (it.hasNext()){
+            if (it.next() < 4){
+                it.remove();
+            }
+        }
     }
 }
