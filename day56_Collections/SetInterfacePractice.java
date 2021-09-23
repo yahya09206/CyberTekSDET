36cdb69fb04a13b489e2ee7574f085c7090f73a4
Yahya <yahyahussein91@gmail.com>
2021-09-23 09:11:26 -0700

    trying to add null throws nullpointerexception

--- day56_Collections/SetInterfacePractice.java
+++ day56_Collections/SetInterfacePractice.java
@@ -1,17 +1,24 @@
 package day56_Collections;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SetInterfacePractice {
     public static void main(String[] args) {
 
-        Set<String> items = new HashSet<>();
+        String[] arr = {"Wooden Spoon", "Book", "Pen", "Phone",
+                "Wooden Spoon", "Wooden Spoon", "Wooden Spoon", "Milk", "Eggs", "Coke", "Paper Towels"};
 
-        Set<String> items2 = new LinkedHashSet<>();
+        Set<String> items1 = new HashSet<>(Arrays.asList(arr));
+        items1.addAll(null);
 
-        Set<String> items3 = new TreeSet<>();
+        Set<String> items2 = new LinkedHashSet<>(Arrays.asList(arr));
+        items2.addAll(null);
+
+        Set<String> items3 = new TreeSet<>(Arrays.asList(arr));
+        items3.addAll(null);
+
+        System.out.println("Items1 = " + items1);
+        System.out.println("Items2 = " + items2);
+        System.out.println("Items3 = " + items3);
     }
 }
