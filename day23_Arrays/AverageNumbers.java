2f8edca6eae6cb2f5a7fa2fa94ceebc16970060e
Yahya <yahyahussein91@gmail.com>
2021-09-27 22:37:35 -0700

    printing name and scores next to each other

--- day23_Arrays/AverageNumbers.java
+++ day23_Arrays/AverageNumbers.java
@@ -1,21 +1,7 @@
-import java.util.Scanner;
+package day23_Arrays;
+
 public class AverageNumbers {
 	public static void main(String[] args){
 
-		Scanner scan = new Scanner(System.in);
-		System.out.println("How many numbers do you want to enter? ");
-		int[] numbers = new int[scan.nextInt()];
-		int sum = 0;
-
-		for(int i = 0; i <= numbers.length; i++){
-			System.out.println("Enter a number: ");
-			numbers[i] = scan.nextInt();
-			sum += numbers[i]; //Returns the sum of the elements in the array
-		}
-
-		System.out.println("Sum = " + sum);
-		double average = (double) sum / numbers.length;
-		System.out.println("average = " + average);
-		scan.close();
 	}
 }
--- day23_Arrays/HighestAndLowestScores.java
+++ day23_Arrays/HighestAndLowestScores.java
@@ -5,5 +5,11 @@ public class HighestAndLowestScores {
 
         int[] numbers = {85, 70, 95, 90, 100};
         String[] names = {"Mike", "Adam", "Tony", "John", "Amy"};
+
+        // iterate thru arrays since they have same length
+        for (int i = 0; i <= numbers.length - 1; i++){
+            System.out.println(names[i] + ": " + numbers[i]);
+        }
+
     }
 }
