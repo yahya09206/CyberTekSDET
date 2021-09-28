2a0ecbc2ee3dee0bd043e8298a5c058592de96cc
Yahya <yahyahussein91@gmail.com>
2021-09-27 22:31:20 -0700

    array of scores and names

--- day23_Arrays/HighestAndLowestScores.java
+++ day23_Arrays/HighestAndLowestScores.java
@@ -3,6 +3,7 @@ package day23_Arrays;
 public class HighestAndLowestScores {
     public static void main(String[] args) {
 
-
+        int[] numbers = {85, 70, 95, 90, 100};
+        String[] names = {"Mike", "Adam", "Tony", "John", "Amy"};
     }
 }
