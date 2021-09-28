bc7b1fe6a7d24f8dfc5db81abd3ee6cb57a15fcd
Yahya <yahyahussein91@gmail.com>
2021-09-27 21:38:35 -0700

    printing size of each array

--- day23_Arrays/MyClassmates.java
+++ day23_Arrays/MyClassmates.java
@@ -5,6 +5,15 @@ public class MyClassmates {
 
 		// If we know what the elements are
 		String[] friends = {"Sabit", "Ahmet", "Tugba", "Beryl", "Ahmet"};
+		System.out.println("Size: " + friends.length);
 
+		// if we don't know what the elements are, but we know how many elements we want to store
+		String[] friends2 = new String[5];
+		System.out.println("Size: " + friends2.length);
+		friends2[0] = "Sabit";
+		friends2[1] = "Ahmet";
+		friends2[2] = "Tugba";
+		friends2[3] = "Beryl";
+		friends2[4] = "Ahmet";
 	}
 }
