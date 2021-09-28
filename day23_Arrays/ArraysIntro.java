1dcce9e74e2256313023d4da1d62ca68a9b6eeb9
Yahya <yahyahussein91@gmail.com>
2021-09-27 21:13:05 -0700

    printing names in reverse

--- day23_Arrays/ArraysIntro.java
+++ day23_Arrays/ArraysIntro.java
@@ -3,5 +3,12 @@ package day23_Arrays;
 public class ArraysIntro {
     public static void main(String[] args) {
 
+        String[] groupTwo = {"Yahya", "Ahmet", "Ahmad", "Gulistan", "Kadir", "Igor", "Aykut", "Saron", "Ateequllah",
+                "Feruza", "Nazli"};
+
+        // print in reverse
+        for(int i = groupTwo.length; i > 0; i--){
+            System.out.println(groupTwo[i]);
+        }
     }
 }
