5efbc13fe378b4959342c687ec6fd56b1dc9fddc
Yahya <yahyahussein91@gmail.com>
2021-09-27 21:30:13 -0700

    declare string of days array

---
+++ day23_Arrays/Days.java
@@ -0,0 +1,7 @@
+package day23_Arrays;
+
+public class Days {
+    public static void main(String[] args) {
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    }
+}
--- day23_Arrays/Months.java
+++ day23_Arrays/Months.java
@@ -15,6 +15,22 @@ public class Months {
         System.out.println(months[num-1]);
 
 
+        System.out.println("------------------------------------------");
+        // print numbers only divisible by 2
+        int[] nums = {12,34,2,123,455,3345,32334,554,100,40};
+
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] % 2 == 0){
+                System.out.println(nums[i]);
+            }
+        }
+
+        System.out.println("------------------------------------------");
+        // print every other number from array
+        int[] nums2 = {12,34,2,123,455,3345,32334,554,100,40};
+        for (int i = 0; i < nums2.length; i++){
+            System.out.println(nums2[i] + 2);
+        }
 
     }
 }
