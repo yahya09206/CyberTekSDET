9389731513057369d94c8828199d7fdb44314782
Yahya <yahyahussein91@gmail.com>
2021-09-27 21:22:01 -0700

    prompting for num and printing month

--- day23_Arrays/Months.java
+++ day23_Arrays/Months.java
@@ -1,9 +1,20 @@
 package day23_Arrays;
 
+import java.util.Scanner;
+
 public class Months {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
         String[] months = {"January", "Febraury", "March", "April", "May", "June", "July", "August", "September",
                 "October", "November", "December"};
+
+        System.out.println("Enter a number from 1-12");
+        int num = scan.nextInt();
+
+        System.out.println(months[num-1]);
+
+
+
     }
 }
