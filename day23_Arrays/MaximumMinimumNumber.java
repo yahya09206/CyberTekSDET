bdb889b5154581d159d97c892fb4fd2591e4c6cb
Yahya <yahyahussein91@gmail.com>
2021-09-27 21:56:10 -0700

    wow i suck at this

--- day23_Arrays/MaximumMinimumNumber.java
+++ day23_Arrays/MaximumMinimumNumber.java
@@ -3,6 +3,15 @@ package day23_Arrays;
 public class MaximumMinimumNumber {
     public static void main(String[] args) {
 
+        int[] nums = {10, 5, 4, 400, 50, 0, -1};
+        int max = nums[0];
 
+        for (int i = 0; i <= nums.length - 1; i++){
+            int each = nums[i];
+            if (each > max){
+                max = each;
+            }
+        }
+        System.out.println(max);
     }
 }
