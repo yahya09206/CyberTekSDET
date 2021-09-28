05e452030e670add286d25f2723ae7776e9c2273
Yahya <yahyahussein91@gmail.com>
2021-09-27 21:17:20 -0700

    printing arrays in both orders

--- day23_Arrays/ArraysIntro.java
+++ day23_Arrays/ArraysIntro.java
@@ -7,7 +7,12 @@ public class ArraysIntro {
                 "Feruza", "Nazli"};
 
         // print in reverse
-        for(int i = groupTwo.length; i > 0; i--){
+        for(int i = groupTwo.length - 1; i > 0; i--){
+            System.out.println(groupTwo[i]);
+        }
+        System.out.println("------------------------------------------");
+        // print regular
+        for (int i = 0; i <= groupTwo.length - 1; i++){
             System.out.println(groupTwo[i]);
         }
     }
