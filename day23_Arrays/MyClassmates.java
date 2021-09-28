40dc53319560a57478561cd9ca27b79f01a06b12
Yahya <yahyahussein91@gmail.com>
2021-09-27 21:40:14 -0700

    printing both arrays

--- day23_Arrays/MyClassmates.java
+++ day23_Arrays/MyClassmates.java
@@ -7,6 +7,10 @@ public class MyClassmates {
 		String[] friends = {"Sabit", "Ahmet", "Tugba", "Beryl", "Ahmet"};
 		System.out.println("Size: " + friends.length);
 
+		for (int i = 0; i <= friends.length - 1; i++){
+			System.out.println(friends[i]);
+		}
+
 		// if we don't know what the elements are, but we know how many elements we want to store
 		String[] friends2 = new String[5];
 		System.out.println("Size: " + friends2.length);
@@ -15,5 +19,9 @@ public class MyClassmates {
 		friends2[2] = "Tugba";
 		friends2[3] = "Beryl";
 		friends2[4] = "Ahmet";
+
+		for (int i = 0; i <= friends2.length - 1; i++){
+			System.out.println(friends2[i]);
+		}
 	}
 }
