54cbe5caff40d76deafd269c5039b58062666f5a
Yahya <yahyahussein91@gmail.com>
2021-09-27 21:19:20 -0700

    declaring string array of months

---
+++ day23_Arrays/Months.java
@@ -0,0 +1,9 @@
+package day23_Arrays;
+
+public class Months {
+    public static void main(String[] args) {
+
+        String[] months = {"January", "Febraury", "March", "April", "May", "June", "July", "August", "September",
+                "October", "November", "December"};
+    }
+}
