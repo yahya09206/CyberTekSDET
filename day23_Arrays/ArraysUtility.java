700fea7ad8f7e2acee91895cd3817fca92b5bdce
Yahya <yahyahussein91@gmail.com>
2021-09-27 23:21:33 -0700

    done with arrays utility

--- day23_Arrays/ArraysUtility.java
+++ day23_Arrays/ArraysUtility.java
@@ -4,57 +4,53 @@ import java.util.Arrays;
 
 public class ArraysUtility {
 	public static void main(String[] args){
-		
-		//toString - used when you want to print entire array
-		int[] array = {1,2,3,4,5,6};
-		System.out.println(array);
 
-		//Formatted array values
+		// toString
+		int[] array = {1,2,3,4,5,6};
 		System.out.println(Arrays.toString(array));
 
 		String[] array2 = new String[5];
-		System.out.println(array2);
 		System.out.println(Arrays.toString(array2));
 
-		int[] nums = new int[5];
-		System.out.println(Arrays.toString(nums));
+		int[] array3 = new int[5];
+		System.out.println(Arrays.toString(array3));
 
-		double[] nums2 = new double[5];
-		System.out.println(Arrays.toString(nums2));
+		double[] array4 = new double[5];
+		System.out.println(Arrays.toString(array4));
 
-		// sort(): sorts the elements of the array in ascending order
-		String[] students = {"Boburbek", "Aysu", "Abbos", "Sabir"};
+		System.out.println("--------------------------------------");
+		// sort() : sorts the elements of the array in ascending order
+
+		String[] students = {"Boburbek", "Aysu", "Abbos", "Savir"};
+		// call to string method, for strings you can't simply just call .sort on it you need to call toString first
 		System.out.println(Arrays.toString(students));
+		// call sort method after printing out array
 		Arrays.sort(students);
+		// print sorted array
 		System.out.println(Arrays.toString(students));
 
-		int[] numbers = {9,10,4,1,3,-1,0,1,2};
+		int[] numbers = {9, 10, 4, 1, 3, -1, 0, 1, 2};
 		System.out.println(Arrays.toString(numbers));
 		Arrays.sort(numbers);
-		System.out.println(Arrays.toString(numbers));
-		System.out.println("Minimum numbers: " + numbers[0]);
-		System.out.println("Maximum numbers: " + numbers[numbers.length - 1]);
+
+		System.out.println(numbers[0]);
+		System.out.println(numbers[numbers.length - 1]);
 
 		char[] chars = {'z', 'b', 'k', 'a', 'c', 'y', 'x'};
+		System.out.println(Arrays.toString(chars));
+
 		Arrays.sort(chars);
 		System.out.println(Arrays.toString(chars));
 
-
-		//equals() method
+		// equals(arr1, arr2) ==> return true or false
 		int[] num1 = {1,2,3};
 		int[] num2 = {1,2,3};
 
-		int[] num3 = {3,2,1};
-		int[] num4 = {2,3,1};
-
-		Arrays.sort(num3);
-		Arrays.sort(num4);
-		boolean r3 = Arrays.equals(num3, num4);
-
 		boolean r1 = Arrays.equals(num1, num2);
-		boolean r2 = Arrays.equals(num2, num3);
 		System.out.println("r1 = " + r1);
-		System.out.println("r2 = " + r2);
-		System.out.println("r3 = " + r3);
+
+
+
+
 	}
 }
