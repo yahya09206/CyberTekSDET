a3e4d58bc8672dbcb09a6f748891713b854346ae
Yahya <yahyahussein91@gmail.com>
2021-09-27 21:57:14 -0700

    printing max and min numbers

--- day23_Arrays/MaximumMinimumNumber.java
+++ day23_Arrays/MaximumMinimumNumber.java
@@ -5,13 +5,18 @@ public class MaximumMinimumNumber {
 
         int[] nums = {10, 5, 4, 400, 50, 0, -1};
         int max = nums[0];
+        int min = nums[0];
 
         for (int i = 0; i <= nums.length - 1; i++){
             int each = nums[i];
             if (each > max){
                 max = each;
             }
+            if (each < min){
+                min = each;
+            }
         }
         System.out.println(max);
+        System.out.println(min);
     }
 }
