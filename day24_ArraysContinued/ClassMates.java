5ae5d70d4670a5a1a35e9d149ffc8d521f4f6a19
Yahya <yahyahussein91@gmail.com>
2021-09-29 19:46:46 -0700

    printing array of classmates names

--- day24_ArraysContinued/ClassMates.java
+++ day24_ArraysContinued/ClassMates.java
@@ -1,7 +1,12 @@
 package day24_ArraysContinued;
 
+import java.util.Arrays;
+
 public class ClassMates {
 	public static void main(String[] args){
 
+		String[] classmates = {"Gulistan","Fruza","Ahmet","memet","Muhter","Tamara","Abbos",
+				"Shirin","Robinson","Shirin"};
+		System.out.println(Arrays.toString(classmates));
 	}
 }
