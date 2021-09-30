98cad23dd52a34bbe7da48c06cc78071f3c9ac12
Yahya <yahyahussein91@gmail.com>
2021-09-29 19:55:52 -0700

    program to store student grades and scores

--- day24_ArraysContinued/Grades.java
+++ day24_ArraysContinued/Grades.java
@@ -3,5 +3,18 @@ package day24_ArraysContinued;
 public class Grades {
 	public static void main(String[] args){
 
+		String [] names = {"Anna", "Nancy", "Sarah", "Abbos", "Yuliang", "Raphael", "Tamerlan", "Muhtar", "Jack"};
+		int [] scores = {90, 75, 80, 85, 91, 92, 93, 45, 65};
+		char[] grades = new char[names.length]; //{A, C, B}
+		int countA = 0;
+		int countB = 0;
+		int countC = 0;
+		int countD = 0;
+		int countF = 0;
+
+		if (scores[0] >= 90_){
+
+		}
+
 	}
 }
