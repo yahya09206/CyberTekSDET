9a37ee3968a70ca980bfd62ab568112bce094de1
Yahya <yahyahussein91@gmail.com>
2021-09-30 20:05:15 -0700

    printing string in reverse order by using split method

--- day24_ArraysContinued/StringMethods2.java
+++ day24_ArraysContinued/StringMethods2.java
@@ -1,8 +1,19 @@
 package day24_ArraysContinued;
 
+import java.util.Arrays;
+
 public class StringMethods2 {
 	public static void main(String[] args){
 
+		// split
+		String sentence = "I love learning java programming language";
+		String[] words = sentence.split(" ");
+
+		System.out.println(Arrays.toString(words));
+
+		for (int i = words.length - 1; i >= 0; i--) {
+			System.out.print(words[i] + " ");
+		}
 
 	}
 }
@@ -10,6 +21,6 @@ public class StringMethods2 {
 String methods:
 	split(value): returns string array, splits the string by given value
 	example:
-		sentence = i love programming
+		sentence = i love Java programming
 		sentence.split(" "); ==> {"I", "love", "Java", "programming"}
 */
