c93be40d535f30464dc50ec44f3ca0be29485e3c
Yahya <yahyahussein91@gmail.com>
2021-09-30 19:37:03 -0700

    code to reverse array of int

--- day24_ArraysContinued/ReverseArray.java
+++ day24_ArraysContinued/ReverseArray.java
@@ -2,5 +2,11 @@ package day24_ArraysContinued;
 
 public class ReverseArray {
 	public static void main(String[] args){
+
+		int[] arr = {1,2,3,4,5,6};
+		int[] reverse = new int[arr.length];
+
+
+
 	}
 }
