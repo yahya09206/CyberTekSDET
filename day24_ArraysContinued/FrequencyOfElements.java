f4c5928b8d00fea121ef641295010575339be60a
Yahya <yahyahussein91@gmail.com>
2021-09-30 19:32:29 -0700

    finding for one unique element

--- day24_ArraysContinued/FrequencyOfElements.java
+++ day24_ArraysContinued/FrequencyOfElements.java
@@ -3,6 +3,18 @@ package day24_ArraysContinued;
 public class FrequencyOfElements {
 	public static void main(String[] args){
 
+		int[] arr = {1,1,2,3,3,4};
+		int element = arr[0];
+		int count = 0;
+		for (int i = 0; i <= arr.length - 1; i++){
+
+			if (arr[i] == element){
+				count++;
+			}
+		}
+		if (count == 1) {
+			System.out.println(element + ", ");
+		}
 
 	}
 }
