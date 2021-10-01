ce8080646eeae4791bb51ee4f6e10b0737024f78
Yahya <yahyahussein91@gmail.com>
2021-09-30 19:35:06 -0700

    fixed loop errors

--- day24_ArraysContinued/FrequencyOfElements.java
+++ day24_ArraysContinued/FrequencyOfElements.java
@@ -5,17 +5,17 @@ public class FrequencyOfElements {
 
 		int[] arr = {1,1,2,3,3,4};
 
-		for (int j = arr.length - 1; j >= 0; j--) {
+		for (int j = 0; j < arr.length; j++) {
 			int element = arr[j];
 			int count = 0;
-			for (int i = 0; i <= arr.length - 1; i++){
+			for (int i = 0; i < arr.length; i++){
 
 				if (arr[i] == element){
 					count++;
 				}
 			}
 			if (count == 1) {
-				System.out.println(element + ", ");
+				System.out.println(element);
 			}
 		}
 
