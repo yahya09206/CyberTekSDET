0e8a8f1c089cf3c2dea3234dfab6caf87f3c56fb
Yahya <yahyahussein91@gmail.com>
2021-09-30 23:47:19 -0700

    declare two string variables

--- day24_ArraysContinued/Anagram.java
+++ day24_ArraysContinued/Anagram.java
@@ -4,5 +4,9 @@ public class Anagram {
 	public static void main(String[] args){
 
 		// find if two string are anagrams of each other
+		String word1 = "earth";
+		String word2 = "heart";
+
+
 	}
 }
