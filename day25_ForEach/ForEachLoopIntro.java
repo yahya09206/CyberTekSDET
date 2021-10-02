f4dd5dda56d05e8ed6788bb1efebf195420935e6
Yahya <yahyahussein91@gmail.com>
2021-10-01 21:48:30 -0700

    printing array using both regular and for each loop

--- day25_ForEach/ForEachLoopIntro.java
+++ day25_ForEach/ForEachLoopIntro.java
@@ -5,7 +5,12 @@ public class ForEachLoopIntro {
 
 		int[] numbers = {1,2,3,4,5};
 
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
 
-
+		for (int n : numbers){
+			System.out.println("n = " + n);
+		}
 	}
 }
