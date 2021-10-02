c6f24873afccd974c229354499cdd09f2643f0da
Yahya <yahyahussein91@gmail.com>
2021-10-01 21:39:10 -0700

    new array to merge all arrays same as their lengths combined

--- day25_ForEach/Merge3Arrays.java
+++ day25_ForEach/Merge3Arrays.java
@@ -6,5 +6,11 @@ public class Merge3Arrays {
 		int[] arr1 = {1,2,3},
 				arr2 = {4,5,6},
 				arr3 = {7,8,9,12,13,14};
+
+		int[] arr4 = new int[arr1.length + arr2.length + arr3.length];
+
+		for (int i = 0; i < arr1.length; i++) {
+			arr4[i] = arr1[i];
+		}
 	}
 }
