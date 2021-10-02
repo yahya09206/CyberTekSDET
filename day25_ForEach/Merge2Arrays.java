c408d6f363601daa3ee3b9cb02a5bd910b8befd7
Yahya <yahyahussein91@gmail.com>
2021-10-01 21:27:02 -0700

    variable to keep track of index inside of 3rd array

--- day25_ForEach/Merge2Arrays.java
+++ day25_ForEach/Merge2Arrays.java
@@ -5,6 +5,14 @@ public class Merge2Arrays {
 
 		int[] arr1 = {1,2,3,4},
 				arr2 = {5,6};
+		// set size of 3rd array to same length as arr1 and arr2 combined
 		int[] arr3 = new int[arr1.length + arr2.length];
+
+		// declare variable to represent index numbers of third array
+		int j = 0;
+		for (int i = 0; i < arr1.length; i++) {
+			// increment j so it can move to next index and store new element
+			arr3[j++] = arr1[i];
+		}
 	}
 }
