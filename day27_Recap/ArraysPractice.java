1dbbf09137f12a9c19e08a88a13864fe933ead77
Yahya <yahyahussein91@gmail.com>
2021-10-01 23:04:50 -0700

    practice with outofboundsexception

--- day27_Recap/ArraysPractice.java
+++ day27_Recap/ArraysPractice.java
@@ -1,8 +1,18 @@
 package day27_Recap;
 
+import java.util.Arrays;
+
 public class ArraysPractice {
 	public static void main(String[] args){
 
+		int[] array = new int[2];
+		array[0] = 10;
+		array[1] = 20;
+
+		array[3] = 30;
+		array[4] = 40;
+
+		System.out.println(Arrays.toString(array));
 
 	}
 }
