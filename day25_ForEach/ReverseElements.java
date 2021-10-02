d919ff47d0acdb01c7d19ecad5e1cc356b2645b9
Yahya <yahyahussein91@gmail.com>
2021-10-01 21:12:01 -0700

    added outer loop to go thru list

--- day25_ForEach/ReverseElements.java
+++ day25_ForEach/ReverseElements.java
@@ -7,14 +7,18 @@ public class ReverseElements {
 
 		String[] array = {"Java", "Python", "C#"};
 
-		String element = array[0]; // "Java"
-		String reverse = ""; // "avaJ
 
-		for (int i = element.length() - 1; i >= 0 ; i--) {
-			reverse += element.charAt(i);
+		for (int j = 0; j < array.length; j++) {
+			String element = array[0]; // "Java"
+			String reverse = ""; // "avaJ
+
+			for (int i = element.length() - 1; i >= 0 ; i--) {
+				reverse += element.charAt(i);
+			}
+			array[j] = reverse; // assigning reversed element to the index of the array
 		}
 
-		array[0] = reverse; // assigning reversed element to the index of the array
+
 		System.out.println(Arrays.toString(array));
 	}
 }
