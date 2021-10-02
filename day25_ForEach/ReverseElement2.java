d3652e0e4be4d40c9bcd72b8c03d974e315fe379
Yahya <yahyahussein91@gmail.com>
2021-10-01 22:40:02 -0700

    unique elements are printing

--- day25_ForEach/ReverseElement2.java
+++ day25_ForEach/ReverseElement2.java
@@ -3,5 +3,14 @@ package day25_ForEach;
 public class ReverseElement2 {
 	public static void main(String[] args){
 
+		String[] array = {"Java", "Python", "C#", "Cybertek", "School", "Selenium", "API"};
+
+		for (String each : array) {
+			String reverse = "";
+			for (int i = each.length() - 1; i >= 0 ; i--) { // index numbers of the string
+				reverse += each.charAt(i); // gets the characters from the string element
+			}
+			System.out.println(reverse);
+		}
 	}
 }
--- day25_ForEach/UniqueElements.java
+++ day25_ForEach/UniqueElements.java
@@ -3,5 +3,18 @@ package day25_ForEach;
 public class UniqueElements {
 	public static void main(String[] args){
 
+		String[] words = {"java", "java", "python", "c#"};
+
+		for (String each : words) {
+			int count = 0;
+			for (String word : words) {
+				if (word.equals(each)) {
+					count++;
+				}
+			}
+			if (count == 1) {
+				System.out.println(each);
+			}
+		}
 	}
 }
