d8036382b9a4abb512bca4da8215faf289f55f09
Yahya <yahyahussein91@gmail.com>
2021-10-01 22:43:41 -0700

    done with finding unique chars in char array

--- day25_ForEach/UniqueElements2.java
+++ day25_ForEach/UniqueElements2.java
@@ -3,5 +3,18 @@ package day25_ForEach;
 public class UniqueElements2 {
 	public static void main(String[] args){
 
+		char[] chars = {'A', 'B', 'A', 'C', 'A', 'D', 'D'};
+
+		for (char each : chars){
+			int count = 0;
+			for (char eachs : chars) {
+				if (each == eachs){
+					count++;
+				}
+			}
+			if (count == 1){
+				System.out.println(each);
+			}
+		}
 	}
 }
