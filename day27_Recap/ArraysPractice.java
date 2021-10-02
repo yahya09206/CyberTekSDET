242762158b534fc1031904ee3ab987320aab699f
Yahya <yahyahussein91@gmail.com>
2021-10-01 23:45:41 -0700

    done with recap

--- day27_Recap/ArraysPractice.java
+++ day27_Recap/ArraysPractice.java
@@ -9,10 +9,22 @@ public class ArraysPractice {
 		array[0] = 10;
 		array[1] = 20;
 
-		array[3] = 30;
-		array[4] = 40;
+		array = new int[4];
+		array[2] = 30;
+		array[3] = 40;
 
 		System.out.println(Arrays.toString(array));
+		int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+		// check length of above array
+		System.out.println(numbers.length);
+		System.out.println(numbers[0]);
+		System.out.println(numbers[numbers.length - 1]);
+
+		//System.out.println(numbers[200]);
+		for (int i = 0; i < numbers.length; i++){
+			System.out.println("numbers[i] = " + numbers[i]);
+		}
+
 
 	}
 }
--- day27_Recap/FirstUniqueCharacter.java
+++ day27_Recap/FirstUniqueCharacter.java
@@ -3,5 +3,12 @@ package day27_Recap;
 public class FirstUniqueCharacter {
 	public static void main(String[] args){
 
+		String str = "aaa bccddefggggh";
+
+		for (char each : str.replace(" ", "").toCharArray()){ // represents each character of str excluding space
+			if (str.indexOf(each) == str.lastIndexOf(each)){
+				System.out.println(each);
+			}
+		}
 	}
 }
