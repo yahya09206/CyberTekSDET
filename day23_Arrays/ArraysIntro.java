a1578ac8e5f0b222bc85264e876f3a17981ec2e0
Yahya <yahyahussein91@gmail.com>
2021-12-15 22:46:47 -0800

    restarting arrays section

--- day23_Arrays/ArraysIntro.java
+++ day23_Arrays/ArraysIntro.java
@@ -3,17 +3,5 @@ package day23_Arrays;
 public class ArraysIntro {
     public static void main(String[] args) {
 
-        String[] groupTwo = {"Yahya", "Ahmet", "Ahmad", "Gulistan", "Kadir", "Igor", "Aykut", "Saron", "Ateequllah",
-                "Feruza", "Nazli"};
-
-        // print in reverse
-        for(int i = groupTwo.length - 1; i > 0; i--){
-            System.out.println(groupTwo[i]);
-        }
-        System.out.println("------------------------------------------");
-        // print regular
-        for (int i = 0; i <= groupTwo.length - 1; i++){
-            System.out.println(groupTwo[i]);
-        }
     }
 }
