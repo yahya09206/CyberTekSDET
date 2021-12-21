86cc0d9c9da2b90d175864786cc2e2a3ce7d47ee
Yahya <yahyahussein91@gmail.com>
2021-12-20 20:30:37 -0800

    cleaning up

--- day23_Arrays/MaximumMinimumNumber.java
+++ day23_Arrays/MaximumMinimumNumber.java
@@ -3,20 +3,6 @@ package day23_Arrays;
 public class MaximumMinimumNumber {
     public static void main(String[] args) {
 
-        int[] nums = {10, 5, 4, 400, 50, 0, -1};
-        int max = nums[0];
-        int min = nums[0];
 
-        for (int i = 0; i <= nums.length - 1; i++){
-            int each = nums[i];
-            if (each > max){
-                max = each;
-            }
-            if (each < min){
-                min = each;
-            }
-        }
-        System.out.println(max);
-        System.out.println(min);
     }
 }
