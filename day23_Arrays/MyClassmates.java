034468f562f278ee83d28adff57a72bad8312e63
Yahya <yahyahussein91@gmail.com>
2021-12-22 22:53:09 -0800

    clean up

--- day23_Arrays/MyClassmates.java
+++ day23_Arrays/MyClassmates.java
@@ -3,26 +3,6 @@ package day23_Arrays;
 public class MyClassmates {
 	public static void main(String[] args){
 
-		// If we know what the elements are
-		String[] friends = {"Sabit", "Ahmet", "Tugba", "Beryl", "Ahmet"};
-		System.out.println("Size: " + friends.length);
 
-		for (int i = 0; i <= friends.length - 1; i++){
-			System.out.println(friends[i]);
-		}
-
-		System.out.println("------------------------------------------");
-		// if we don't know what the elements are, but we know how many elements we want to store
-		String[] friends2 = new String[5];
-		System.out.println("Size: " + friends2.length);
-		friends2[0] = "Sabit";
-		friends2[1] = "Ahmet";
-		friends2[2] = "Tugba";
-		friends2[3] = "Beryl";
-		friends2[4] = "Ahmet";
-
-		for (int i = 0; i <= friends2.length - 1; i++){
-			System.out.println(friends2[i]);
-		}
 	}
 }
