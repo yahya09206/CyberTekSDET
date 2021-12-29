0e87b959c616698e2b33187116da1385c78222f8
Yahya <yahyahussein91@gmail.com>
2021-12-28 19:13:11 -0800

    create string using literal way

--- day15_Strings/StringIntro.java
+++ day15_Strings/StringIntro.java
@@ -1,11 +1,12 @@
 package day15_Strings;
 
-import java.lang.String;
+//import java.lang.String;
 
 public class StringIntro {
 	public static void main(String[] args){
 
 		//Scanner scan = new Scanner(System.in);
+		String str = "Cybertek"; // string literal
 
 	}
 }
