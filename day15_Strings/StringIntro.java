40ab5bc473b2638d800b0c38f0601c0c6f4cadfa
Yahya <yahyahussein91@gmail.com>
2021-12-28 19:06:04 -0800

    imported scanner class

--- day15_Strings/StringIntro.java
+++ day15_Strings/StringIntro.java
@@ -4,5 +4,6 @@ public class StringIntro {
 	public static void main(String[] args){
 
 
+
 	}
 }
--- day23_Arrays/MyClassmates.java
+++ day23_Arrays/MyClassmates.java
@@ -3,6 +3,7 @@ package day23_Arrays;
 public class MyClassmates {
 	public static void main(String[] args){
 
+		System.out.println("Checking");
 
 	}
 }
