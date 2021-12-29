9201f53f843c107d7743d6297a3341bcf9ab5c9d
Yahya <yahyahussein91@gmail.com>
2021-12-28 19:25:42 -0800

    task for strings

--- day15_Strings/StringIntro.java
+++ day15_Strings/StringIntro.java
@@ -6,8 +6,14 @@ public class StringIntro {
 	public static void main(String[] args){
 
 		//Scanner scan = new Scanner(System.in);
-		String str = "Cybertek"; // string literal
+		String str1 = "Cybertek"; // string literal
 		String str2 = new String("Cybertek"); // new keyword
+
+		System.out.println("--------------------------------------------");
+
+		String s1 = "Cat";
+		String s2 = "Cat";
+		String s3 = "Cat";
 
 	}
 }
