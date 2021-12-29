a23fe62c522741a76e697ece19fe4d0f83c89293
Yahya <yahyahussein91@gmail.com>
2021-12-28 20:51:52 -0800

    code to print first and last char

--- day15_Strings/LongestString.java
+++ day15_Strings/LongestString.java
@@ -1,7 +1,15 @@
 package day15_Strings;
 
+import java.util.Scanner;
+
 public class LongestString {
 	public static void main(String[] args){
 
+		// Lunch tasks program 1
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter sentence");
+		String sentence = scanner.nextLine();
+		char lastChar = sentence.charAt(sentence.length()-1);
+		System.out.println(sentence.charAt(0) + ", " + lastChar);
 	}
 }
