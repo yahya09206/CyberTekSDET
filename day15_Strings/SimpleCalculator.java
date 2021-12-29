b8f3b7d37047d136c69dd52440aea74894ecd27c
Yahya <yahyahussein91@gmail.com>
2021-12-28 19:59:35 -0800

    program for simple calculator

--- day15_Strings/SimpleCalculator.java
+++ day15_Strings/SimpleCalculator.java
@@ -1,7 +1,18 @@
 package day15_Strings;
 
+import java.util.Scanner;
+
 public class SimpleCalculator {
 	public static void main(String[] args){
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int num1 = scanner.nextInt();
+		System.out.println("Enter second number: ");
+		int num2 = scanner.nextInt();
+
+		int addition = num1 + num2;
+
+		System.out.println("result: " + addition);
 	}
 }
