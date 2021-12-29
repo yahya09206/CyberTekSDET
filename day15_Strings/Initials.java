60d38a8937f01269cc837fd67c537352cf17ec42
Yahya <yahyahussein91@gmail.com>
2021-12-28 18:43:13 -0800

    starting over on strings

--- day15_Strings/Initials.java
+++ day15_Strings/Initials.java
@@ -1,20 +1,8 @@
-import java.util.Scanner;
+package day15_Strings;
+
 public class Initials {
 	public static void main(String[] args){
 
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Please enter your first name: ");
-		//String firstName = scan.next();
-		//Get first char of first name
-		char f = scan.next().charAt(0);
 
-		System.out.print("Please enter your last name: ");
-		//String lastName = scan.next();
-		char l = scan.next().charAt(0);
-		scan.close();
-
-		// System.out.println("Initials = " + firstName.charAt(0) + "." + lastName.charAt(0));
-
-		System.out.println("Initials = " + f + "." + l);
 	}
 }
--- day15_Strings/LongestString.java
+++ day15_Strings/LongestString.java
@@ -1,19 +1,7 @@
-import java.util.Scanner;
+package day15_Strings;
+
 public class LongestString {
 	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Please enter two string");
-		String s1 = scan.nextLine();
-		String s2 = scan.nextLine();
 
-		if(s1.length() > s2.length()){
-			System.out.println(s1);
-		}else if(s1.length() == s2.length()){
-			System.out.println("Both strings are equal in length");
-		}else {
-			System.out.println(s2);
-		}
-
-		scan.close();
 	}
 }
