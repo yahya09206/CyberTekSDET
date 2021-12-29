3dd49b57ec87acad7b73f849bab12b078a09cf98
Yahya <yahyahussein91@gmail.com>
2021-12-28 20:36:45 -0800

    printing specific char

--- day15_Strings/StringMethods.java
+++ day15_Strings/StringMethods.java
@@ -7,5 +7,9 @@ public class StringMethods {
 
         char ch = str.charAt(0);
         System.out.println("ch = " + ch);
+
+        String word = "Cybertek School";
+        char ch1 = word.charAt(9);
+        System.out.println(ch1);
     }
 }
