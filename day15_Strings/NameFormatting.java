28fa54b68e0da11b3fd6ca524bf4bbbd9bed7bdc
Yahya <yahyahussein91@gmail.com>
2021-12-29 19:32:31 -0800

    program to format code for name

--- day15_Strings/NameFormatting.java
+++ day15_Strings/NameFormatting.java
@@ -1,8 +1,17 @@
 package day15_Strings;
 
+import java.util.Scanner;
+
 public class NameFormatting {
 	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
 
 
 	}
 }
+/*
+ask the user to enter firs name
+	1. make sure there is no white space
+	2. make sure first character is uppercase and rest of the characters need to be in lowercase
+ */
--- day15_Strings/StringMethodsTwo.java
+++ day15_Strings/StringMethodsTwo.java
@@ -4,6 +4,12 @@ public class StringMethodsTwo {
 	public static void main(String[] args){
 
 		String sentence = "Java Programming Language";
-		String name =
+		// second number stops one short
+		String name = sentence.substring(0,3);
+		System.out.println(name);
+
+		String s2 = "Cybertek School";
+		String name2 = s2.substring(0, 7 + 1);
+		System.out.println(name2);
 	}
 }
