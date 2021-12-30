4d08e6a21bfa45f1c2a28c01a4b3b4c7ecccf74f
Yahya <yahyahussein91@gmail.com>
2021-12-29 20:56:49 -0800

    done with conditional statements

--- day16_StringContinued/StringPractice3.java
+++ day16_StringContinued/StringPractice3.java
@@ -1,3 +1,5 @@
+package day16_StringContinued;
+
 import java.util.Scanner;
 public class StringPractice3 {
 	public static void main(String[] args){
@@ -6,19 +8,17 @@ public class StringPractice3 {
 		System.out.println("Enter a string with 3 letters: ");
 		String word = scan.next();
 
-		if(word.length() == 3){
-			if(word.charAt(1) == 'a'){
+		if (word.length() == 3){
+			if (word.charAt(1) == 'a'){
 				System.out.println("Cool word");
-			}else {
-				System.out.println("Okay word");
 			}
-		}else { // if word has more than 3 or less than 3 characters
-			if(word.length() < 3){
-				System.out.println("Word is too short");
-			}else {
+		}else{
+			if (word.length() > 3){
 				System.out.println("Word is too long");
+			}else if (word.length() < 3){
+				System.out.println("Word is too short");
 			}
 		}
-		scan.close();
+
 	}
 }
