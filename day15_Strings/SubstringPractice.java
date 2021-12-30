6602ae5a74015cc52838063d249092626dd33c8f
Yahya <yahyahussein91@gmail.com>
2021-12-29 19:47:24 -0800

    fixed typo

--- day15_Strings/SubstringPractice.java
+++ day15_Strings/SubstringPractice.java
@@ -3,8 +3,8 @@ package day15_Strings;
 public class SubstringPractice {
 	public static void main(String[] args){
 
-		String sentence = "My favorite series is The Walking Dead";
-		String tvSeries = sentence.substring(22);
+		String sentence = "My favorite TV series is The Walking Dead";
+		String tvSeries = sentence.substring(25);
 		System.out.println(tvSeries);
 
 	}
