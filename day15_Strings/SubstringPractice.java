3532240bf4640e239e4d706d7013f25ccacc1217
Yahya <yahyahussein91@gmail.com>
2021-12-29 19:46:57 -0800

    printing tv series name

--- day15_Strings/SubstringPractice.java
+++ day15_Strings/SubstringPractice.java
@@ -3,5 +3,9 @@ package day15_Strings;
 public class SubstringPractice {
 	public static void main(String[] args){
 
+		String sentence = "My favorite series is The Walking Dead";
+		String tvSeries = sentence.substring(22);
+		System.out.println(tvSeries);
+
 	}
 }
