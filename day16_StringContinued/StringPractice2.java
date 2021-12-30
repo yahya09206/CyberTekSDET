e3ff87b3ab7e0f8e82f686b173eecf79cf8d2752
Yahya <yahyahussein91@gmail.com>
2021-12-29 20:51:25 -0800

    added condition

--- day16_StringContinued/StringPractice2.java
+++ day16_StringContinued/StringPractice2.java
@@ -9,7 +9,11 @@ public class StringPractice2 {
 		String word1 = scan.next();
 		String word2 = scan.next();
 
-		String result = word1.substring(1) + word2.substring(1);
-		System.out.println(result);
+		if (word1.length() >= 2 && word2.length() >= 2){
+			String result = word1.substring(1) + word2.substring(1);
+			System.out.println(result);
+		}else {
+			System.out.println("Too Short");
+		}
 	}
 }
