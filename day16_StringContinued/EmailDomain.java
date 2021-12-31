493a4ee748b006cffd4e5fae2684e7b9e77a28ce
Yahya <yahyahussein91@gmail.com>
2021-12-30 20:21:30 -0800

    program to get domain of emial

--- day16_StringContinued/EmailDomain.java
+++ day16_StringContinued/EmailDomain.java
@@ -1,12 +1,9 @@
+package day16_StringContinued;
+
 public class EmailDomain {
 	public static void main(String[] args){
 
-		String email = "Cybertek@yahoo.com";
+		String email = "Cybertek@gmail.com";
 
-		int beginningIndex = email.indexOf("@") + 1;
-		int endingIndex = email.indexOf(".");
-		String domain = email.substring(beginningIndex, endingIndex);
-
-		System.out.println("domain = " + domain);
 	}
 }
--- day16_StringContinued/WebName.java
+++ day16_StringContinued/WebName.java
@@ -1,8 +1,8 @@
+package day16_StringContinued;
+
 public class WebName {
 	public static void main(String[] args){
 
-		String url = "www.amazon.com";
-		String name = url.substring(url.indexOf(".") + 1, url.lastIndexOf("."));
-		System.out.println(name);
+
 	}
 }
