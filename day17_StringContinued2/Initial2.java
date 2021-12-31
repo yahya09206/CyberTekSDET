0d8094a6977da9c9e0923ce3e386f191ef6bfbf7
Yahya <yahyahussein91@gmail.com>
2021-12-30 21:28:47 -0800

    getting just first name from full name

--- day17_StringContinued2/Initial2.java
+++ day17_StringContinued2/Initial2.java
@@ -1,8 +1,14 @@
 package day17_StringContinued2;
 
+import java.util.Scanner;
+
 public class Initial2 {
 	public static void main(String[] args){
 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your full name:");
+		String fullName = scan.nextLine(); //Cybertek School
 
+		String firstName = fullName.substring(0, fullName.indexOf(" "));
 	}
 }
