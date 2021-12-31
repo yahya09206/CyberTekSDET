17bbe70e97657fc650bc338404e19709cdea8299
Yahya <yahyahussein91@gmail.com>
2021-12-30 20:25:45 -0800

    used lastindex of

--- day16_StringContinued/EmailDomain.java
+++ day16_StringContinued/EmailDomain.java
@@ -3,10 +3,10 @@ package day16_StringContinued;
 public class EmailDomain {
 	public static void main(String[] args){
 
-		String email = "Cybertek@gmail.com";
+		String email = "Cybertek.School@gmail.com";
 
 		int beginningIndex = email.indexOf("@") + 1;
-		int endingIndex = email.indexOf(".");
+		int endingIndex = email.lastIndexOf(".");
 		String domain = email.substring(beginningIndex, endingIndex);
 		System.out.println("domain = " + domain);
 
