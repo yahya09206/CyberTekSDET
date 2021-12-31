1d3c8290b75f8033dad8e00da757a568fb17d84b
Yahya <yahyahussein91@gmail.com>
2021-12-30 20:28:39 -0800

    get name of just the url

--- day16_StringContinued/WebName.java
+++ day16_StringContinued/WebName.java
@@ -3,6 +3,8 @@ package day16_StringContinued;
 public class WebName {
 	public static void main(String[] args){
 
-
+		String url = "www.amazon.com";
+		String name = url.substring(4, 10);
+		System.out.println("name = " + name);
 	}
 }
