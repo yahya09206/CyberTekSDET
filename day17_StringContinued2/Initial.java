80d730a7ce67f2144f47c9d6ff3f768c94c434b8
Yahya <yahyahussein91@gmail.com>
2021-12-30 21:24:25 -0800

    third way using both charat and touppercase together

--- day17_StringContinued2/Initial.java
+++ day17_StringContinued2/Initial.java
@@ -13,8 +13,10 @@ public class Initial {
 
 		//String initial = firstName.substring(0,1).toUpperCase() + "." + lastName.substring(0,1).toUpperCase();
 
-		String initial = firstName.charAt(0) + "." + lastName.charAt(0);
-		initial = initial.toUpperCase();
+//		String initial = firstName.charAt(0) + "." + lastName.charAt(0);
+//		initial = initial.toUpperCase();
+
+		String initial = firstName.toUpperCase().charAt(0) + "." + lastName.toUpperCase().charAt(0);
 
 		System.out.println("initial = " + initial);
 		scan.close();
