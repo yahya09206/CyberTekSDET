4b6ae9cf3d3f0be08d0cd8ff8d0b16a0464e0c22
Yahya <yahyahussein91@gmail.com>
2021-12-30 21:53:32 -0800

    finito

--- day17_StringContinued2/StringMethods3.java
+++ day17_StringContinued2/StringMethods3.java
@@ -3,6 +3,14 @@ package day17_StringContinued2;
 public class StringMethods3 {
 	public static void main(String[] args){
 
+		String sentence = "My favorite programming languages are: C#, Java, Javascript";
+		boolean hasPython = sentence.contains("Python");
+		boolean hasJava = sentence.contains("Java");
+		boolean hasJava2 = sentence.toUpperCase().contains("Java");
+
+		System.out.println("hasJava = " + hasPython);
+		System.out.println("hasJava = " + hasJava);
+		System.out.println("hasJava2 = " + hasJava2);
 
 	}
 }
