fbe1432ae7f245128919d75b9edbaf006cd71aab
Yahya <yahyahussein91@gmail.com>
2021-12-30 21:35:30 -0800

    check if first and last char are same

--- day17_StringContinued2/FirstAndLastChars.java
+++ day17_StringContinued2/FirstAndLastChars.java
@@ -1,8 +1,21 @@
 package day17_StringContinued2;
 
+import java.util.Scanner;
+
 public class FirstAndLastChars {
 	public static void main(String[] args){
 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a word:");
+		String word = scan.nextLine();
 
+		String first = word.substring(0,1);
+		String last = word.substring(word.length()-1);
+
+		if (first.equalsIgnoreCase(last)) {
+			System.out.println("Same");
+		}else {
+			System.out.println("Not the same");
+		}
 	}
 }
