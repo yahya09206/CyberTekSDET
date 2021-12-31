7b7150b0a931f2736eba9ecdc2e03f4c23bde9c1
Yahya <yahyahussein91@gmail.com>
2021-12-30 21:43:18 -0800

    check if login credentials are correct

--- day17_StringContinued2/Login.java
+++ day17_StringContinued2/Login.java
@@ -1,7 +1,25 @@
 package day17_StringContinued2;
 
+import java.util.Scanner;
+
 public class Login {
 	public static void main(String[] args){
+
+		String correctUsername = "Cybertek@gmail.com";
+		String correctPassword = "Cybertek123";
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter username");
+		String userName = scanner.next();
+		System.out.println("Enter password");
+		String password = scanner.next();
+
+		if (userName.equalsIgnoreCase(correctUsername) && password.equalsIgnoreCase(correctPassword)){
+			System.out.println("Successfully logged in");
+		}else {
+			System.out.println("Wrong credentials");
+		}
+
 
 
 	}
