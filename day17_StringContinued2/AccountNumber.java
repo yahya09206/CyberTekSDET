7da91587e71ef170805d394b266aeb75334833af
Yahya <yahyahussein91@gmail.com>
2021-12-30 21:08:42 -0800

    converting to switch

--- day17_StringContinued2/AccountNumber.java
+++ day17_StringContinued2/AccountNumber.java
@@ -7,5 +7,22 @@ public class AccountNumber {
 
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter your account number:");
+		String account = scan.next();
+
+		/*
+		if (account.length() == 7 || account.length() == 10){
+			if (account.charAt(0) == '2' && account.length() == 7){
+				System.out.println("Valid Account");
+			}else if(account.charAt(0) == '5' && account.length() == 10){
+				System.out.println("Valid Account");
+			}else {
+				System.out.println("Invalid account");
+			}
+		}else {
+			System.out.println("Invalid Account");
+		}
+		 */
+
+
 	}
 }
