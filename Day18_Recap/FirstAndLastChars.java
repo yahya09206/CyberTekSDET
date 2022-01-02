7bf8fd49e8d73079bb0eb50fc44a4df39f82bc13
Yahya <yahyahussein91@gmail.com>
2022-01-01 20:22:32 -0800

    substring way is working

--- Day18_Recap/FirstAndLastChars.java
+++ Day18_Recap/FirstAndLastChars.java
@@ -10,7 +10,15 @@ public class FirstAndLastChars {
         String word1 = scan.next();
         String word2 = scan.next();
 
+        /*
         if (word1.charAt(word1.length() - 1) == word2.charAt(0)){
+            System.out.println(word1 + word2.substring(1));
+        }else {
+            System.out.println(word1 + word2);
+        }
+         */
+        // re do with substring
+        if (word1.substring(word1.length() - 1).equalsIgnoreCase(word2.substring(0,1))){
             System.out.println(word1 + word2.substring(1));
         }else {
             System.out.println(word1 + word2);
