076b53b92122f1a68808b86c601982ca4a8d72cc
Yahya <yahyahussein91@gmail.com>
2022-01-01 20:31:27 -0800

    chekcing if contains digit

--- Day18_Recap/DigitLetterSpecialChar.java
+++ Day18_Recap/DigitLetterSpecialChar.java
@@ -8,6 +8,11 @@ public class DigitLetterSpecialChar {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a word:");
         String word = scan.next();
+        char f = word.charAt(0);
+
+        if (f >= '0' && f <= '9'){
+            System.out.println("First character is a digit");
+        }
 
     }
 }
