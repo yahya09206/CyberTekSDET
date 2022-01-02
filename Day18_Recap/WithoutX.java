558b2ede9f7101e0b04028b2021072364a3df773
Yahya <yahyahussein91@gmail.com>
2022-01-01 20:05:23 -0800

    printing word correctyl

--- Day18_Recap/WithoutX.java
+++ Day18_Recap/WithoutX.java
@@ -1,4 +1,18 @@
 package Day18_Recap;
 
+import java.util.Scanner;
+
 public class WithoutX {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter word: ");
+        String word = scan.next();
+
+        if (word.startsWith("x")){
+            System.out.println(word.substring(1));
+        }else {
+            System.out.println(word);
+        }
+    }
 }
