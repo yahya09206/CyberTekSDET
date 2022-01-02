a1566f8e7b290b1d21aa1abac44655f87cfd54bf
Yahya <yahyahussein91@gmail.com>
2022-01-01 20:30:37 -0800

    prompting user for word

--- Day18_Recap/DigitLetterSpecialChar.java
+++ Day18_Recap/DigitLetterSpecialChar.java
@@ -1,7 +1,13 @@
 package Day18_Recap;
 
+import java.util.Scanner;
+
 public class DigitLetterSpecialChar {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word:");
+        String word = scan.next();
 
     }
 }
