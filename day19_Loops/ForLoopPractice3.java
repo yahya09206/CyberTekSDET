7564c67b4e26dba3e67f957ddd9a535469181fa4
Yahya <yahyahussein91@gmail.com>
2022-01-02 19:47:07 -0800

    program to print if divisible by 3 or 5 or both or neither

---
+++ day19_Loops/ForLoopPractice3.java
@@ -0,0 +1,10 @@
+package day19_Loops;
+
+public class ForLoopPractice3 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 50 ; i++) {
+            System.out.println("Push Up " + i);
+        }
+    }
+}
