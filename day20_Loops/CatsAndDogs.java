b691253a3115d8294566ffb207768f3940ba3ff0
Yahya <yahyahussein91@gmail.com>
2022-01-02 22:00:27 -0800

    getting count of cat and dog

--- day20_Loops/CatsAndDogs.java
+++ day20_Loops/CatsAndDogs.java
@@ -6,13 +6,18 @@ public class CatsAndDogs {
         String sentence = "Cat Dog Dog Cat";
 
         int numberOfCats = 0;
+        int numberofDogs = 0;
 
         for (int i = 0; i <= sentence.length() - 3; i++) {
             String each = sentence.substring(i, i + 3);
             if (each.equals("Cat")){
                 numberOfCats++;
             }
+            if (each.equals("Dog")){
+                numberofDogs++;
+            }
         }
         System.out.println("numberOfCats = " + numberOfCats);
+        System.out.println("numberofDogs = " + numberofDogs);
     }
 }
