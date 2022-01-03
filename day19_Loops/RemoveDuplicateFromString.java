9013c60c9b8295a8cdf7da8ad15ea662d2358d7d
Yahya <yahyahussein91@gmail.com>
2022-01-02 20:28:00 -0800

    removing dups from string

--- day19_Loops/RemoveDuplicateFromString.java
+++ day19_Loops/RemoveDuplicateFromString.java
@@ -3,6 +3,14 @@ package day19_Loops;
 public class RemoveDuplicateFromString {
 	public static void main(String[] args){
 
+		String word = "ababababab";
+		String result = "";
 
+		for (int i = 0; i <= word.length() - 1; i++) {
+			if (!result.contains("" + word.charAt(i))){
+				result += word.charAt(i);
+			}
+		}
+		System.out.println("result = " + result);
 	}
 }
