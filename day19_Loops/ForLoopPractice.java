a55bcd88dda3e6dcdbb6213f2cfea4188b614f08
Yahya <yahyahussein91@gmail.com>
2022-01-02 19:20:38 -0800

    printing odd by skipping every other number

--- day19_Loops/ForLoopPractice.java
+++ day19_Loops/ForLoopPractice.java
@@ -13,5 +13,13 @@ public class ForLoopPractice {
                 System.out.print(i + " ");
             }
         }
+        System.out.println();
+        System.out.println("----------------------------------------------");
+        for (int i = 1; i <= 100; i+=2){
+            if (i % 2 != 0){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }
 }
