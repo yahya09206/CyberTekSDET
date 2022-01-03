16dbf533f629a22962fa969849f8ca050200a772
Yahya <yahyahussein91@gmail.com>
2022-01-02 18:58:56 -0800

    printing hello world with loop and without

---
+++ day19_Loops/ForLoopIntro.java
@@ -0,0 +1,17 @@
+package day19_Loops;
+
+public class ForLoopIntro {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        for (int i = 0; i <= 5; i++){
+            System.out.println("Hello World");
+        }
+
+    }
+}
