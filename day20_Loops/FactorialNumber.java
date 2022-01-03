fd8e68b4003b7e0d570a82edf7e491615428bd7f
Yahya <yahyahussein91@gmail.com>
2022-01-02 21:23:57 -0800

    printing factorial

--- day20_Loops/FactorialNumber.java
+++ day20_Loops/FactorialNumber.java
@@ -1,7 +1,18 @@
 package day20_Loops;
 
+import java.util.Scanner;
+
 public class FactorialNumber {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= num; i++){
+            result *= i;
+        }
+        System.out.println(result);
 
     }
 }
