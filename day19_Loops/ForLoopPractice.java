7d7629945af1ead8da0b0eb2f68ae56a651fc4a0
Yahya <yahyahussein91@gmail.com>
2022-01-02 19:17:18 -0800

    printing odd from 1-100

--- day19_Loops/ForLoopPractice.java
+++ day19_Loops/ForLoopPractice.java
@@ -6,5 +6,12 @@ public class ForLoopPractice {
         for (int i = 0; i <= 1000; i++){
             System.out.print(i + " ");
         }
+
+        System.out.println("----------------------------------------------");
+        for (int i = 0; i <= 100; i++){
+            if (i % 2 != 0){
+                System.out.print(i + " ");
+            }
+        }
     }
 }
