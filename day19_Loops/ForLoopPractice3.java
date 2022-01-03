8f15c4e2afc3a9891163a51259dcc200338c0258
Yahya <yahyahussein91@gmail.com>
2022-01-02 19:56:55 -0800

    printing sum from 1-100

--- day19_Loops/ForLoopPractice3.java
+++ day19_Loops/ForLoopPractice3.java
@@ -6,5 +6,21 @@ public class ForLoopPractice3 {
         for (int i = 1; i <= 8 ; i++) {
             System.out.println("* * * * * *");
         }
+
+        System.out.println("-------------------------------------------");
+        System.out.println("* * * * * *");
+        for (int i = 1; i <= 8; i++){
+            System.out.println("*         *");
+        }
+        System.out.println("* * * * * *");
+
+        System.out.println("-------------------------------------------");
+
+        int sum = 0;
+
+        for (int i = 1; i < 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
