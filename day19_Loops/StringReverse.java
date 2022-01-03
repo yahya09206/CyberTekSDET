43e694348a392abc4b7002b766a4252a7ed8b8e7
Yahya <yahyahussein91@gmail.com>
2022-01-02 20:16:59 -0800

    reversing hard coded name

--- day19_Loops/StringReverse.java
+++ day19_Loops/StringReverse.java
@@ -3,6 +3,13 @@ package day19_Loops;
 public class StringReverse {
 	public static void main(String[] args){
 
+		String name = "Muhtar";
+		String result = "";
+
+		for (int i = name.length() - 1; i >= 0; i--) {
+			result += name.charAt(i);
+		}
+		System.out.println("result = " + result);
 
 	}
 }
