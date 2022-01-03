78201543180652446aeb97696db25d2ed04b0e15
Yahya <yahyahussein91@gmail.com>
2022-01-02 20:17:59 -0800

    reversing name provided by scanner

--- day19_Loops/StringReverse.java
+++ day19_Loops/StringReverse.java
@@ -1,9 +1,12 @@
 package day19_Loops;
 
+import java.util.Scanner;
+
 public class StringReverse {
 	public static void main(String[] args){
-
-		String name = "Muhtar";
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter name");
+		String name = scan.next();
 		String result = "";
 
 		for (int i = name.length() - 1; i >= 0; i--) {
