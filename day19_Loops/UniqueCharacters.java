e3abfd893a008fc50f5eee52f646f537ee175807
Yahya <yahyahussein91@gmail.com>
2022-01-02 20:40:38 -0800

    testing with diff values

--- day19_Loops/UniqueCharacters.java
+++ day19_Loops/UniqueCharacters.java
@@ -3,10 +3,10 @@ package day19_Loops;
 public class UniqueCharacters {
 	public static void main(String[] args){
 
-		String word = "aabcc";
+		String word = "aaabcccdeeeeefggggggh";
 		String result = "";
 
-		for (int i = 0; i < word.length() - 1; i++) {
+		for (int i = 0; i <= word.length() - 1; i++) {
 			char ch = word.charAt(i);
 			boolean isUnique = word.indexOf(ch) == word.lastIndexOf(ch);
 
