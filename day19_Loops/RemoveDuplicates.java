63608d13a26619ca1ba2d7c2ce88c159737219c6
Yahya <yahyahussein91@gmail.com>
2022-01-02 18:37:17 -0800

    code to remove duplicates from string

--- day19_Loops/RemoveDuplicates.java
+++ day19_Loops/RemoveDuplicates.java
@@ -1,10 +1,12 @@
 package day19_Loops;
 
+import java.util.Scanner;
+
 public class RemoveDuplicates {
 
     public static void main(String[] args) {
 
-
+        Scanner scan = new Scanner(System.in);
 
     }
 }
