92478335aeebc31bec42737c4850ed70b4a9a18a
Yahya <yahyahussein91@gmail.com>
2022-01-02 20:21:44 -0800

    code for finding palindrome

--- day19_Loops/Palindrome.java
+++ day19_Loops/Palindrome.java
@@ -1,8 +1,22 @@
 package day19_Loops;
 
+import java.util.Scanner;
+
 public class Palindrome {
 	public static void main(String[] args){
 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter name");
+		String name = scan.next();
+		String result = "";
 
+		for (int i = name.length() - 1; i >= 0; i--) {
+			result += name.charAt(i);
+		}
+		if (result.equals(name)){
+			System.out.println("Is palindrome");
+		}else {
+			System.out.println("Not palindrome");
+		}
 	}
 }
