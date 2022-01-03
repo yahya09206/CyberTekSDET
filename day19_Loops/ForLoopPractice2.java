0805e6846db7d8579325ab7200abe91b8de33631
Yahya <yahyahussein91@gmail.com>
2022-01-02 19:26:30 -0800

    printing letters of alphabet

---
+++ day19_Loops/ForLoopPractice2.java
@@ -0,0 +1,10 @@
+package day19_Loops;
+
+public class ForLoopPractice2 {
+    public static void main(String[] args) {
+
+        for (int i = 65; i <= 90; i++){
+            System.out.print((char)i + " ");
+        }
+    }
+}
