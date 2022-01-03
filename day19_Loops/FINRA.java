e22a70f7808f579c10867e1a877458cf701678a0
Yahya <yahyahussein91@gmail.com>
2022-01-02 19:51:33 -0800

    switched to using ternary

--- day19_Loops/FINRA.java
+++ day19_Loops/FINRA.java
@@ -3,16 +3,21 @@ package day19_Loops;
 public class FINRA {
     public static void main(String[] args) {
 
+        String result = "";
+
+
         for (int i = 1; i <= 100; i++) {
+            //result += (i % 3 == 0 && i % 5 == 0)? "FINRA " :(i % 3 == 0)? "FIN " :(i % 5 == 0)? "RA " : i + " ";
             if (i % 3 == 0 && i % 5 == 0){
-                System.out.print("FINRA ");
+                result += "FINRA ";
             }else if (i % 3 == 0){
-                System.out.print("FIN ");
+                result += "FIN ";
             }else if (i % 5 == 0){
-                System.out.print("RA ");
+                result += "RA ";
             }else {
-                System.out.print(i + " ");
+                result += i + " ";
             }
         }
+        System.out.println(result);
     }
 }
