3046aa67a59dbbc7220b7ef6fc2c7bbc56524f06
Yahya <yahyahussein91@gmail.com>
2022-01-03 19:38:15 -0800

    storing each char into its place

--- day21_WhileDoWhile/LetterDigitSpecialChar.java
+++ day21_WhileDoWhile/LetterDigitSpecialChar.java
@@ -9,6 +9,21 @@ public class LetterDigitSpecialChar {
         String digits = "";
         String specialChars = "";
 
+        for (int i = 0; i <= input.length() - 1; i++) {
+            char each = input.charAt(i);
+            if (each >= 'A' && each <= 'Z'){
+                letters += each;
+            }else if (each >= 'a' && each <= 'z'){
+                letters += each;
+            }else if (each >= '0' && each <= '9'){
+                digits += each;
+            }else {
+                specialChars += each;
+            }
+        }
+        System.out.println("letters = " + letters);
+        System.out.println("digits = " + digits);
+        System.out.println("specialChars = " + specialChars);
 
     }
 }
