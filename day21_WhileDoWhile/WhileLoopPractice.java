0e15cc06127f337ce3f0a7daf4dfd0f5625bb514
Yahya <yahyahussein91@gmail.com>
2022-01-03 19:52:20 -0800

    more while loops

--- day21_WhileDoWhile/WhileLoopPractice.java
+++ day21_WhileDoWhile/WhileLoopPractice.java
@@ -3,6 +3,13 @@ package day21_WhileDoWhile;
 public class WhileLoopPractice {
 	public static void main(String[] args){
 
+		String sentence = "Java is fun, Java is cool";
+		int countJava = 0;
+
+		while (sentence.contains("Java")){
+			countJava++;
+			sentence = sentence.replaceFirst("Java", " ");
+		}
 
 	}
 }
