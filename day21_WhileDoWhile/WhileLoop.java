20e8980309c0edfb937577346ef6ec800a955080
Yahya <yahyahussein91@gmail.com>
2022-01-03 19:47:34 -0800

    whileloops iterations

--- day21_WhileDoWhile/WhileLoop.java
+++ day21_WhileDoWhile/WhileLoop.java
@@ -3,6 +3,15 @@ package day21_WhileDoWhile;
 public class WhileLoop {
 	public static void main(String[] args){
 
+		if (true) {
+			System.out.println("Hello batch 23");
+		}
 
+		System.out.println("---------------------------------------");
+		//while loop is a repeated if statement
+
+		while (true){
+			System.out.println("Hello batch 23");
+		}
 	}
 }
