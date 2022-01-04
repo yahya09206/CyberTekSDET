148a586af3cdc3fe283a1d82a4b09ec03002564e
Yahya <yahyahussein91@gmail.com>
2022-01-03 20:38:21 -0800

    long way to check for credentials

--- day22_Recap/Credentials.java
+++ day22_Recap/Credentials.java
@@ -9,6 +9,24 @@ public class Credentials {
 		String correctPassword = "cybertek12345";
 
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your username");
+		String username = scanner.next();
+
+		System.out.println("Enter your password");
+		String password = scanner.next();
+
+		if (username.equals(correctUsername) && password.equals(correctPassword)){
+			System.out.println("Logged in");
+		}else {
+			for (int i = 0; i <= 3; i++) {
+				System.out.println("Invalid credentials");
+				System.out.println("Enter your username");
+				username = scanner.next();
+
+				System.out.println("Enter your password");
+				password = scanner.next();
+			}
+		}
 
 	}
 }
