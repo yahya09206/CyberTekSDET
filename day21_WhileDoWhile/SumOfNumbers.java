ac95c2c9299289a19ecb6ff35ea012be8830bc50
Yahya <yahyahussein91@gmail.com>
2022-01-03 20:13:59 -0800

    printing even numbers using while loop instead of foor

--- day21_WhileDoWhile/SumOfNumbers.java
+++ day21_WhileDoWhile/SumOfNumbers.java
@@ -6,15 +6,19 @@ public class SumOfNumbers {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
+		int sum = 0;
+		int n = 0;
 
-		while(true){
+		while(n >= 0){
 			System.out.println("Enter a number:");
-			int n = scan.nextInt();
+			n = scan.nextInt();
 
 			if (n < 0){
 				break;
 			}
-		}
 
+			sum += n;
+		}
+		System.out.println("sum = " + sum);
 	}
 }
--- day21_WhileDoWhile/WhileLoopPractice2.java
+++ day21_WhileDoWhile/WhileLoopPractice2.java
@@ -3,5 +3,15 @@ package day21_WhileDoWhile;
 public class WhileLoopPractice2 {
 	public static void main(String[] args){
 
+		int num = 1;
+		while (num <= 10){
+
+			if (num % 2 == 0){
+				System.out.print(num + " ");
+			}
+
+			num++;
+		}
+		System.out.println();
 	}
 }
