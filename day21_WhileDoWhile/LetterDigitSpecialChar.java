8a46bf8003a0eeb5bbd2f897d250f7bcb500222f
Yahya <yahyahussein91@gmail.com>
2022-01-03 19:34:39 -0800

    get digits letters and specials chars from string

---
+++ day21_WhileDoWhile/LetterDigitSpecialChar.java
@@ -0,0 +1,14 @@
+package day21_WhileDoWhile;
+
+public class LetterDigitSpecialChar {
+    public static void main(String[] args) {
+
+        String input = "mn@#123Ab!";
+
+        String letters = "";
+        String digits = "";
+        String specialChars = "";
+
+
+    }
+}
