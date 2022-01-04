00767f933719bfbbe6bc0cd000d89d3354092d12
Yahya <yahyahussein91@gmail.com>
2022-01-03 20:07:06 -0800

    find sum of numbers using while loop

--- day21_WhileDoWhile/SumOfNumbers.java
+++ day21_WhileDoWhile/SumOfNumbers.java
@@ -1,8 +1,20 @@
 package day21_WhileDoWhile;
 
+import java.util.Scanner;
+
 public class SumOfNumbers {
 	public static void main(String[] args){
 
+		Scanner scan = new Scanner(System.in);
+
+		while(true){
+			System.out.println("Enter a number:");
+			int n = scan.nextInt();
+
+			if (n < 0){
+				break;
+			}
+		}
 
 	}
 }
