6725f700b592e34cae42f2def037bef74e47167c
Yahya <yahyahussein91@gmail.com>
2022-01-03 18:45:51 -0800

    added condition for negative numbers

--- day21_WhileDoWhile/MultiplyTwoNumbers.java
+++ day21_WhileDoWhile/MultiplyTwoNumbers.java
@@ -1,5 +1,20 @@
 package day21_WhileDoWhile;
 
 public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
 
+        int a = 10;
+        int b = -5;
+        int result = 0;
+
+        // for negative
+//        if (b < 0){
+//            b *= -1;
+//        }
+
+        for (int i = 0; i < b; i++) {
+            result += a;
+        }
+        System.out.println("result = " + result);
+    }
 }
