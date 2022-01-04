7dea3d1cc02b4d96707d3260c98110dca5449619
Yahya <yahyahussein91@gmail.com>
2022-01-03 19:09:25 -0800

    result vars for which each num is divisible by

--- day21_WhileDoWhile/Numbers.java
+++ day21_WhileDoWhile/Numbers.java
@@ -1,7 +1,24 @@
 package day21_WhileDoWhile;
 
+import java.util.Scanner;
+
 public class Numbers {
     public static void main(String[] args) {
 
+        System.out.println("Enter a number");
+        int n = new Scanner(System.in).nextInt();
+
+        if (n <= 0){
+            System.exit(0);
+        }
+
+        String result1 = "Divisble by 15";
+        String result2 = "Divisible by 5";
+        String result3 = "Divisible by 3";
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("Hello");
     }
 }
