670d3843dd791cdbe30b27a67fcbb85aac390934
Yahya <yahyahussein91@gmail.com>
2022-01-04 20:07:30 -0800

    checking for proper user input

--- day22_Recap/SumOfTwoNumbers.java
+++ day22_Recap/SumOfTwoNumbers.java
@@ -6,18 +6,27 @@ public class SumOfTwoNumbers {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
-		String answer = scan.next();
+		String answer = "";
 
 		do {
 
-			System.out.println("Enter two numbers");
+			System.out.println("Enter two numbers:");
 			int num1 = scan.nextInt();
 			int num2 = scan.nextInt();
 
 			System.out.println("Addition: " + (num1 + num2));
 			System.out.println("Do you want to continue?");
-			answer = scan.next();
-		}while (answer.equals("yes"));
+			answer = scan.next().toLowerCase();
+
+			if ((answer.equals("yes") || answer.equals("no"))){
+				System.out.println("Invalid answer, Please re-enter");
+				System.out.println("Do you want to continue?");
+				answer = scan.next().toLowerCase();
+			}
+
+		}while (answer.equalsIgnoreCase("yes"));
+
+		scan.close();
 
 	}
 }
