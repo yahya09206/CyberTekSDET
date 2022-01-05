cc0530a77ba6c41fccc4c41d77c2b04f3a6856a3
Yahya <yahyahussein91@gmail.com>
2022-01-04 21:53:49 -0800

    remove dups and get count of each char

---
+++ day23_Arrays/FrequencyOfEachCharacter.java
@@ -0,0 +1,12 @@
+package day23_Arrays;
+
+public class FrequencyOfEachCharacter {
+    public static void main(String[] args) {
+
+        String str = "aabccc";
+        String removeDup = "";
+        String result = "";
+
+
+    }
+}
