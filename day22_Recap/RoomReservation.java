4ea9d1345dd4ca80a3300e3b9f269ba9486bd144
Yahya <yahyahussein91@gmail.com>
2022-01-04 20:46:13 -0800

    printing total price

--- day22_Recap/RoomReservation.java
+++ day22_Recap/RoomReservation.java
@@ -1,7 +1,34 @@
 package day22_Recap;
 
+import java.util.Scanner;
+
 public class RoomReservation {
 	public static void main(String[] args){
 
+		Scanner scan = new Scanner(System.in);
+		int totalPrice = 0;
+
+		System.out.println("Welcome to Cybertek Hotel");
+		System.out.println("Would you like to reserve a room?");
+		String answer = scan.nextLine().toLowerCase();
+
+		if (answer.equals("yes")){
+			System.out.println("Which bed type do you want?");
+			String type = scan.nextLine().toLowerCase();
+			if (type.equals("king bed")){
+				totalPrice += 120;
+			}else if (type.equals("queen bed")){
+				totalPrice += 100;
+			}else if (type.equals("single bed")){
+				totalPrice += 80;
+			}else {
+				System.out.println("Invalid room is selected");
+			}
+
+			System.out.println("Do you want to reserve another room?");
+			answer = scan.nextLine().toLowerCase();
+		}
+
+		System.out.println("Your total price is: " + totalPrice);
 	}
 }
