b81d107a71c05bceeecbcd9acc9f72e19f3d4905
Yahya <yahyahussein91@gmail.com>
2022-01-04 20:04:23 -0800

    switch to do while so it can run at least once

--- day22_Recap/SumOfTwoNumbers.java
+++ day22_Recap/SumOfTwoNumbers.java
@@ -6,11 +6,18 @@ public class SumOfTwoNumbers {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter two numbers");
-		int num1 = scan.nextInt();
-		int num2 = scan.nextInt();
+		String answer = scan.next();
 
-		System.out.println("Addition: " + (num1 + num2));
-		System.out.println("Do you want to continue?");
+		do {
+
+			System.out.println("Enter two numbers");
+			int num1 = scan.nextInt();
+			int num2 = scan.nextInt();
+
+			System.out.println("Addition: " + (num1 + num2));
+			System.out.println("Do you want to continue?");
+			answer = scan.next();
+		}while (answer.equals("yes"));
+
 	}
 }
