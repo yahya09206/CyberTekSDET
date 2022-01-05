d0815c6d126f8ae317753d271534676a4927d676
Yahya <yahyahussein91@gmail.com>
2022-01-04 20:16:20 -0800

    simple calculator program

--- day22_Recap/Calculator.java
+++ day22_Recap/Calculator.java
@@ -1,8 +1,14 @@
 package day22_Recap;
 
+import java.util.Scanner;
+
 public class Calculator {
 	public static void main(String[] args){
 
+		Scanner scan = new Scanner(System.in);
+		int num1;
+		int num2;
+		char operator;
 
 	}
 }
