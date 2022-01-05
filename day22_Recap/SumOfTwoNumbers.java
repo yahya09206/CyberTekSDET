aaacc81c6c9c08fcec1961d568f4e0f09ce63e89
Yahya <yahyahussein91@gmail.com>
2022-01-04 20:01:01 -0800

    program to continously find sum of two numbers

--- day22_Recap/SumOfTwoNumbers.java
+++ day22_Recap/SumOfTwoNumbers.java
@@ -1,7 +1,11 @@
 package day22_Recap;
 
+import java.util.Scanner;
+
 public class SumOfTwoNumbers {
 	public static void main(String[] args){
 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter two numbers");
 	}
 }
