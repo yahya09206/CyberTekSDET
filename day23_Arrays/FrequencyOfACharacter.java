cec9f55579820d445bd5fe27650b1881674f939b
Yahya <yahyahussein91@gmail.com>
2022-01-04 21:36:26 -0800

    setting up for loop

--- day23_Arrays/FrequencyOfACharacter.java
+++ day23_Arrays/FrequencyOfACharacter.java
@@ -3,5 +3,14 @@ package day23_Arrays;
 public class FrequencyOfACharacter {
     public static void main(String[] args) {
 
+        String str = "aabcccd";
+
+        char ch = 'c';
+        int frequency = 0;
+
+        for (int i = 0; i <= str.length() - 1; i++) {
+
+        }
+
     }
 }
