19026def7975d1fd30fc3593c02290e3bc57864d
Yahya <yahyahussein91@gmail.com>
2022-01-06 20:15:02 -0800

    printing max number from array

--- day23_Arrays/MaximumMinimumNumber.java
+++ day23_Arrays/MaximumMinimumNumber.java
@@ -3,6 +3,14 @@ package day23_Arrays;
 public class MaximumMinimumNumber {
     public static void main(String[] args) {
 
+        int[] maxNumber = {1,60,22,44,5};
+        int max = 0;
 
+        for (int i = 0; i <= maxNumber.length - 1; i++){
+            if (maxNumber[i] > max){
+                max = maxNumber[i];
+            }
+        }
+        System.out.println(max);
     }
 }
