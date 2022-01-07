8e155604648e3dac625a58668bb63a58cea25da4
Yahya <yahyahussein91@gmail.com>
2022-01-06 19:37:45 -0800

    array of strings containing names

--- day23_Arrays/ArraysIntro.java
+++ day23_Arrays/ArraysIntro.java
@@ -3,5 +3,7 @@ package day23_Arrays;
 public class ArraysIntro {
     public static void main(String[] args) {
 
+        String[] group2 = {"Yahya", "Gulistan", "Kadir", "Feruza", "Nazli", "Ahmet", "Igor"};
+
     }
 }
--- day23_Arrays/MyClassmates.java
+++ day23_Arrays/MyClassmates.java
@@ -3,7 +3,5 @@ package day23_Arrays;
 public class MyClassmates {
 	public static void main(String[] args){
 
-		System.out.println("Checking");
-
 	}
 }
