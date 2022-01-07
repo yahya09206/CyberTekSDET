82f7915402e76a03bd7e8eead67d6eb7de0ab28f
Yahya <yahyahussein91@gmail.com>
2022-01-06 19:55:08 -0800

    array to store days of week

--- day23_Arrays/Days.java
+++ day23_Arrays/Days.java
@@ -3,5 +3,7 @@ package day23_Arrays;
 public class Days {
     public static void main(String[] args) {
 
+        String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
     }
 }
