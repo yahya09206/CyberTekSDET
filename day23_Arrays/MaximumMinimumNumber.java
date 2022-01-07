4e044d23ed1b756ade10f92ecb95f3c6bed847f9
Yahya <yahyahussein91@gmail.com>
2022-01-06 20:17:03 -0800

    working on finding min

--- day23_Arrays/MaximumMinimumNumber.java
+++ day23_Arrays/MaximumMinimumNumber.java
@@ -5,12 +5,16 @@ public class MaximumMinimumNumber {
 
         int[] maxNumber = {1,60,22,44,5};
         int max = 0;
+        int min = 0;
 
         for (int i = 0; i <= maxNumber.length - 1; i++){
             if (maxNumber[i] > max){
                 max = maxNumber[i];
+            }else if (maxNumber[i] < min){
+                min = maxNumber[i];
             }
         }
-        System.out.println(max);
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
     }
 }
