bbb59868f3dca01b832f1956985c19d89ed890de
Yahya <yahyahussein91@gmail.com>
2022-01-06 19:59:01 -0800

    declare array of size 5

--- day23_Arrays/Days.java
+++ day23_Arrays/Days.java
@@ -1,9 +1,16 @@
 package day23_Arrays;
 
+import java.util.Scanner;
+
 public class Days {
     public static void main(String[] args) {
 
         String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+        System.out.println("Enter a number");
+        int n = new Scanner(System.in).nextInt();
+
+        System.out.println(days[n-1]);
 
     }
 }
--- day23_Arrays/MyClassmates.java
+++ day23_Arrays/MyClassmates.java
@@ -3,5 +3,6 @@ package day23_Arrays;
 public class MyClassmates {
 	public static void main(String[] args){
 
+		String[] classMates = new String[5];
 	}
 }
