526f6e7a22602d09801dee13cccc2b18810fa0fd
Yahya <yahyahussein91@gmail.com>
2022-01-06 20:56:50 -0800

    size of array based on how many numbers

--- day23_Arrays/AverageNumbers.java
+++ day23_Arrays/AverageNumbers.java
@@ -1,7 +1,12 @@
 package day23_Arrays;
 
+import java.util.Scanner;
+
 public class AverageNumbers {
 	public static void main(String[] args){
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("How many numbers do you want to enter?");
+		int[] size = new int[scanner.nextInt()];
 	}
 }
