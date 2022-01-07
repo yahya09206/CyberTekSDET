4b2d13e9445913da7f1e00d62a82517f8ceb6eb6
Yahya <yahyahussein91@gmail.com>
2022-01-06 20:58:06 -0800

    assigning values based on user input

--- day23_Arrays/AverageNumbers.java
+++ day23_Arrays/AverageNumbers.java
@@ -8,5 +8,11 @@ public class AverageNumbers {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("How many numbers do you want to enter?");
 		int[] size = new int[scanner.nextInt()];
+		int sum = 0;
+
+		for (int i = 0; i <= size.length; i++){
+			System.out.println("Enter next number");
+			size[i] = scanner.nextInt();
+		}
 	}
 }
