ced892533ab0fc9d3ebd7b2b6d80388945cbda43
Yahya <yahyahussein91@gmail.com>
2022-01-06 19:45:36 -0800

    prompting user for which month to print

--- day23_Arrays/Months.java
+++ day23_Arrays/Months.java
@@ -1,8 +1,16 @@
 package day23_Arrays;
 
+import java.util.Scanner;
+
 public class Months {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter which month to print");
+        int month = scan.nextInt();
+        String[] nameOfMonth = {"January", "February", "March", "April", "May"};
+
+        System.out.println(nameOfMonth[month]);
 
 
     }
