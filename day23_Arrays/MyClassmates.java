29a03d4d05db9a06d57aef38aec674b3b9f93d24
Yahya <yahyahussein91@gmail.com>
2022-01-06 20:06:36 -0800

    printing values of array

--- day23_Arrays/MyClassmates.java
+++ day23_Arrays/MyClassmates.java
@@ -5,5 +5,9 @@ public class MyClassmates {
 
 		String[] classMates = new String[5];
 		System.out.println("Size:" + classMates.length);
+
+		for (int i = 0; i <= classMates.length; i++) {
+			System.out.println(classMates[i]);
+		}
 	}
 }
