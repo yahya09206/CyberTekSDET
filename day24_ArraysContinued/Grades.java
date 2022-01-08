a617efd5fbb3288ff37616ae132cdf8591d4e5e7
Yahya <yahyahussein91@gmail.com>
2022-01-07 21:12:16 -0800

    manually adding grades

--- day24_ArraysContinued/Grades.java
+++ day24_ArraysContinued/Grades.java
@@ -6,5 +6,19 @@ public class Grades {
 		String[] names = {"Anna", "Nancy", "Sarah"};
 		int[] scores = {90, 75, 80};
 		char[] grades = new char[names.length];
+
+		int score = scores[0];
+
+		if (score >= 90){
+			grades[0] = 'A';
+		}else if (score >= 80){
+			grades[0] = 'B';
+		}else if (score >= 70){
+			grades[0] = 'C';
+		}else if (score >= 60){
+			grades[0] = 'D';
+		}else {
+			grades[0] = 'F';
+		}
 	}
 }
