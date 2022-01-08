8c44e6492f2a6e2426a7b647a4bca2b60fdf0adb
Yahya <yahyahussein91@gmail.com>
2022-01-07 21:32:29 -0800

    added outer loop to find rest of unique numbers

--- day24_ArraysContinued/FrequencyOfElements.java
+++ day24_ArraysContinued/FrequencyOfElements.java
@@ -5,10 +5,20 @@ public class FrequencyOfElements {
 
 		int[] arr = {1,1,2,3,3,4};
 
-		int element = arr[0];
-		int count = 0;
+		for (int j = 0; j <= arr.length - 1; j++) {
+			int element = arr[j];
+			int count = 0;
 
+			for (int i = 0; i <= arr.length - 1 ; i++) {
+				// compare element to current index
+				if (element == arr[i]){
+					count++;
+				}
+			}
 
-
+			if (count == 1){ // check if element is unique
+				System.out.println(element);
+			}
+		}
 	}
 }
