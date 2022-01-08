c7952d64cde1d0911440cb0473bc04b54de0b3af
Yahya <yahyahussein91@gmail.com>
2022-01-07 21:28:32 -0800

    finding unique elements inside array

--- day24_ArraysContinued/FrequencyOfElements.java
+++ day24_ArraysContinued/FrequencyOfElements.java
@@ -3,5 +3,12 @@ package day24_ArraysContinued;
 public class FrequencyOfElements {
 	public static void main(String[] args){
 
+		int[] arr = {1,1,2,3,3,4};
+
+		int element = arr[0];
+		int count = 0;
+
+
+
 	}
 }
