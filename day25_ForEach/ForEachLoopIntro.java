c13c2a6f0e457098388cc280378e184006baec08
Yahya <yahyahussein91@gmail.com>
2022-01-08 17:19:03 -0800

    will continue tomorrw

--- day25_ForEach/ForEachLoopIntro.java
+++ day25_ForEach/ForEachLoopIntro.java
@@ -3,21 +3,6 @@ package day25_ForEach;
 public class ForEachLoopIntro {
 	public static void main(String[] args){
 
-		int[] numbers = {1,2,3,4,5};
 
-		for (int i = 0; i < numbers.length; i++) {
-			System.out.println(numbers[i]);
-		}
-
-		for (int n : numbers){
-			System.out.println("n = " + n);
-		}
-
-		System.out.println("---------------------------------");
-		String[] classMates = {"Yahya", "Kadir", "Igor", "Gulistan", "Nazli"};
-
-		for (String each : classMates){
-			System.out.println("each = " + each);
-		}
 	}
 }
