083b40969db8b45da21ca583168c733df455c94f
Yahya <yahyahussein91@gmail.com>
2022-01-09 21:15:00 -0800

    finding unique elements using for each loop

--- day25_ForEach/UniqueElements.java
+++ day25_ForEach/UniqueElements.java
@@ -3,5 +3,18 @@ package day25_ForEach;
 public class UniqueElements {
 	public static void main(String[] args){
 
+		String[] words = {"java", "java", "python", "c#"};
+
+		int count = 0;
+
+		for (String element : words) {
+			if (element.equals("java")){
+				count++;
+			}
+		}
+
+		if (count == 1){
+			System.out.println("java");
+		}
 	}
 }
