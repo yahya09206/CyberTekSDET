c67c408a7161b30eb9f3832edcc19db1882b684d
Yahya <yahyahussein91@gmail.com>
2022-01-09 20:02:24 -0800

    code to merge 2 arrays with different lengths

--- day25_ForEach/Merge3Arrays.java
+++ day25_ForEach/Merge3Arrays.java
@@ -3,5 +3,10 @@ package day25_ForEach;
 public class Merge3Arrays {
 	public static void main(String[] args){
 
+		int[] arr1 = {1,2,3,11},
+				arr2 = {4,5,6},
+				arr3 = {7,8,9,12,13,14};
+
+		int[] arr4 = new int[arr1.length + arr2.length + arr3.length];
 	}
 }
