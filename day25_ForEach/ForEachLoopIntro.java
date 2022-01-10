a91992fbb823d1be1af29f7f8795f18c9ffce221
Yahya <yahyahussein91@gmail.com>
2022-01-09 20:39:39 -0800

    printing array of ints using for each loop

--- day25_ForEach/ForEachLoopIntro.java
+++ day25_ForEach/ForEachLoopIntro.java
@@ -3,6 +3,16 @@ package day25_ForEach;
 public class ForEachLoopIntro {
 	public static void main(String[] args){
 
+		int[] numbers = {1,2,3,4,5};
 
+		for (int i = 0; i <= numbers.length - 1; i++) {
+			System.out.println("numbers[i] = " + numbers[i]);
+		}
+
+		System.out.println("---------------------------------------------");
+
+		for (int each : numbers){
+			System.out.println(each);
+		}
 	}
 }
