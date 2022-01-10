1dd35ecd41ad2729b555fa19ec28e3d24c99b184
Yahya <yahyahussein91@gmail.com>
2022-01-09 19:51:47 -0800

    create third array equal to size of first and second array combined

--- day25_ForEach/Merge2Arrays.java
+++ day25_ForEach/Merge2Arrays.java
@@ -3,6 +3,10 @@ package day25_ForEach;
 public class Merge2Arrays {
 	public static void main(String[] args){
 
+		int[] arr1 = {1,2,3,4},
+				arr2 = {5,6};
+
+		int[] arr3 = new int[arr1.length + arr2.length];
 
 	}
 }
