22c875f0dd5e441a5ff717dd24fc533f9864ea90
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:15:52 -0800

    test cases passing

---
+++ Day18_Recap/Replit/PrintFirstAndLastLetters.java
@@ -0,0 +1,14 @@
+package Day18_Recap.Replit;
+
+import java.util.Scanner;
+
+public class PrintFirstAndLastLetters {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String word = scan.next();
+
+        System.out.println(word.substring(0,1) + word.substring(word.length() - 1));
+
+    }
+}
