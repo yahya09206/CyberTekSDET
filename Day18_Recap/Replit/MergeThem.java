fcdce48d90cf8ad7dde28060a815269d03ad664a
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:41:54 -0800

    using substring to get letter

--- Day18_Recap/Replit/MergeThem.java
+++ Day18_Recap/Replit/MergeThem.java
@@ -10,7 +10,10 @@ public class MergeThem {
         String word2 = scan.next();
 
         if (word1.length() == 3 && word2.length() == 3){
-
+            System.out.println(word1.substring(0,1) + word2.substring(0,1) + word1.substring(1,2) + word2.substring(1
+                    ,2) + word1.substring(2) + word2.substring(2));
+        }else {
+            System.out.println("cannot merge");
         }
 
     }
