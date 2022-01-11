26ec7ac818ea79ec630f81fbbbedad7e8dccf88b
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:44:28 -0800

    program to reverse word manually

---
+++ Day18_Recap/Replit/ReverseIt.java
@@ -0,0 +1,13 @@
+package Day18_Recap.Replit;
+
+import java.util.Scanner;
+
+public class ReverseIt {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String word = scan.next();
+
+
+    }
+}
