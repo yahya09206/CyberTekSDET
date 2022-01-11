d3da9492f3314709f7905fc502d9f85016f9a447
Yahya <yahyahussein91@gmail.com>
2022-01-10 20:17:44 -0800

    code to print middle three letters of word of certain length

---
+++ Day18_Recap/Replit/MiddleThree.java
@@ -0,0 +1,13 @@
+package Day18_Recap.Replit;
+
+import java.util.Scanner;
+
+public class MiddleThree {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String word = scan.next();
+
+        if ()
+    }
+}
