546ad52982cb0427722e94a52f74e40401b1ee44
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:50:21 -0800

    forgot to concat to string

--- Day18_Recap/Replit/ReverseIt.java
+++ Day18_Recap/Replit/ReverseIt.java
@@ -8,6 +8,12 @@ public class ReverseIt {
         Scanner scan = new Scanner(System.in);
         String word = scan.next();
 
-
+        if (word.length() == 5){
+            System.out.println("" + word.charAt(4) + word.charAt(3) + word.charAt(2) + word.charAt(1) + word.charAt(0));
+        }else if (word.length() > 5){
+            System.out.println("Too long!");
+        }else {
+            System.out.println("Too short!");
+        }
     }
 }
