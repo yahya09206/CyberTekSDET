4104b18433c483931506ca586f597f0e6e2a7843
Yahya <yahyahussein91@gmail.com>
2022-01-10 20:23:25 -0800

    checking if email contains certain name

---
+++ Day18_Recap/Replit/Alejandro.java
@@ -0,0 +1,17 @@
+package Day18_Recap.Replit;
+
+import java.util.Scanner;
+
+public class Alejandro {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        String a = s.nextLine();
+
+        if (a.contains("alejandro")){
+            System.out.println("read");
+        }else {
+            System.out.println("don't read");
+        }
+    }
+}
