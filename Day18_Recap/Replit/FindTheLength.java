06165d02fb47c995463d2a9e229e974d3dcac5f6
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:10:30 -0800

    printing length of text

--- Day18_Recap/Replit/FindTheLength.java
+++ Day18_Recap/Replit/FindTheLength.java
@@ -1,7 +1,16 @@
 package Day18_Recap.Replit;
 
+import java.util.Scanner;
+
 public class FindTheLength {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the text:");
+        String text = scan.next();
+
+        System.out.println("Length is: " + text.length());
+
 
     }
 }
