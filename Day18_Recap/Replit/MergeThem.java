dc8d83a7d99206af2d886b34a7ec66302ef8bfd6
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:39:35 -0800

    making sure both words have length of 3

--- Day18_Recap/Replit/MergeThem.java
+++ Day18_Recap/Replit/MergeThem.java
@@ -1,8 +1,17 @@
 package Day18_Recap.Replit;
 
+import java.util.Scanner;
+
 public class MergeThem {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        String word1 = scan.next();
+        String word2 = scan.next();
+
+        if (word1.length() == 3 && word2.length() == 3){
+
+        }
 
     }
 }
