fba00733941aafe7969e5cba06f8d26b75dececd
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:33:57 -0800

    last condition for two characters

--- Day18_Recap/Replit/MiddleOne.java
+++ Day18_Recap/Replit/MiddleOne.java
@@ -13,6 +13,11 @@ public class MiddleOne {
             System.out.println(word.charAt(word.length() / 2));
         }else if (word.length() == 1){
             System.out.println(word + word + word);
+        }else if (word.length() % 2 == 0 && word.length() >= 4){
+            int midEven = word.length() / 2;
+            System.out.println("" + word.charAt(midEven - 1) + word.charAt(midEven));
+        }else {
+            System.out.println(word.length() + word.length());
         }
     }
 }
