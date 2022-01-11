1c36aff73b4e76742824c5f688e403211cf00642
Yahya <yahyahussein91@gmail.com>
2022-01-10 20:04:01 -0800

    code to print half of word twice

---
+++ Day18_Recap/Replit/PrintHalfTwice.java
@@ -0,0 +1,14 @@
+package Day18_Recap.Replit;
+
+import java.util.Scanner;
+
+public class PrintHalfTwice {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String word = scan.next();
+
+
+
+    }
+}
