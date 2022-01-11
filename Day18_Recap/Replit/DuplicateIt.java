d14f7071eafbda52d09305861a622392b66c7108
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:36:08 -0800

    duplicate two different words in a certain order

---
+++ Day18_Recap/Replit/DuplicateIt.java
@@ -0,0 +1,14 @@
+package Day18_Recap.Replit;
+
+import java.util.Scanner;
+
+public class DuplicateIt {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String word1 = scan.next();
+        String word2 = scan.next();
+
+
+    }
+}
