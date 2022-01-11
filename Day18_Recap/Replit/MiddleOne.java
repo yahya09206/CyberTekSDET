d6b082ba1b1076347389c67da48006ccede4a09a
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:23:48 -0800

    printing second condition

--- Day18_Recap/Replit/MiddleOne.java
+++ Day18_Recap/Replit/MiddleOne.java
@@ -9,6 +9,10 @@ public class MiddleOne {
         String word = scan.next();
 
 
-
+        if (word.length() >= 3 && word.length() % 2 != 0){
+            System.out.println(word.charAt(word.length() / 2));
+        }else if (word.length() == 1){
+            System.out.println(word + word + word);
+        }
     }
 }
