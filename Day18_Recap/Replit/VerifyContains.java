c2717e32866155d99ce4013d5feaa1ab4644dd16
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:00:52 -0800

    code to see if word is contained in sentence

---
+++ Day18_Recap/Replit/VerifyContains.java
@@ -0,0 +1,15 @@
+package Day18_Recap.Replit;
+
+import java.util.Scanner;
+
+public class VerifyContains {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String word = scan.nextLine();
+        String sentence = scan.nextLine();
+
+
+
+    }
+}
