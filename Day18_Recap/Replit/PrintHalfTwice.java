631ce3a1fe01a69297c04b046267151c5b348c72
Yahya <yahyahussein91@gmail.com>
2022-01-10 20:10:31 -0800

    this one took a min

--- Day18_Recap/Replit/PrintHalfTwice.java
+++ Day18_Recap/Replit/PrintHalfTwice.java
@@ -8,7 +8,10 @@ public class PrintHalfTwice {
         Scanner scan = new Scanner(System.in);
         String word = scan.next();
 
+        int getMiddle = (word.length()) / 2;
+        String firstHalf = word.substring(0, getMiddle);
 
+        System.out.println(firstHalf + firstHalf);
 
     }
 }
