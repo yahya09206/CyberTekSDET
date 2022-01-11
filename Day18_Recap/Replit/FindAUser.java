93b7ed12b70be75fc6a4776f3b205d5a47d9fc71
Yahya <yahyahussein91@gmail.com>
2022-01-10 19:08:59 -0800

    test cases passing

--- Day18_Recap/Replit/FindAUser.java
+++ Day18_Recap/Replit/FindAUser.java
@@ -1,7 +1,21 @@
 package Day18_Recap.Replit;
 
+import java.util.Scanner;
+
 public class FindAUser {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter full name:");
+        String name = scan.nextLine();
+
+        boolean nameFound = name.equalsIgnoreCase("Max Payne") || name.equalsIgnoreCase("Alan Wake");
+
+        if (nameFound){
+            System.out.println("User found!");
+        }else {
+            System.out.println("User not found!");
+        }
 
     }
 }
