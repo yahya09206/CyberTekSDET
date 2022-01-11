5025002e7d5d729516b666c2c30e8c52bd175810
Yahya <yahyahussein91@gmail.com>
2022-01-10 20:13:18 -0800

    simple if else statement to check for length

--- Day18_Recap/Replit/PrintLongestWord.java
+++ Day18_Recap/Replit/PrintLongestWord.java
@@ -9,6 +9,10 @@ public class PrintLongestWord {
         String word1 = scan.next();
         String word2 = scan.next();
 
-
+        if (word1.length() > word2.length()){
+            System.out.println(word1);
+        }else {
+            System.out.println(word2);
+        }
     }
 }
