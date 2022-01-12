e12cc38a81c65ac81058c90e8564e3e332d10877
Yahya <yahyahussein91@gmail.com>
2022-01-11 19:44:58 -0800

    code to print star specific amount of times

---
+++ day22_Recap/Replit/PrintNTimes.java
@@ -0,0 +1,14 @@
+package day22_Recap.Replit;
+
+import java.util.Scanner;
+
+public class PrintNTimes {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+
+
+    }
+}
