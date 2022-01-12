01a7904ee14dabf67ae5b0002b94d2751536f3a8
Yahya <yahyahussein91@gmail.com>
2022-01-11 19:42:59 -0800

    test cases passing

--- day22_Recap/Replit/EachLetter.java
+++ day22_Recap/Replit/EachLetter.java
@@ -8,7 +8,9 @@ public class EachLetter {
         Scanner scan = new Scanner(System.in);
         String word = scan.nextLine();
 
-
+        for (int i = 0; i <= word.length() - 1; i++){
+            System.out.println(word.charAt(i));
+        }
 
     }
 }
