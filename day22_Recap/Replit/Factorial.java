f07c399db8740ab0400599f4116bbb02f3a9d888
Yahya <yahyahussein91@gmail.com>
2022-01-11 19:35:22 -0800

    for loop to multiple by each number up to n

--- day22_Recap/Replit/Factorial.java
+++ day22_Recap/Replit/Factorial.java
@@ -1,8 +1,18 @@
 package day22_Recap.Replit;
 
+import java.util.Scanner;
+
 public class Factorial {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        long result = 1L;
 
+        for (int i = 1; i <= n; i++){
+            n *= i;
+        }
+
+        System.out.println(result);
     }
 }
