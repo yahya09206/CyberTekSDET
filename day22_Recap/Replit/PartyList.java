1e66b41e1d2fc8e7c0f90e78f8e996a0c8a0304e
Yahya <yahyahussein91@gmail.com>
2022-01-11 19:08:00 -0800

    prompting for first guest name

--- day22_Recap/Replit/PartyList.java
+++ day22_Recap/Replit/PartyList.java
@@ -1,7 +1,19 @@
 package day22_Recap.Replit;
 
+import java.util.Scanner;
+
 public class PartyList {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String list = " ";
+        System.out.println("What is the first guest's name?");
+        String prompt = scan.next();
+        String countinue = "";
+
+        while(countinue.equalsIgnoreCase("yes")){
+            System.out.println("");
+        }
 
     }
 }
