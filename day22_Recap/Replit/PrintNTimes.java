58573cb72e9ec67d14ffcd56026a3b1d9c06dca9
Yahya <yahyahussein91@gmail.com>
2022-01-11 19:46:37 -0800

    printing star

--- day22_Recap/Replit/PrintNTimes.java
+++ day22_Recap/Replit/PrintNTimes.java
@@ -8,7 +8,9 @@ public class PrintNTimes {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-
+        for (int i = 0; i <= n; i++){
+            System.out.println("*");
+        }
 
     }
 }
