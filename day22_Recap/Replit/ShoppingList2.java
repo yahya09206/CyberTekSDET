fd02adb292c6779e980c164298782f39c2b4e9ea
Yahya <yahyahussein91@gmail.com>
2022-01-11 19:03:18 -0800

    code for printing shopping list and total price of items

---
+++ day22_Recap/Replit/ShoppingList2.java
@@ -0,0 +1,31 @@
+package day22_Recap.Replit;
+
+import java.util.Scanner;
+
+public class ShoppingList2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String shoppingListReport = "";
+        String item = "";
+        String countinue = "";
+        double price = 0;
+        int count = 1;
+        double totalPrice = 0;
+
+        do {
+            System.out.println("Enter Item" + count + " and its price:");
+            item = scan.next();
+            price = scan.nextDouble();
+            shoppingListReport += ("Item" + count + ": " + item + " Price: " + price + ", ");
+            totalPrice += price;
+            System.out.println("Add one more item?");
+            countinue = scan.next().toLowerCase();
+            count++;
+        }while(countinue.equals("yes"));
+
+        System.out.println(shoppingListReport.substring(0, shoppingListReport.length() - 2));
+        System.out.println("Total price: " + totalPrice);
+
+    }
+}
