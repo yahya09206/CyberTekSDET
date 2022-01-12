a086882b4892b4171ea8e2b36cf69bb2482d717c
Yahya <yahyahussein91@gmail.com>
2022-01-11 19:31:16 -0800

    counting occurence of words

---
+++ day22_Recap/Replit/CatsAndDogs.java
@@ -0,0 +1,20 @@
+package day22_Recap.Replit;
+
+import java.util.Scanner;
+
+public class CatsAndDogs {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int countOfCats = 0;
+        int countOfDogs = 0;
+        String word = scan.next();
+        int wordcharCount = word.length();
+        int tCharCount = 3; // character count of target word we are looking for
+
+        for (int i = 0; i <= wordcharCount - tCharCount; i++){
+
+        }
+
+    }
+}
