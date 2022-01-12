7ad820af5a882125f99e2cbeed5b2d4516e3539a
Yahya <yahyahussein91@gmail.com>
2022-01-11 19:42:08 -0800

    code to print each letter on different line

---
+++ day22_Recap/Replit/EachLetter.java
@@ -0,0 +1,14 @@
+package day22_Recap.Replit;
+
+import java.util.Scanner;
+
+public class EachLetter {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String word = scan.nextLine();
+
+
+
+    }
+}
