1d075a54f3dbe6d4b0e498c44aa5ab6fe7a584fc
Yahya <yahyahussein91@gmail.com>
2022-01-12 21:00:23 -0800

    checking if array size is equal to two

--- day27_Recap/Replit/FrontPiece.java
+++ day27_Recap/Replit/FrontPiece.java
@@ -1,7 +1,20 @@
 package day27_Recap.Replit;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FrontPiece {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int[] num = new int[size];
+        for(int i =0; i < size; i++) {
+            num[i] = scan.nextInt();
+        }
+
+        if (num.length == 2){
+            System.out.println(Arrays.toString(num));
+        }
     }
 }
