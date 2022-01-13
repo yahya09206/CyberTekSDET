457615331077626ddd7879f4c8f829eaed32c703
Yahya <yahyahussein91@gmail.com>
2022-01-12 21:06:40 -0800

    create a variable of double sum

--- day27_Recap/Replit/AvgTemp.java
+++ day27_Recap/Replit/AvgTemp.java
@@ -1,7 +1,15 @@
 package day27_Recap.Replit;
 
+import java.util.Scanner;
+
 public class AvgTemp {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        double[] temps = { scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(),
+                scan.nextDouble(), scan.nextDouble(), scan.nextDouble() , scan.nextDouble()  };
+
+
+        double sum = 0;
     }
 }
