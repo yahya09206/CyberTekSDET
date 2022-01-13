51b7e173a2e27d3c0ac7fba2ca494ea8d5f56473
Yahya <yahyahussein91@gmail.com>
2022-01-12 21:13:39 -0800

    variable to keep track of count

--- day27_Recap/Replit/CountEvens.java
+++ day27_Recap/Replit/CountEvens.java
@@ -1,7 +1,14 @@
 package day27_Recap.Replit;
 
+import java.util.Scanner;
+
 public class CountEvens {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+
+        int count = 0;
 
     }
 }
