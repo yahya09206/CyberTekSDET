a2220ab6043a774195e5ee195d666a2b4f4bbc87
Yahya <yahyahussein91@gmail.com>
2022-01-12 20:56:17 -0800

    set last value to be same as new value at last index in array

--- day27_Recap/Replit/MakeLast.java
+++ day27_Recap/Replit/MakeLast.java
@@ -1,8 +1,19 @@
 package day27_Recap.Replit;
 
+import java.util.Scanner;
+
 public class MakeLast {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int[] nums = new int[size];
+        for(int i =0; i < size; i++) {
+            nums[i] = scan.nextInt();
+        }
+
+        int[] newNums = new int[nums.length * 2];
+        newNums[nums.length - 1] = nums[nums.length - 1];
 
     }
 }
