4d55468e6a325ef8797b66dc511471fcdf6fd28e
Yahya <yahyahussein91@gmail.com>
2022-01-13 19:11:44 -0800

    test cases are passing

--- day27_Recap/Replit/Has55.java
+++ day27_Recap/Replit/Has55.java
@@ -12,9 +12,10 @@ public class Has55 {
 
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == 5 && nums[i + 1] == 5){
-
+                result = "true";
             }
         }
+        System.out.println(result);
 
     }
 }
