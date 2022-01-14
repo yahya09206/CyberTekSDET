5fc50d8c1d79059bb18ec6d214430d8bb3806cc3
Yahya <yahyahussein91@gmail.com>
2022-01-13 19:02:17 -0800

    lets see first try

--- day27_Recap/Replit/FindMax.java
+++ day27_Recap/Replit/FindMax.java
@@ -8,7 +8,14 @@ public class FindMax {
         Scanner input = new Scanner(System.in);
         int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
 
+        int max = 0;
 
+        for (int i = 0; i <= nums.length; i++) {
+            if (nums[i] > max){
+                max = nums[i];
+            }
+        }
+        System.out.println(max);
 
     }
 }
