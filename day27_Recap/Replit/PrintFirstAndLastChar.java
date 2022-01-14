11555ee89166aa8034f692982a74608de3208bca
Yahya <yahyahussein91@gmail.com>
2022-01-13 19:30:49 -0800

    code to print first and last char of strings in array

---
+++ day27_Recap/Replit/PrintFirstAndLastChar.java
@@ -0,0 +1,12 @@
+package day27_Recap.Replit;
+
+import java.util.Scanner;
+
+public class PrintFirstAndLastChar {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String[] words = {input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine()};
+
+    }
+}
