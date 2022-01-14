e5a8b6473d036345888e4a1b2b7de281400adb29
Yahya <yahyahussein91@gmail.com>
2022-01-13 19:09:47 -0800

    setting up for loop

--- day27_Recap/Replit/Has55.java
+++ day27_Recap/Replit/Has55.java
@@ -8,7 +8,11 @@ public class Has55 {
         Scanner input = new Scanner(System.in);
         int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
 
+        String result = "false";
 
+        for (int i = 0; i < nums.length - 1; i++) {
+
+        }
 
     }
 }
