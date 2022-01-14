bd1b09d04478299907e0c907109784726018b3ca
Yahya <yahyahussein91@gmail.com>
2022-01-13 19:10:47 -0800

    check if left i is equal to five then check to the right of it

--- day27_Recap/Replit/Has55.java
+++ day27_Recap/Replit/Has55.java
@@ -11,7 +11,9 @@ public class Has55 {
         String result = "false";
 
         for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 5 && nums[i + 1] == 5){
 
+            }
         }
 
     }
