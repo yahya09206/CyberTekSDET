f177cbb8bd591558360c0b9d85e475d4f81141fb
Yahya <yahyahussein91@gmail.com>
2022-01-13 19:04:36 -0800

    test cases passing

--- day27_Recap/Replit/FindMax.java
+++ day27_Recap/Replit/FindMax.java
@@ -9,7 +9,6 @@ public class FindMax {
         int[] nums = {input.nextInt(),input.nextInt(),input.nextInt()};
 
         int max = nums[0];
-
         for (int i = 0; i <= nums.length - 1; i++) {
             if (nums[i] > max){
                 max = nums[i];
