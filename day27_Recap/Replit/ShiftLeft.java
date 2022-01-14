f94becdc616518dfb3ee61311f33d07c97e9b036
Yahya <yahyahussein91@gmail.com>
2022-01-13 19:15:44 -0800

    code to shift values to left

---
+++ day27_Recap/Replit/ShiftLeft.java
@@ -0,0 +1,15 @@
+package day27_Recap.Replit;
+
+import java.util.Scanner;
+
+public class ShiftLeft {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int[] nums = new int[size];
+
+
+
+    }
+}
