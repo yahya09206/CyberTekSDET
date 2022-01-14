7e8fdc35e8d08236158b4f0d2ebcc0d32d0d9e3f
Yahya <yahyahussein91@gmail.com>
2022-01-13 19:03:43 -0800

    assigned max to first element in array

--- day27_Recap/Replit/FindMax.java
+++ day27_Recap/Replit/FindMax.java
@@ -6,16 +6,15 @@ public class FindMax {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+        int[] nums = {input.nextInt(),input.nextInt(),input.nextInt()};
 
-        int max = 0;
+        int max = nums[0];
 
-        for (int i = 0; i <= nums.length; i++) {
+        for (int i = 0; i <= nums.length - 1; i++) {
             if (nums[i] > max){
                 max = nums[i];
             }
         }
         System.out.println(max);
-
     }
 }
