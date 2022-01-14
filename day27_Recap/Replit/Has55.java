edd106f15c4d5fa2b68a2933f7cccbee6105a185
Yahya <yahyahussein91@gmail.com>
2022-01-13 19:08:43 -0800

    check if two fives are next to each other in array

---
+++ day27_Recap/Replit/Has55.java
@@ -0,0 +1,14 @@
+package day27_Recap.Replit;
+
+import java.util.Scanner;
+
+public class Has55 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+
+
+
+    }
+}
