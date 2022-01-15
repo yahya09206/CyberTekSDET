f81abc4b903bde55cb5cabf60ca34dfdf8553553
Yahya <yahyahussein91@gmail.com>
2022-01-14 20:49:27 -0800

    had the right idea

--- day27_Recap/Replit/PrintFirstAndLastChar2.java
+++ day27_Recap/Replit/PrintFirstAndLastChar2.java
@@ -1,20 +1,22 @@
 package day27_Recap.Replit;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PrintFirstAndLastChar2 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        String[] words = {input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine()};
+        String[] words = {input.nextLine(),input.nextLine()};
 
-        String[] newArr = new String[words.length];
-        int j = 0;
-        for (int i = 0; i < words.length; i++) {
+        String[] chars = new String[words.length];
+
+        for (int i = 0; i < words.length - 1; i++) {
             String eachWord = words[i];
-            newArr[j++] = words[eachWord.charAt(0) + eachWord.charAt(eachWord.length() - 1)];
+            chars[i] = "" + words[eachWord.charAt(0) + eachWord.charAt(eachWord.length() - 1)];
             //System.out.println("" + eachWord.charAt(0) + eachWord.charAt(eachWord.length() - 1));
         }
+        Arrays.toString(newArr);
 
     }
 }
