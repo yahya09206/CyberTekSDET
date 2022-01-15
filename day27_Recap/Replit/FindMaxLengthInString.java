3db9e3f56c7ca9ccb43675f89805d22fa3c29bc8
Yahya <yahyahussein91@gmail.com>
2022-01-14 20:32:01 -0800

    pasted over prompt code

--- day27_Recap/Replit/FindMaxLengthInString.java
+++ day27_Recap/Replit/FindMaxLengthInString.java
@@ -1,7 +1,18 @@
 package day27_Recap.Replit;
 
+import java.util.Scanner;
+
 public class FindMaxLengthInString {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String[] words = new String[5];
+        for(int i = 0; i < 5;  i++) {
+
+            words[i] = input.nextLine();
+
+        }
+
 
 
     }
