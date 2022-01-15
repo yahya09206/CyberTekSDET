df6e1f9162875ebea31f6923d0cd9e13dad2827e
Yahya <yahyahussein91@gmail.com>
2022-01-14 21:09:30 -0800

    same as last except 3 on each line

---
+++ day27_Recap/Replit/Printing03.java
@@ -0,0 +1,18 @@
+package day27_Recap.Replit;
+
+import java.util.Scanner;
+
+public class Printing03 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String[] arr = new String[6];
+        for (int i=0;i<6;i++)
+        {
+            arr[i] = input.nextLine();
+        }
+
+
+
+    }
+}
