8504fba2832daedf206975c02501872f72e1c353
Yahya <yahyahussein91@gmail.com>
2022-01-14 20:54:43 -0800

    code to print shortest word

---
+++ day27_Recap/Replit/PrintShortestWord.java
@@ -0,0 +1,14 @@
+package day27_Recap.Replit;
+
+import java.util.Scanner;
+
+public class PrintShortestWord {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String[] str = {scan.next(), scan.next(), scan.next(),
+                scan.next(), scan.next(), scan.next()};
+
+
+    }
+}
