30b76b9530900d65d6f35a0d1a1139b2c3df57a5
Yahya <yahyahussein91@gmail.com>
2022-01-15 20:03:38 -0800

    array reverse not working

--- day27_Recap/ArraysAndString/ArraysAndStrings.java
+++ day27_Recap/ArraysAndString/ArraysAndStrings.java
@@ -4,15 +4,22 @@ public class ArraysAndStrings {
     public static void main(String[] args) {
 
         String str = "Java is fun";
+        String[] str2 = {"Java is fun"};
 
 
         String result = "";
-        String[] result2 = str.split(" ");
 
         for (int i = str.length() - 1; i >= 0 ; i--) {
             result += str.charAt(i);
         }
         System.out.println(result);
+
+
+        String result2 = "";
+        for (int i = str2.length; i > 0; i--){
+            result2 += "" + str2[i];
+        }
+        System.out.println(result2);
 
     }
 }
