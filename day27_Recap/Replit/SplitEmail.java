9f8f878f22a32f83049450429594d5015b9b6c76
Yahya <yahyahussein91@gmail.com>
2022-01-15 19:44:00 -0800

    splitting email by @ sign

---
+++ day27_Recap/Replit/SplitEmail.java
@@ -0,0 +1,14 @@
+package day27_Recap.Replit;
+
+import java.util.Scanner;
+
+public class SplitEmail {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String email = input.nextLine();
+
+        String[] str = email.split("@");
+
+    }
+}
