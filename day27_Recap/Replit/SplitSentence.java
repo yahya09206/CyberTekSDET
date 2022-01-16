8f4303d8996a292a37081786e1092ed52102b600
Yahya <yahyahussein91@gmail.com>
2022-01-15 19:47:39 -0800

    code to print words on seperate lines

---
+++ day27_Recap/Replit/SplitSentence.java
@@ -0,0 +1,13 @@
+package day27_Recap.Replit;
+
+import java.util.Scanner;
+
+public class SplitSentence {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String sentence = input.nextLine();
+
+
+    }
+}
