47882c7e9eaa56f341093c29e93fa93ca7914b19
Yahya <yahyahussein91@gmail.com>
2022-01-15 19:49:06 -0800

    this was pretty easy

--- day27_Recap/Replit/SplitSentence.java
+++ day27_Recap/Replit/SplitSentence.java
@@ -8,6 +8,11 @@ public class SplitSentence {
         Scanner input = new Scanner(System.in);
         String sentence = input.nextLine();
 
+        String[] str = sentence.split(" ");
+
+        for (int i = 0; i <= str.length - 1; i++){
+            System.out.println(str[i]);
+        }
 
     }
 }
