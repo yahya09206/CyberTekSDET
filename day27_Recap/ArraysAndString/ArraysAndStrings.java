46757c49970856d43054533a94f89c347e50bbda
Yahya <yahyahussein91@gmail.com>
2022-01-15 20:12:58 -0800

    using foreach is much easier

--- day27_Recap/ArraysAndString/ArraysAndStrings.java
+++ day27_Recap/ArraysAndString/ArraysAndStrings.java
@@ -20,6 +20,7 @@ public class ArraysAndStrings {
             for (int i = element.length() - 1; i >= 0; i--){
                 reverse += element.charAt(i);
             }
+            System.out.println(reverse);
         }
     }
 }
