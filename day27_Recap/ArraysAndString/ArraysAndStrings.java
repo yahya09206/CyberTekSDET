7c099fe9627cfa662a393da0f652135313592d89
Yahya <yahyahussein91@gmail.com>
2022-01-15 19:54:52 -0800

    regular string reverse is working

--- day27_Recap/ArraysAndString/ArraysAndStrings.java
+++ day27_Recap/ArraysAndString/ArraysAndStrings.java
@@ -4,9 +4,15 @@ public class ArraysAndStrings {
     public static void main(String[] args) {
 
         String str = "Java is fun";
-        String[] str2 = {"Java is fun"};
+
 
         String result = "";
+        String[] result2 = str.split(" ");
+
+        for (int i = str.length() - 1; i >= 0 ; i--) {
+            result += str.charAt(i);
+        }
+        System.out.println(result);
 
     }
 }
