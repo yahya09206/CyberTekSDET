1d29004e958f7158a2a91da78b5ad5ca8b10faec
Yahya <yahyahussein91@gmail.com>
2022-01-15 19:51:53 -0800

    reversing string and array

--- day27_Recap/ArraysAndString/ArraysAndStrings.java
+++ day27_Recap/ArraysAndString/ArraysAndStrings.java
@@ -3,6 +3,10 @@ package day27_Recap.ArraysAndString;
 public class ArraysAndStrings {
     public static void main(String[] args) {
 
+        String str = "Java is fun";
+        String[] str2 = {"Java is fun"};
+
+        String result = "";
 
     }
 }
