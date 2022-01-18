8842dcebe1037aa05bd0c527d15ae3dbcfcede59
Yahya <yahyahussein91@gmail.com>
2022-01-17 20:02:13 -0800

    code to find if anagram

---
+++ day27_Recap/MorePractice/Anagram.java
@@ -0,0 +1,13 @@
+package day27_Recap.MorePractice;
+
+public class Anagram {
+    public static void main(String[] args) {
+
+        /*
+    Write a  function that check if a string is build out of the same letters as another string.
+            Ex: same("abc", "cab"); -> true , Solution 1:
+                same("abc", "abb"); -> false:
+     */
+
+    }
+}
