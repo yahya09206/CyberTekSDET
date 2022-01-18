7b3d7b8039ca4a684567f0259893419f3852827d
Yahya <yahyahussein91@gmail.com>
2022-01-17 20:16:00 -0800

    other loop to go through each char

--- day27_Recap/MorePractice/FirstUniqueCharacter.java
+++ day27_Recap/MorePractice/FirstUniqueCharacter.java
@@ -11,7 +11,9 @@ public class FirstUniqueCharacter {
         // loop to go thru whole string
         for (int i = 0; i <= str.length() ; i++) {
             int frequency = 0;
+            for (int j = 0; j <= str.length(); j++) {
 
+            }
         }
     }
 }
