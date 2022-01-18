1477556db9679a82acbc4bd4f1aabc2ce821da55
Yahya <yahyahussein91@gmail.com>
2022-01-17 20:21:21 -0800

    called method to check if working

--- day27_Recap/MorePractice/FirstUniqueCharacter.java
+++ day27_Recap/MorePractice/FirstUniqueCharacter.java
@@ -1,18 +1,20 @@
 package day27_Recap.MorePractice;
 
 public class FirstUniqueCharacter {
+    public static void main(String[] args) {
 
+        System.out.println(firstNonRepeatedChar("aaaaabbbcdddeee"));
+    }
     /*
     write a program that can return the first non-repeated character from a string
      */
-    public char firstNonRepeatedChar(String str){
-
+    public static char firstNonRepeatedChar(String str){
 
         // loop to go thru whole string
-        for (int i = 0; i <= str.length() ; i++) {
+        for (int i = 0; i <= str.length() - 1; i++) {
             int frequency = 0;
             // loop to go thru each char
-            for (int j = 0; j <= str.length(); j++) {
+            for (int j = 0; j <= str.length() - 1; j++) {
                 // compare at each index
                 if (str.charAt(i) == str.charAt(j)){
                     // add to freq if equal
