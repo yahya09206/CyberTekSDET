91ef929f28a0c539d3131e267fb416952231a9cb
Yahya <yahyahussein91@gmail.com>
2022-01-17 20:14:10 -0800

    code to find first unique character

---
+++ day27_Recap/MorePractice/FirstUniqueCharacter.java
@@ -0,0 +1,12 @@
+package day27_Recap.MorePractice;
+
+public class FirstUniqueCharacter {
+
+    /*
+    write a program that can return the first non-repeated character from a string
+     */
+    public char firstNonRepeatedChar(String str){
+
+
+    }
+}
