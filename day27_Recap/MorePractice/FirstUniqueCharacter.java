0f7a0c7e6f991844d961a938514d3ce987c9559f
Yahya <yahyahussein91@gmail.com>
2022-01-17 20:18:32 -0800

    updated comments

--- day27_Recap/MorePractice/FirstUniqueCharacter.java
+++ day27_Recap/MorePractice/FirstUniqueCharacter.java
@@ -11,11 +11,15 @@ public class FirstUniqueCharacter {
         // loop to go thru whole string
         for (int i = 0; i <= str.length() ; i++) {
             int frequency = 0;
+            // loop to go thru each char
             for (int j = 0; j <= str.length(); j++) {
+                // compare at each index
                 if (str.charAt(i) == str.charAt(j)){
+                    // add to freq if equal
                     frequency++;
                 }
             }
+            // return if only one occurrence
             if (frequency == 1){
                 return str.charAt(i);
             }
