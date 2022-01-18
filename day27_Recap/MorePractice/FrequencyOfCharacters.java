67019b953956c43322e4cd56149fde6c08682bf3
Yahya <yahyahussein91@gmail.com>
2022-01-17 20:24:50 -0800

    first for loop to go thru string

--- day27_Recap/MorePractice/FrequencyOfCharacters.java
+++ day27_Recap/MorePractice/FrequencyOfCharacters.java
@@ -8,6 +8,11 @@ public class FrequencyOfCharacters {
     public static String frequencyOfChars(String str){
         String result = "";
 
+        for (int i = 0; i <= str.length() - 1; i++){
+            int frequency = 0;
+
+        }
+
         return result;
     }
 }
