23b51b3730c07792c6331c079661c1aad047ed3d
Yahya <yahyahussein91@gmail.com>
2022-01-21 23:04:18 -0800

    task to be completed

--- replit/Practice.java
+++ replit/Practice.java
@@ -4,6 +4,11 @@ public class Practice {
     public static void main(String[] args) {
 
         // Task1:
+//        1. create a class named EmployeeInfo
+//        1. declare the following variables:
+//        name, gender, age, companyName, jobTitle, isFullTime, isMarried, salary
+
+//        2. full info of the employee
 
     }
 }
