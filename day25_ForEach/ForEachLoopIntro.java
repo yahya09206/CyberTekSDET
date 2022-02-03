4100f900480e357fb7474e5633e5cd8a009147f1
Yahya <yahyahussein91@gmail.com>
2022-02-02 20:41:30 -0800

    using for each to print nums

--- day25_ForEach/ForEachLoopIntro.java
+++ day25_ForEach/ForEachLoopIntro.java
@@ -3,5 +3,15 @@ package day25_ForEach;
 public class ForEachLoopIntro {
 	public static void main(String[] args){
 
+		int[] numbers = {1,2,3,4,5};
+
+		// old way
+		for(int i = 0; i <= numbers.length - 1; i++){
+			System.out.print(numbers[i]);
+		}
+
+		for (int number : numbers) {
+			System.out.println(number);
+		}
 	}
 }
