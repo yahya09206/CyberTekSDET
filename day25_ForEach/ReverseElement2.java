fa5d1a7c822a4c8ea36fe6103a18887b770e4360
Yahya <yahyahussein91@gmail.com>
2022-02-02 20:19:49 -0800

    elements are reversing in place

--- day25_ForEach/ReverseElement2.java
+++ day25_ForEach/ReverseElement2.java
@@ -1,15 +1,21 @@
 package day25_ForEach;
 
+import java.util.Arrays;
+
 public class ReverseElement2 {
 	public static void main(String[] args){
 
 		String[] arr = {"Java", "Python", "C#"};
-		String reverse = "";
 
-		String element = arr[0];
+		for (int j = 0; j <= arr.length - 1; j++){
+			String element = arr[j];
+			String reverse = "";
 
-		for (int i = element.length() - 1; i >= 0; i--){
-
+			for (int i = element.length() - 1; i >= 0; i--){
+				reverse += element.charAt(i);
+			}
+			arr[j] = reverse;
 		}
+		System.out.println(Arrays.toString(arr));
 	}
 }
