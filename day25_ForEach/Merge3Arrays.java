b46929c2103b0db7d229faff44bcb12ef99e6d99
Yahya <yahyahussein91@gmail.com>
2022-02-02 20:29:28 -0800

    code to merge 3 arrays

--- day25_ForEach/Merge3Arrays.java
+++ day25_ForEach/Merge3Arrays.java
@@ -3,5 +3,8 @@ package day25_ForEach;
 public class Merge3Arrays {
 	public static void main(String[] args){
 
+		int[] arr1 = {1,2,3,4};
+		int[] arr2 = {5,6};
+		int[] arr3 = {10,11,12,13};
 	}
 }
