4fa25340519d13ff2312cd93eb7d8859ddf75663
Yahya <yahyahussein91@gmail.com>
2022-02-02 21:32:44 -0800

    nested for each loops

--- day25_ForEach/UniqueElements2.java
+++ day25_ForEach/UniqueElements2.java
@@ -3,5 +3,20 @@ package day25_ForEach;
 public class UniqueElements2 {
 	public static void main(String[] args){
 
+		char[] chars = {'A', 'B', 'A', 'C', 'A', 'D', 'D'};
+
+		for (char characters : chars) {
+
+			int count = 0;
+
+			for (char each : chars) {
+				if (each == characters) {
+					count++;
+				}
+			}
+			if (count == 1){
+				System.out.println(characters);
+			}
+		}
 	}
 }
