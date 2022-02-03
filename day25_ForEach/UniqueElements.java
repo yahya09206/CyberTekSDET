63f18bc019fab644f413df821d69276a474ae012
Yahya <yahyahussein91@gmail.com>
2022-02-02 21:28:56 -0800

    printing unique elements using for each loop

--- day25_ForEach/UniqueElements.java
+++ day25_ForEach/UniqueElements.java
@@ -3,5 +3,20 @@ package day25_ForEach;
 public class UniqueElements {
 	public static void main(String[] args){
 
+		String[] arr = {"java", "java", "python", "c#"};
+
+		for (String each : arr) {
+
+			int count = 0;
+
+			for (String word : arr) {
+				if (word.equals(each)) {
+					count++;
+				}
+			}
+			if (count == 1) {
+				System.out.println(each);
+			}
+		}
 	}
 }
